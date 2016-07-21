3e00b02d2abb9f65db4dc5c19f5e6ce801bba51c
Evan Hallmark <ehallmark@gttgrp.com>
2016-07-21 14:13:22 -0700

    sql

---
+++ add_vector_columns.sql
@@ -0,0 +1,8 @@
+\connect patentdb;
+
+ALTER TABLE patent_vectors ADD COLUMN is_testing BOOLEAN NOT NULL DEFAULT(FALSE);
+ALTER TABLE patent_vectors ADD COLUMN compdb_technologies DOUBLE PRECISION[];
+ALTER TABLE patent_vectors ADD COLUMN is_valuable BOOLEAN;
+
+CREATE INDEX is_testing_index ON patent_vectors(is_testing);
+CREATE INDEX is_valuable_index ON patent_vectors(is_valuable) WHERE is_valuable IS NOT NULL;
