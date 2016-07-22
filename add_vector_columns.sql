af70d08fd7dd7a4f5fe57314a71b6c0342152d5a
Evan Hallmark <ehallmark@gttgrp.com>
2016-07-22 11:46:35 -0700

    sql

--- add_vector_columns.sql
+++ add_vector_columns.sql
@@ -6,3 +6,4 @@ ALTER TABLE patent_vectors ADD COLUMN is_valuable BOOLEAN;
 
 CREATE INDEX is_testing_index ON patent_vectors(is_testing);
 CREATE INDEX is_valuable_index ON patent_vectors(is_valuable) WHERE is_valuable IS NOT NULL;
+CREATE INDEX non_null_compdb_tech_idx ON patent_vectors(compdb_technologies) WHERE compdb_technologies IS NOT NULL;
