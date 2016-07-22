0a52e2bd46a99454fd7d3007348c05a28d0d31f2
Evan Hallmark <ehallmark@gttgrp.com>
2016-07-22 09:26:05 -0700

    sql

--- create_last_ingest_table.sql
+++ create_last_ingest_table.sql
@@ -7,5 +7,5 @@ CREATE TABLE last_vectors_ingest (
     pub_date INTEGER
 );
 
-INSERT INTO last_vectors_ingest (program_name,pub_date) VALUES ("patent_vectors",20050000), ("classification_vectors", 20050000), ("claim_vectors", 20050000);
+INSERT INTO last_vectors_ingest (program_name,pub_date) VALUES ('patent_vectors',20050000), ('classification_vectors', 20050000), ('claim_vectors', 20050000);
 
