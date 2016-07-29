bb716bf04f7d186fd7d7f25eba2896f93d4b54e0
Evan Hallmark <ehallmark@gttgrp.com>
2016-07-29 08:43:43 -0700

    fixed claim iterator

--- src/seeding/PatentClaimIterator.java
+++ src/seeding/PatentClaimIterator.java
@@ -1,6 +1,8 @@
 package seeding;
 
+
 import java.sql.SQLException;
+
 
 /**
  * Created by ehallmark on 7/29/16.
@@ -12,4 +14,42 @@ public class PatentClaimIterator extends BasePatentIterator {
     protected void resetQuery() throws SQLException {
         resultSet = Database.getPatentClaimVectorData();
     }
+    @Override
+    public String nextSentence() {
+        try {
+            // Check for more results in result set
+            resultSet.next();
+            return resultSet.getString(1);
+
+        } catch(SQLException sql) {
+            sql.printStackTrace();
+            throw new RuntimeException("SQL ERROR");
+        }
+    }
+
+    @Override
+    public boolean hasNext() {
+        try {
+            return (resultSet == null || !(resultSet.isAfterLast() || resultSet.isLast()));
+        } catch (SQLException sql) {
+            sql.printStackTrace();
+            throw new RuntimeException("SQL ERROR WHILE ITERATING");
+        }
+    }
+
+    @Override
+    public void reset() {
+        try {
+            if(resultSet!=null && !resultSet.isClosed()) resultSet.close();
+        } catch(SQLException sql) {
+            sql.printStackTrace();
+        }
+        try {
+            resetQuery();
+        } catch(SQLException sql) {
+            sql.printStackTrace();
+            throw new RuntimeException("UNABLE TO RESET QUERY");
+        }
+    }
+
 }
