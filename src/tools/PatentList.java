eb7249deabfca5a2166739ac1a93da6f1a28daf5
Evan Hallmark <ehallmark@gttgrp.com>
2016-08-01 09:52:01 -0700

    better serialization

--- src/tools/PatentList.java
+++ src/tools/PatentList.java
@@ -2,13 +2,15 @@ package tools;
 
 import server.AbstractPatent;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
 /**
  * Created by ehallmark on 8/1/16.
  */
-public class PatentList extends ArrayList<AbstractPatent> {
-    public String sortedBy;
+public class PatentList extends ArrayList<AbstractPatent> implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String sortedBy;
     public PatentList(int capacity, String type) {
         super(capacity);
         sortedBy=type;
