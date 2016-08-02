656f23361b8367055503f34b2681928c83c0e6d7
Evan Hallmark <ehallmark@gttgrp.com>
2016-08-02 09:24:26 -0700

    stupid

--- src/server/PatentResponse.java
+++ src/server/PatentResponse.java
@@ -8,7 +8,6 @@ import java.util.List;
  * Created by ehallmark on 7/27/16.
  */
 public class PatentResponse extends ServerResponse {
-    public List<PatentList> results;
     public PatentResponse(List<PatentList> patents, String query) {
         super(query,"Success",patents);
     }
