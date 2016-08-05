690fc9dcf6b0bc56a57e1d56d46c524b986b54cd
Evan Hallmark <ehallmark@gttgrp.com>
2016-08-05 11:19:41 -0700

    minor bug

--- src/seeding/Database.java
+++ src/seeding/Database.java
@@ -31,7 +31,7 @@ public class Database {
 	private static final String selectVectorsStatement = "SELECT pub_doc_number,"+ String.join(",",Constants.DEFAULT_1D_VECTORS)+","+String.join(",",Constants.DEFAULT_2D_VECTORS)+",claims_numbers FROM patent_vectors WHERE pub_doc_number=ANY(?)";
 	private static final String selectSingleVectorStatement = "SELECT "+ String.join(",",Constants.DEFAULT_1D_VECTORS)+","+String.join(",",Constants.DEFAULT_2D_VECTORS)+",claims_numbers FROM patent_vectors WHERE pub_doc_number=?";
 	private static final String selectAllCandidateSets = "SELECT name, id FROM candidate_sets";
-	private static final String selectPatentNumbersByAssignee = "select doc_number from (select distinct on (p.doc_number) p.doc_number,name,q.uid from patent_assignment_property_document as p join patent_assignment_assignee as q on (p.assignment_reel_frame=q.assignment_reel_frame) where (p.doc_kind='B1' or p.doc_kind='B2') and upper(name) like upper(?)||'%' order by p.doc_number,q.uid desc) as temp join patent_assignment_assignee as a on (temp.uid=a.uid and upper(a.name) like upper(?)||'%'";
+	private static final String selectPatentNumbersByAssignee = "select doc_number from (select distinct on (p.doc_number) p.doc_number,name,q.uid from patent_assignment_property_document as p join patent_assignment_assignee as q on (p.assignment_reel_frame=q.assignment_reel_frame) where (p.doc_kind='B1' or p.doc_kind='B2') and upper(name) like upper(?)||'%' order by p.doc_number,q.uid desc) as temp join patent_assignment_assignee as a on (temp.uid=a.uid and upper(a.name) like upper(?)||'%')";
 	private static final Set<Integer> badTech = new HashSet<>(Arrays.asList(136,182,301,316,519,527));
 
 	public static void setupMainConn() throws SQLException {
@@ -148,6 +148,7 @@ public class Database {
 		PreparedStatement ps = seedConn.prepareStatement(selectPatentNumbersByAssignee);
 		ps.setString(1, assignee);
 		ps.setString(2, assignee);
+		System.out.println(ps);
 		ResultSet rs = ps.executeQuery();
 		List<String> patents = new LinkedList<>();
 		while(rs.next()) {
