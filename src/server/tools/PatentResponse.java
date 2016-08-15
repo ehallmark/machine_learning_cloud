a1b8cb1e20c538497dc033142d69f7cc27273548
Evan Hallmark <ehallmark@gttgrp.com>
2016-08-15 07:54:45 -0700

    weird

--- src/server/tools/PatentResponse.java
+++ src/server/tools/PatentResponse.java
@@ -16,9 +16,9 @@ public class PatentResponse extends ServerResponse {
         super(query, to_html_table(patents,query,findDissimilar).render(),patents);
     }
 
-    private static Tag to_html_table(List<PatentList> patentLists, String query, boolean findDissimiar) {
+    private static Tag to_html_table(List<PatentList> patentLists, String query, boolean findDissimilar) {
         // List
-        String similarName = findDissimiar ? "Dissimilar" : "Similar";
+        String similarName = findDissimilar ? "Dissimilar" : "Similar";
         return div().with(
                 h3().with(label(similarName+" Patents to: "),a(query).withHref("https://www.google.com/patents/US"+query)),br(),
                 div().with(patentLists.stream().map(patentList ->
@@ -33,7 +33,7 @@ public class PatentResponse extends ServerResponse {
                                     patentList.getPatents().stream().sorted(new Comparator<AbstractPatent>() {
                                         @Override
                                         public int compare(AbstractPatent o1, AbstractPatent o2) {
-                                            return !findDissimiar ? Double.compare(o1.getSimilarity(),o2.getSimilarity()) : Double.compare(o2.getSimilarity(),o1.getSimilarity());
+                                            return findDissimilar ? Double.compare(o1.getSimilarity(),o2.getSimilarity()) : Double.compare(o2.getSimilarity(),o1.getSimilarity());
                                         }
                                     }).map(patent->
                                             tr().with(td().with(a(patent.getName()).withHref("https://www.google.com/patents/US"+patent.getName().split("\\s+")[0])),td(Double.toString(patent.getSimilarity())))
