9761b198d082ac045d8a98e49d34cfe0385b4962
Evan Hallmark <ehallmark@gttgrp.com>
2016-08-16 11:37:50 -0700

    bug

--- src/seeding/BuildParagraphVectors.java
+++ src/seeding/BuildParagraphVectors.java
@@ -12,43 +12,47 @@ import java.io.File;
  */
 public class BuildParagraphVectors {
 
-
     public static void main(String[] args) throws Exception {
+        Database.setupMainConn();
+        Database.setupSeedConn();
 
-        BasePatentIterator iterator = new BasePatentIterator(Constants.START_DATE);
-        TokenizerFactory t = new DefaultTokenizerFactory();
-        t.setTokenPreProcessor(new MyPreprocessor());
+        try {
+            BasePatentIterator iterator = new BasePatentIterator(Constants.START_DATE);
+            TokenizerFactory t = new DefaultTokenizerFactory();
+            t.setTokenPreProcessor(new MyPreprocessor());
 
-        ParagraphVectors vec = new ParagraphVectors.Builder()
-                .minWordFrequency(1)
-                .iterations(5)
-                .epochs(1)
-                .layerSize(Constants.VECTOR_LENGTH)
-                .learningRate(0.025)
-                .windowSize(5)
-                .iterate(iterator)
-                .trainWordVectors(false)
-                .tokenizerFactory(t)
-                .sampling(0)
-                .build();
+            ParagraphVectors vec = new ParagraphVectors.Builder()
+                    .minWordFrequency(1)
+                    .iterations(5)
+                    .epochs(1)
+                    .layerSize(Constants.VECTOR_LENGTH)
+                    .learningRate(0.025)
+                    .windowSize(5)
+                    .iterate(iterator)
+                    .trainWordVectors(false)
+                    .tokenizerFactory(t)
+                    .sampling(0)
+                    .build();
 
-        vec.fit();
+            vec.fit();
 
-        /*
-            In training corpus we have few lines that contain pretty close words invloved.
-            These sentences should be pretty close to each other in vector space
-            line 3721: This is my way .
-            line 6348: This is my case .
-            line 9836: This is my house .
-            line 12493: This is my world .
-            line 16393: This is my work .
-            this is special sentence, that has nothing common with previous sentences
-            line 9853: We now have one .
-            Note that docs are indexed from 0
-         */
+            /*
+                In training corpus we have few lines that contain pretty close words invloved.
+                These sentences should be pretty close to each other in vector space
+                line 3721: This is my way .
+                line 6348: This is my case .
+                line 9836: This is my house .
+                line 12493: This is my world .
+                line 16393: This is my work .
+                this is special sentence, that has nothing common with previous sentences
+                line 9853: We now have one .
+                Note that docs are indexed from 0
+             */
 
-        WordVectorSerializer.writeWordVectors(vec, new File(Constants.WORD_VECTORS_PATH));
+            WordVectorSerializer.writeWordVectors(vec, new File(Constants.WORD_VECTORS_PATH));
 
-
+        } finally {
+            Database.close();
+        }
     }
 }
