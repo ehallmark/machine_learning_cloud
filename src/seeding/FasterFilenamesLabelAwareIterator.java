1add644519b817da5c51e649f20452c44de313d7
Evan Hallmark <ehallmark@gttgrp.com>
2016-08-19 10:17:26 -0700

    faster?

--- src/seeding/FasterFilenamesLabelAwareIterator.java
+++ src/seeding/FasterFilenamesLabelAwareIterator.java
@@ -1,15 +1,13 @@
 package seeding;
 
 import lombok.NonNull;
-import org.deeplearning4j.text.documentiterator.FilenamesLabelAwareIterator;
+import org.apache.commons.io.FileUtils;
 import org.deeplearning4j.text.documentiterator.LabelAwareIterator;
 import org.deeplearning4j.text.documentiterator.LabelledDocument;
 import org.deeplearning4j.text.documentiterator.LabelsSource;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.nio.file.Files;
+import java.io.*;
+import java.nio.file.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -20,12 +18,13 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class FasterFilenamesLabelAwareIterator implements LabelAwareIterator {
 
-    protected List<File> files;
-    protected Iterator<File> iter;
+    protected File folder;
+    protected Iterator<Path> iter;
     protected AtomicInteger position = new AtomicInteger(0);
     protected LabelsSource labelsSource;
     protected boolean absPath = false;
     protected long lastTime;
+    protected final int SIZE = 8;
 
     /*
         Please keep this method protected, it's used in tests
@@ -34,25 +33,22 @@ public class FasterFilenamesLabelAwareIterator implements LabelAwareIterator {
 
     }
 
-    protected FasterFilenamesLabelAwareIterator(@NonNull List<File> files, @NonNull LabelsSource source) {
-        this.files = files;
-        this.iter = files.iterator();
+    protected FasterFilenamesLabelAwareIterator(@NonNull File folder, @NonNull LabelsSource source) throws IOException {
+        iter = Files.newDirectoryStream(folder.toPath()).iterator();
         this.labelsSource = source;
+        this.folder=folder;
         lastTime = System.currentTimeMillis();
+
     }
 
     @Override
     public LabelledDocument nextDocument() {
-        File fileToRead = iter.next();
+        File fileToRead = iter.next().toFile();
         String label = (absPath) ? fileToRead.getAbsolutePath() : fileToRead.getName();
         labelsSource.storeLabel(label);
         try {
             LabelledDocument document = new LabelledDocument();
-            BufferedReader reader = new BufferedReader(new FileReader(fileToRead));
-            StringBuilder builder = new StringBuilder();
-            String line = "";
-            while ((line = reader.readLine()) != null) builder.append(line);
-            document.setContent(builder.toString());
+            document.setContent(FileUtils.readFileToString(fileToRead));
             document.setLabel(label);
             int pos = position.getAndIncrement();
             if(pos%1000==999) {
@@ -60,7 +56,6 @@ public class FasterFilenamesLabelAwareIterator implements LabelAwareIterator {
                 lastTime = System.currentTimeMillis();
                 position.set(0);
             }
-            reader.close();
             return document;
         } catch (Exception e) {
             throw new RuntimeException(e);
@@ -77,7 +72,11 @@ public class FasterFilenamesLabelAwareIterator implements LabelAwareIterator {
 
     @Override
     public void reset() {
-        iter=files.iterator();
+        try {
+            iter = Files.newDirectoryStream(folder.toPath()).iterator();
+        } catch(IOException ioe) {
+            throw new RuntimeException("IO Exception when resetting iterator");
+        }
     }
 
     @Override
@@ -113,31 +112,12 @@ public class FasterFilenamesLabelAwareIterator implements LabelAwareIterator {
             return this;
         }
 
-        private void scanFolder(File folderToScan) {
-            File[] files = folderToScan.listFiles();
-            if (files == null || files.length ==0 ) return;
 
-
-            for (File fileLabel: files) {
-                if (fileLabel.isDirectory()) {
-                    scanFolder(fileLabel);
-                } else {
-                    fileList.add(fileLabel);
-                }
-            }
-        }
-
-        public FasterFilenamesLabelAwareIterator build() {
+        public FasterFilenamesLabelAwareIterator build() throws IOException{
             // search for all files in all folders provided
 
-
-            for (File file: foldersToScan) {
-                if (!file.isDirectory()) continue;
-                scanFolder(file);
-            }
-
             LabelsSource source = new LabelsSource(labels);
-            FasterFilenamesLabelAwareIterator iterator = new FasterFilenamesLabelAwareIterator(fileList, source);
+            FasterFilenamesLabelAwareIterator iterator = new FasterFilenamesLabelAwareIterator(foldersToScan.get(0), source);
             iterator.absPath = this.absPath;
 
             return iterator;
