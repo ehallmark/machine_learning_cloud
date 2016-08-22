8431b82bb64c73bc00314ce9e937ae4483978efa
Evan Hallmark <ehallmark@gttgrp.com>
2016-08-22 08:39:11 -0700

    changes

---
+++ src/seeding/CreateTokensInDatabase.java
@@ -0,0 +1,37 @@
+package seeding;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created by ehallmark on 8/22/16.
+ */
+public class CreateTokensInDatabase {
+
+    public static void main(String[] args) throws Exception {
+        Database.setupSeedConn();
+        Database.setupMainConn();
+        ResultSet rs = Database.selectRawPatentNumbers();
+        AtomicInteger cnt = new AtomicInteger(0);
+        long lastTime = System.currentTimeMillis();
+        List<String> patents = new ArrayList<>(1000);
+        while(rs.next()) {
+            patents.add(rs.getString(1));
+            int count = cnt.getAndIncrement();
+            if(count % 10 == 9) {
+                Database.createWordVectorsInDB(patents);
+                patents.clear();
+                if(count >= 999) {
+                    Database.commit();
+                    System.out.println("Time to commit 1000 patents: " + new Double(lastTime - System.currentTimeMillis()) / 1000 + "seconds");
+                    lastTime = System.currentTimeMillis();
+                    cnt.set(0);
+                }
+            }
+        }
+        Database.commit();
+        Database.close();
+    }
+}
