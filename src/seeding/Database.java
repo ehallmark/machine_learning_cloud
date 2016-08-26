965e52eee078711a38c8729b981d1aa5d6601f1d
Evan Hallmark <ehallmark@gttgrp.com>
2016-08-26 11:01:54 -0700

    changes

--- src/seeding/Database.java
+++ src/seeding/Database.java
@@ -32,7 +32,7 @@ public class Database {
 	private static final String selectDateStatement = "SELECT pub_date FROM last_vectors_ingest WHERE program_name=?";
 	private static final String selectVectorsStatement = "SELECT pub_doc_number, description_vectors,claims_vectors FROM patent_vectors WHERE pub_doc_number=ANY(?)";
 	//private static final String selectAllVectorsStatement = "SELECT pub_doc_number, vector FROM raw_patents";
-	private static final String selectSingleVectorStatement = "SELECT description_vectors::real[],claims_vectors::real[][] FROM patent_vectors WHERE pub_doc_number=?";
+	private static final String selectSingleVectorStatement = "SELECT description_vectors,claims_vectors FROM patent_vectors WHERE pub_doc_number=?";
 	private static final String selectAllCandidateSets = "SELECT name, id FROM candidate_sets";
 	private static final String selectPatentNumbersByAssignee = "select distinct doc_number from (select distinct on (p.doc_number) p.doc_number,name,q.uid from patent_assignment_property_document as p join patent_assignment_assignee as q on (p.assignment_reel_frame=q.assignment_reel_frame) where (p.doc_kind='B1' or p.doc_kind='B2') and upper(name) like upper(?)||'%' order by p.doc_number,q.uid desc) as temp join patent_assignment_assignee as a on (temp.uid=a.uid and upper(a.name) like upper(?)||'%')";
 	private static PreparedStatement updateParagraphVectorStatement;
