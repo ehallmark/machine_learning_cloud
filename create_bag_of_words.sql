d39e0b38bc7b9e2d53bd6927a08b0ab54c0e4cd5
Evan Hallmark <ehallmark@gttgrp.com>
2016-08-29 13:27:47 -0700

    c

--- create_bag_of_words.sql
+++ create_bag_of_words.sql
@@ -1,7 +1,6 @@
 \connect patentdb
 
 CREATE TABLE bag_of_words (
-    name VARCHAR(50) PRIMARY KEY,
-    num_words integer not null,
+    name VARCHAR(20) PRIMARY KEY,
     bow integer[] not null
 );
