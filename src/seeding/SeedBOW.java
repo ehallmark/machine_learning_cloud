a828357fd3c3d79af6367315cc8d22b6291f7f63
Evan Hallmark <ehallmark@gttgrp.com>
2016-08-29 12:24:21 -0700

    changes

--- src/seeding/SeedBOW.java
+++ src/seeding/SeedBOW.java
@@ -60,6 +60,7 @@ public class SeedBOW {
 
         }
 
+        System.out.println("Total number of documents: "+vocabCache.totalNumberOfDocs());
         System.out.println("Total number of words: "+vocabCache.numWords());
         System.out.println("Has word method: + "+vocabCache.containsWord("method")+" count "+vocabCache.wordFrequency("method"));
 
@@ -70,15 +71,23 @@ public class SeedBOW {
         List<String> words = vocabCache.vocabWords().stream().filter(vw->!stopWords.contains(vw.getLabel())||vw.getElementFrequency()>= Constants.DEFAULT_MIN_WORD_FREQUENCY).map(vw->vw.getLabel()).collect(Collectors.toList());
         SequenceIterator<VocabWord> sequenceIterator = BuildParagraphVectors.createSequenceIterator(iterator);
         Integer[] counts = new Integer[words.size()];
+        Float[] tfidfCounts = new Float[words.size()];
         while(sequenceIterator.hasMoreSequences()) {
             Sequence<VocabWord> sequence = sequenceIterator.nextSequence();
             String name = sequence.getSequenceLabel().getLabel();
             Arrays.fill(counts, 0);
+            Arrays.fill(tfidfCounts, 0);
+            Map<Integer,Integer> indicesToCheck = new HashMap<>();
             for(VocabWord vw : sequence.getElements()) {
                 int idx = words.indexOf(vw.getLabel());
-                if(idx >= 0) counts[idx] = Math.round((int)vw.getElementFrequency());
+                if(idx >= 0) {
+                    counts[idx]++;
+                    indicesToCheck.put(idx,(int)Math.round(vw.getElementFrequency()));
+                }
             }
-            System.out.println(name+" => "+Arrays.toString(counts));
+            indicesToCheck.entrySet().forEach(entry->{
+                tfidfCounts[entry.getKey()]=new Float(Math.log(new Double(vocabCache.totalNumberOfDocs())/entry.getValue()));
+            });
         }
         // update bag of words
     }
