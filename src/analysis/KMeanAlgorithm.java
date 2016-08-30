9014a1cedf0e1291729f10ae8557c416343c4f61
Evan Hallmark <ehallmark@gttgrp.com>
2016-08-30 08:03:10 -0700

    kmeans

--- src/analysis/KMeanAlgorithm.java
+++ src/analysis/KMeanAlgorithm.java
@@ -217,7 +217,7 @@ public class KMeanAlgorithm
     public static void main(String[] args) throws Exception
     {
 
-        SimilarPatentFinder finder = new SimilarPatentFinder(null, new File("candidateSets/2"),"name");
+        SimilarPatentFinder finder = new SimilarPatentFinder(null, new File("candidateSets/598"),"ETSI");
         initialize(finder.getPatentList().stream().map(p->p.getVector()).collect(Collectors.toList()));
         kMeanCluster(finder.getPatentList().stream().map(f->f.getVector()).iterator());
 
