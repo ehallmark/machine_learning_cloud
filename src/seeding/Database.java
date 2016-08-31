e83c40f2381978b647862f585eb7d1eadb7de4c9
Evan Hallmark <ehallmark@gttgrp.com>
2016-08-31 11:46:42 -0700

    randomness

--- src/seeding/Database.java
+++ src/seeding/Database.java
@@ -376,6 +376,7 @@ public class Database {
 
 	public static Map<String, List<String>> getCompDBMap() throws SQLException {
 		Database.setupCompDBConn();
+		Database.setupSeedConn();
 		Map<String, List<String>> patentToTechnologyHash = new HashMap<>();
 		PreparedStatement ps = compDBConn.prepareStatement("SELECT array_agg(distinct t.id) as technologies, array_agg(distinct (reel||':'||frame)) AS reelframes, r.deal_id FROM recordings as r inner join deals_technologies as dt on (r.deal_id=dt.deal_id) INNER JOIN technologies AS t ON (t.id=dt.technology_id)  WHERE inactive='f' AND asset_count < 25 AND r.deal_id IS NOT NULL AND t.name is not null AND t.id!=ANY(?) GROUP BY r.deal_id");
 		ps.setArray(1, compDBConn.createArrayOf("int4",badTech.toArray()));
@@ -415,7 +416,7 @@ public class Database {
 		}
 		rs.close();
 		ps.close();
-		compDBConn.close();
+		Database.close();
 		return patentToTechnologyHash;
 	}
 
