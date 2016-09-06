97ead23201bc4a6dea401d10ab854773b6f98ed5
Evan Hallmark <ehallmark@gttgrp.com>
2016-09-06 12:06:04 -0700

    debug

--- src/analysis/SimilarPatentFinder.java
+++ src/analysis/SimilarPatentFinder.java
@@ -202,17 +202,15 @@ public class SimilarPatentFinder {
                         }
                     }
 
+                } else {
+                    List<String> data = newTok.getValue().stream().filter(s -> nGramCounts.containsKey(s)).map(s -> new WordFrequencyPair<>(s, nGramCounts.get(s).get())).sorted().map(p -> p.getFirst()).collect(Collectors.toList());
+                    for (int i = 0; i < data.size() - 1; i++) {
+                        String toRemove = data.get(i);
+                        if (nGramCounts.containsKey(toRemove)) nGramCounts.remove(toRemove);
+                    }
+                    if(!data.isEmpty())nGramCounts.get(data.get(data.size() - 1)).set(stemValue);
                 }
             }
-        }
-        for(Map.Entry<String,Set<String>> newTok : newToks.entrySet()) {
-            // remove all but the best ones and set equal to total stemmed count
-            List<String> data = newTok.getValue().stream().filter(s -> nGramCounts.containsKey(s)).map(s -> new WordFrequencyPair<>(s, nGramCounts.get(s).get())).sorted().map(p -> p.getFirst()).collect(Collectors.toList());
-            for (int i = 0; i < data.size() - 1; i++) {
-                String toRemove = data.get(i);
-                if (nGramCounts.containsKey(toRemove)) nGramCounts.remove(toRemove);
-            }
-            nGramCounts.get(data.get(data.size() - 1)).set(stemmedCounts.get(newTok.getKey()).get());
         }
     }
 
