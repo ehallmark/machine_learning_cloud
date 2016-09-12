098408b23408f283fca772f54cf2ad02f8faad9c
Evan Hallmark <ehallmark@gttgrp.com>
2016-09-11 18:02:56 -0700

    changeS

--- src/analysis/SimilarPatentFinder.java
+++ src/analysis/SimilarPatentFinder.java
@@ -224,7 +224,7 @@ public class SimilarPatentFinder {
                     // expand
                     for (Quadruple<double[][], List<Patent>, String, String> result : node.getData().getExpansions()) {
                         if (result.second().size() <= 1) {
-                            leaves.add(node.addChild(new KMeansCalculator(result.third(), result.fourth(), result.second()));
+                            leaves.add(node.addChild(new KMeansCalculator(result.third(), result.fourth(), result.second())));
                         } else {
                             TreeNode<KMeansCalculator> child = node.addChild(new KMeansCalculator(result.third(), result.fourth(), result.first(), result.second(), vocab, result.second().size(), numClusters, sampleSize, iterations, n, numPredictions, equal, rand));
                             if(i.get()==depth-1) preLeaves.add(child);
