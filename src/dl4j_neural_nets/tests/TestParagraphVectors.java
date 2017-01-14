caa4eacde41af8e6149c7c5b752653fd4fa278e9
Evan Hallmark <ehallmar@usc.edu>
2017-01-14 07:06:04 -0800

    changes

--- src/dl4j_neural_nets/tests/TestParagraphVectors.java
+++ src/dl4j_neural_nets/tests/TestParagraphVectors.java
@@ -10,12 +10,15 @@ import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
 import org.deeplearning4j.models.paragraphvectors.ParagraphVectors;
 import org.deeplearning4j.models.sequencevectors.interfaces.SequenceIterator;
 import org.deeplearning4j.models.word2vec.VocabWord;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.ops.transforms.Transforms;
 import seeding.Constants;
 import tools.Emailer;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 /**
@@ -26,43 +29,44 @@ public class TestParagraphVectors {
         int numEpochs = 3;
         File testFile = new File("testFile.pvectors");
         List<String> patents = Arrays.asList(
-                    "9111371",
-                    "9349219",
-                    "8993027",
-                    "8940351" ,
-                    "8956678" ,
-                    "9144251" ,
-                    "9101161" ,
-                    "8956677" ,
-                    "8962058" ,
-                    "9101160" ,
-                    "8945652" ,
-                    "8940350" ,
-                    "9118358" ,
-                    "9148795" ,
-                    "9078084" ,
-                    "9119220" ,
-                    "9125093" ,
-                    "9130810" ,
-                    "8983468" ,
-                    "8995466" ,
-                    "9049722" ,
-                    "8989084" ,
-                    "9369943" ,
-                    "9338767" ,
-                    "9318428" ,
-                    "9071279" ,
-                    "9152902" ,
-                    "9231201" ,
-                    "9177602" ,
-                    "9369604" ,
-                    "9030299" ,
-                    "8988055" ,
-                    "9043608" ,
-                    "9008692",
-                    "9195661"
+                "9111371",
+                "9349219",
+                "8993027",
+                "8940351",
+                "8956678",
+                "9144251",
+                "9101161",
+                "8956677",
+                "8962058",
+                "9101160",
+                "8945652",
+                "8940350",
+                "9118358",
+                "9148795",
+                "9078084",
+                "9119220",
+                "9125093",
+                "9130810",
+                "8983468",
+                "8995466",
+                "9049722",
+                "8989084",
+                "9369943",
+                "9338767",
+                "9318428",
+                "9071279",
+                "9152902",
+                "9231201",
+                "9177602",
+                "9369604",
+                "9030299",
+                "8988055",
+                "9043608",
+                "9008692",
+                "9195661"
         );
-        SequenceIterator<VocabWord> sentenceIterator = DatabaseIteratorFactory.PatentParagraphSamplingSequenceIterator(numEpochs,patents);
+
+        SequenceIterator<VocabWord> sentenceIterator = DatabaseIteratorFactory.PatentParagraphSamplingSequenceIterator(numEpochs, patents);
         ParagraphVectors net = new ParagraphVectors.Builder()
                 .seed(41)
                 .batchSize(1000)
@@ -85,7 +89,7 @@ public class TestParagraphVectors {
                 .sequenceLearningAlgorithm(new DBOW<>())
                 .tokenizerFactory(new MyTokenizerFactory())
                 .setVectorsListeners(Arrays.asList(
-                        new CustomWordVectorListener(testFile,"Paragraph Vectors All Paragraphs",5000000,null,"7455590","claim","alkali_metal","device","femto","finance","touchscreen","smartphone","internet","semiconductor","artificial","intelligence")
+                        new CustomWordVectorListener(testFile, "Paragraph Vectors All Paragraphs", 5000000, null, "7455590", "claim", "alkali_metal", "device", "femto", "finance", "touchscreen", "smartphone", "internet", "semiconductor", "artificial", "intelligence")
                 ))
                 .iterate(sentenceIterator)
                 .build();
@@ -93,20 +97,22 @@ public class TestParagraphVectors {
         WordVectorSerializer.writeParagraphVectors(net, testFile.getAbsolutePath());
 
 
-        ParagraphVectors sentencesModel = ParagraphVectorModel.loadModel(testFile.getAbsolutePath());
+        ParagraphVectors vectors = ParagraphVectorModel.loadModel(testFile.getAbsolutePath());
 
-        // Evaluate model
-        String stats = new ModelEvaluator().evaluateWordVectorModel(sentencesModel.getLookupTable(), "Sentences Model");
-        System.out.println(stats);
-        new Emailer(stats);
 
-        for(int i = 0; i < 14; i++) {
-            ParagraphVectors claimModel = ParagraphVectorModel.loadModel(ParagraphVectorModel.claimsParagraphVectorFile.getAbsolutePath()+i);
+        INDArray inferredVectorA = vectors.inferVector("This is my world .");
+        INDArray inferredVectorA2 = vectors.inferVector("This is my world .");
+        INDArray inferredVectorB = vectors.inferVector("This is my way .");
 
-            // Evaluate model
-            stats = new ModelEvaluator().evaluateWordVectorModel(claimModel.getLookupTable(), "Claim Model [Epoch "+i+"]");
-            System.out.println(stats);
-            new Emailer(stats);
-        }
+        // high similarity expected here, since in underlying corpus words WAY and WORLD have really close context
+        System.out.println("Cosine similarity A/B: " + Transforms.cosineSim(inferredVectorA, inferredVectorB));
+
+        // equality expected here, since inference is happening for the same sentences
+        System.out.println("Cosine similarity A/A2: " + Transforms.cosineSim(inferredVectorA, inferredVectorA2));
+
+        Collection<String> similar = vectors.nearestLabels(vectors.getLookupTable().vector("9195661"), 10);
+
+        System.out.println("Similar lables to 9195661: " + String.join(", ", similar));
     }
+
 }
