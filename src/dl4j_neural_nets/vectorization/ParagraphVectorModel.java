5b873c67c7701cff30ea656a470e379838e62756
Evan Hallmark <ehallmark@gttgrp.com>
2017-01-17 11:40:11 -0800

    change

--- src/dl4j_neural_nets/vectorization/ParagraphVectorModel.java
+++ src/dl4j_neural_nets/vectorization/ParagraphVectorModel.java
@@ -189,7 +189,7 @@ public class ParagraphVectorModel {
 
 
     public static ParagraphVectors loadParagraphsModel() throws IOException {
-        return loadModel(allParagraphsModelFile.getAbsolutePath()+250000000);
+        return loadModel(allParagraphsModelFile.getAbsolutePath());
     }
 
     public static ParagraphVectors loadModel(String path) throws IOException {
