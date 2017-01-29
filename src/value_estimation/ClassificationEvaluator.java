610c37ff9522ec87869be3a20fc98c7d7c6402a4
Evan Hallmark <ehallmar@usc.edu>
2017-01-29 08:31:13 -0800

    changes

--- src/value_estimation/ClassificationEvaluator.java
+++ src/value_estimation/ClassificationEvaluator.java
@@ -8,13 +8,16 @@ import org.deeplearning4j.models.word2vec.VocabWord;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.ops.transforms.Transforms;
 import seeding.Database;
+import server.tools.excel.ExcelWritable;
 import tools.PortfolioList;
 
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.ObjectOutputStream;
+import java.time.LocalDate;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by Evan on 1/27/2017.
@@ -32,82 +35,70 @@ public class ClassificationEvaluator extends Evaluator {
     }
 
     private static Map<String,Double> runModel(ParagraphVectors paragraphVectors){
+        final LocalDate START_DATE = LocalDate.of(2010,1,1);
+        final int SAMPLE_SIZE = 500;
         System.out.println("Starting to load classification evaluator...");
         List<String> classifications = new ArrayList<>(Database.getClassCodes());
         List<String> patents = new ArrayList<>(Database.getValuablePatents());
+        Map<LocalDate,Set<String>> dateToPatentsMap = Collections.synchronizedMap((Map<LocalDate,Set<String>>)Database.tryLoadObject(new File("pubdate_to_patent_map.jobj")));
+        SortedSet<LocalDate> sortedDates = new TreeSet<>(dateToPatentsMap.keySet());
+        sortedDates.removeIf(date->date.compareTo(START_DATE)>=0);
         Collection<String> assignees = Database.getAssignees();
         WeightLookupTable<VocabWord> lookupTable = paragraphVectors.getLookupTable();
-        // sort patents
-        List<Set<String>> similarWordList = new ArrayList<>(classifications.size());
-        Collections.sort(patents);
-        final int periodSize = 10000;
-        final int numPeriods = 30;
-        patents=patents.subList(patents.size()-(periodSize*numPeriods),patents.size());
-        double threshHold = 0.90;
-        List<List<Double>> classScores = new ArrayList<>(patents.size()/periodSize+1);
+        SimilarPatentFinder classCodeFinder = new SimilarPatentFinder(Database.getClassCodes(),null,lookupTable);
 
-        // get relevance to each class code
-        for(int c = 0; c < classifications.size(); c++) {
-            String clazz = classifications.get(c);
-            Set<String> similar = new HashSet<>(paragraphVectors.similarWordsInVocabTo(clazz,threshHold));
-            System.out.println("Similar words to "+clazz+": "+similar.size());
-            similarWordList.add(similar);
-        }
-        for(int i = 0; i < patents.size()-periodSize; i+= periodSize) {
-            List<Double> scores = new ArrayList<>(classifications.size());
+        List<Map<String,Double>> classScores = new ArrayList<>();
+
+
+        for(LocalDate date : sortedDates) {
+
+            System.out.println("Starting date: "+date);
+            List<String> patentGroup = new ArrayList<>(dateToPatentsMap.get(date));
+            // sample patentGroup
+            Collections.shuffle(patentGroup,new Random(System.currentTimeMillis()));
+            List<String> sample = patentGroup.subList(0,Math.max(patentGroup.size(),SAMPLE_SIZE));
+            SimilarPatentFinder sampleFinder = new SimilarPatentFinder(sample,null,lookupTable);
+            INDArray avg = SimilarPatentFinder.computeAvg(sampleFinder.getPatentList());
+            Map<String,Double> scores = new HashMap<>();
+            if(avg!=null) {
+                classCodeFinder.getPatentList().forEach(classCode->{
+                    scores.put(classCode.getName(),1.0+Transforms.cosineSim(classCode.getVector(),avg));
+                });
+            }
             classScores.add(scores);
-            for(int c = 0; c < classifications.size(); c++) {
-                scores.add(0.0);
-            }
-            List<String> patentBatch = patents.subList(i,i+periodSize);
-
-            // get relevance to each class code
-            for(int c = 0; c < classifications.size(); c++) {
-                String clazz = classifications.get(c);
-                Set<String> similar = similarWordList.get(c);
-                if(similar!=null&&!similar.isEmpty()) {
-                    double score = 0.0;
-                    for(String patent : patentBatch) {
-                        if(similar.contains(patent)) score+=1.0;
-                    }
-                    System.out.println("Score for "+clazz+": "+score);
-                    scores.set(c,score);
-                }
-            }
         }
+
         System.out.println("Calculating analysis for evaluator...");
         Map<String,Double> model = new HashMap<>();
         for(String patent: patents) {
             model.put(patent,0.0);
         }
         for(int i = 0; i < classScores.size()-1; i+=2) {
-            List<Double> scoreT = classScores.get(i);
-            List<Double> scoreT2 = classScores.get(i+1);
+            Map<String,Double> scoreT = classScores.get(i);
+            Map<String,Double> scoreT2 = classScores.get(i+1);
+
             for(int j = 0; j < scoreT.size(); j++) {
-                Double p1 = scoreT.get(j);
-                Double p2 = scoreT2.get(j);
-                if(p1>threshHold) {
+                String clazz = classifications.get(j);
+                Double p1 = scoreT.get(clazz);
+                Double p2 = scoreT2.get(clazz);
+                if(p1>0) {
                     double score = (p2-p1)/p1;
-                    String clazz = classifications.get(j);
                     model.put(clazz,model.get(clazz)+score*Math.pow(i,1.5));
                 }
             }
         }
-        SimilarPatentFinder classCodeFinder = new SimilarPatentFinder(Database.getClassCodes(),null,lookupTable);
         patents.forEach(patent->{
+            System.out.println("Patent: "+patent);
             double score = 0.0;
             double toDivide = 0.0;
             INDArray patentVec = lookupTable.vector(patent);
             if(patentVec!=null) {
-                PortfolioList list = classCodeFinder.findSimilarPatentsTo(patent, patentVec, new HashSet<>(), threshHold, 10, PortfolioList.Type.class_codes);
-                for(String clazz : list.getPortfolioAsStrings()) {
-                    INDArray classVec = lookupTable.vector(clazz);
-                    if(classVec!=null&&patentVec!=null) {
-                        double sim = Transforms.cosineSim(classVec, patentVec);
-                        if (sim>threshHold){
-                            score += (sim*model.get(clazz));
-                            toDivide+=sim;
-                        }
+                PortfolioList list = classCodeFinder.findSimilarPatentsTo(patent, patentVec, new HashSet<>(), 0.9, 10, PortfolioList.Type.class_codes);
+                for(ExcelWritable x : list.getPortfolio()) {
+                    String clazz = x.getName();
+                    if(clazz!=null) {
+                        score += (x.getSimilarity()*model.get(clazz));
+                        toDivide+=x.getSimilarity();
                     }
                 }
             }
