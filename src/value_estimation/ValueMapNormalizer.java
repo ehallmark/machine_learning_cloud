7ebb61243a5a8f14f7cac7f04ae9895ecdeab35a
Evan Hallmark <ehallmar@usc.edu>
2017-01-30 04:10:38 -0800

    value map bug

--- src/value_estimation/ValueMapNormalizer.java
+++ src/value_estimation/ValueMapNormalizer.java
@@ -1,9 +1,11 @@
 package value_estimation;
 
 import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.math3.distribution.NormalDistribution;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -13,15 +15,24 @@ import java.util.stream.Collectors;
  * Created by Evan on 1/27/2017.
  */
 public class ValueMapNormalizer {
+    private static final NormalDistribution normalDistribution = new NormalDistribution(0d,1d);
+
     public static void normalizeToRange(Map<String,Double> model, double start, double end) {
-        List<Double> values = model.entrySet().stream().map(e->e.getValue()).collect(Collectors.toList());
+        List<String> keys = new ArrayList<>(model.keySet());
+        List<Double> values = new ArrayList<>(keys.size());
+        for(String key : keys) {
+            values.add(model.get(key));
+        }
         INDArray array = Nd4j.create(ArrayUtils.toPrimitive(values.toArray(new Double[values.size()])));
         double stdDev = Math.sqrt(array.varNumber().doubleValue());
         double mean = array.meanNumber().doubleValue();
-        double min = mean-3.0*stdDev;
-        double max = mean+4.0*stdDev;
-        model.keySet().forEach(key->{
-            model.put(key,Math.max(Math.min(end,start+((model.get(key)-min)/(max-min))*(end-start)),start));
-        });
+        array.subi(mean).divi(stdDev);
+        // array is now standard normal
+
+        for(int i = 0; i < keys.size(); i++) {
+            String key = keys.get(i);
+            double value = normalDistribution.cumulativeProbability(array.getDouble(i))*(end-start)+start;
+            model.put(key,value);
+        }
     }
 }
