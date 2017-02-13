9f2395ec17340093782d619a94d90b65d9ce6190
Evan Hallmark <ehallmark@gttgrp.com>
2017-02-13 11:54:39 -0800

    changes

--- src/analysis/R/Test.java
+++ src/analysis/R/Test.java
@@ -1,8 +1,33 @@
 package analysis.R;
 
+import org.rosuda.JRI.Rengine;
+
 /**
  * Created by ehallmark on 2/13/17.
  */
 public class Test {
 
+    public static void main(String[] args) {
+
+        // Create an R vector in the form of a string.
+        String javaVector = "c(1,2,3,4,5)";
+
+        // Start Rengine.
+        Rengine engine = new Rengine(new String[] { "--no-save" }, false, null);
+
+        // The vector that was created in JAVA context is stored in 'rVector' which is a variable in R context.
+        engine.eval("rVector=" + javaVector);
+
+        //Calculate MEAN of vector using R syntax.
+        engine.eval("meanVal=mean(rVector)");
+
+        //Retrieve MEAN value
+        double mean = engine.eval("meanVal").asDouble();
+
+        //Print output values
+        System.out.println("Mean of given vector is=" + mean);
+
+
+        System.exit(0);
+    }
 }
