09b9c720d9e91d75149c063dec5fdc6ad1c5a7cd
Evan Hallmark <ehallmark@gttgrp.com>
2017-02-16 11:28:58 -0800

    chagnes

--- src/server/highcharts/AbstractChart.java
+++ src/server/highcharts/AbstractChart.java
@@ -62,10 +62,10 @@ public abstract class AbstractChart {
    }
 
     public void attachDoubleClickToForm(String inputID) {
-        options
+        /*options
                 .setPlotOptions(new PlotOptionsChoice().setSeries(new PlotOptions()
-                .setPoint(new PointOptions().setEvents(new Events().setClick(new Function().setFunction("$(\"#"+inputID+"\").val(this.name); $(\"#" + inputID + "\").closest(\"form\").submit();"))))));
-
+                .setPoint(new PointOptions().setEvents(new Events().setDblclick(new Function().setFunction("$(\"#"+inputID+"\").val(this.name); $(\"#" + inputID + "\").closest(\"form\").submit();"))))));
+        */
     }
 
     @Override
