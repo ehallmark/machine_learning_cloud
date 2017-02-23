0954f06594fb7445fadbc7a53a22e9305ff1a403
Evan Hallmark <ehallmark@gttgrp.com>
2017-02-23 10:52:51 -0800

    small changes

--- src/analysis/genetics/keyword_analysis/KeywordSolution.java
+++ src/analysis/genetics/keyword_analysis/KeywordSolution.java
@@ -133,7 +133,7 @@ public class KeywordSolution implements Solution {
         if(tech.containsKey(word)&&GLOBAL_WORD_FREQUENCY_MAP.containsKey(word)) {
             score+=tech.get(word)*-Math.log(GLOBAL_WORD_FREQUENCY_MAP.get(word));
         }
-        return score/Math.pow(numUnderscores(word),2);
+        return score;
     }
 
     private static double numUnderscores(String word) {
--- src/analysis/genetics/keyword_analysis/TechnologyPredictionAlgorithm.java
+++ src/analysis/genetics/keyword_analysis/TechnologyPredictionAlgorithm.java
@@ -11,11 +11,11 @@ public class TechnologyPredictionAlgorithm {
 
     public static void main(String[] args) {
         int populationSize = 250;
-        double samplingProbability = 0.05;
+        double samplingProbability = 0.01;
         int numThreads = 30;
         int numEpochs = 50000;
-        double mutationRate = 0.5;
-        double crossoverRate = 0.02;
+        double mutationRate = 0.3;
+        double crossoverRate = 0.01;
         Map<String,Map<String,Double>> techFrequencyMap = KeywordSolution.getTechnologyToWordFrequencyMap();
         GeneticAlgorithm algorithm = new GeneticAlgorithm(new KeywordSolutionCreator(techFrequencyMap, samplingProbability),populationSize, new KeywordListener(),numThreads);
         algorithm.simulate(numEpochs,mutationRate,crossoverRate);
