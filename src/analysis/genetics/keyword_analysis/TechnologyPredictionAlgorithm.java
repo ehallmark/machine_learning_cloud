b1e9efc6a551640be766ff019284cbfe55e816dc
Evan Hallmark <ehallmar@usc.edu>
2017-02-24 07:37:46 -0800

    changes

--- src/analysis/genetics/keyword_analysis/TechnologyPredictionAlgorithm.java
+++ src/analysis/genetics/keyword_analysis/TechnologyPredictionAlgorithm.java
@@ -10,12 +10,12 @@ import java.util.Map;
 public class TechnologyPredictionAlgorithm {
 
     public static void main(String[] args) {
-        int populationSize = 250;
-        double samplingProbability = 0.01;
+        int populationSize = 500;
+        double samplingProbability = 0.05;
         int numThreads = 30;
         int numEpochs = 50000;
-        double mutationRate = 0.3;
-        double crossoverRate = 0.01;
+        double mutationRate = 0.1;
+        double crossoverRate = 0.1;
         Map<String,Map<String,Double>> techFrequencyMap = KeywordSolution.getTechnologyToWordFrequencyMap();
         GeneticAlgorithm algorithm = new GeneticAlgorithm(new KeywordSolutionCreator(techFrequencyMap, samplingProbability),populationSize, new KeywordListener(),numThreads);
         algorithm.simulate(numEpochs,mutationRate,crossoverRate);
