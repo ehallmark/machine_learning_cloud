aa703637729e103e9a384ffeaa6f1630e2df1420
Evan Hallmark <ehallmark@gttgrp.com>
2017-02-24 12:10:56 -0800

    changes

--- src/analysis/genetics/SolutionCreator.java
+++ src/analysis/genetics/SolutionCreator.java
@@ -1,8 +1,10 @@
 package analysis.genetics;
 
+import java.util.Collection;
+import java.util.List;
 /**
  * Created by Evan on 2/19/2017.
  */
 public interface SolutionCreator {
-    Solution nextRandomSolution();
+    Collection<Solution> nextRandomSolutions(int n);
 }
--- src/analysis/genetics/keyword_analysis/KeywordSolutionCreator.java
+++ src/analysis/genetics/keyword_analysis/KeywordSolutionCreator.java
@@ -21,33 +21,57 @@ public class KeywordSolutionCreator implements SolutionCreator {
         techToWordMap=allWordMap;
     }
     @Override
-    public Solution nextRandomSolution() {
-        Map<String,List<Word>> randomTechToWordMap = new HashMap<>();
-        Map<String,Set<String>> alreadyAddedMap = new HashMap<>();
+    public Collection<Solution> nextRandomSolutions(int num) {
+        List<Map<String,List<Word>>> randomTechToWordMapList = new ArrayList<>(num);
+        List<Map<String,Set<String>>> alreadyAddedMapList = new ArrayList<>(num);
+        for(int i = 0; i < num; i++) {
+            randomTechToWordMapList.add(new HashMap<>(techToWordMap.size()));
+            alreadyAddedMapList.add(new HashMap<>(techToWordMap.size()));
+        }
         System.out.println("Creating random solution...");
         AtomicInteger size = new AtomicInteger(0);
         techToWordMap.forEach((tech,words)->{
-            int samples = Math.max(minWordsPerTechnology,Math.round((float)samplingProbability*words.size()));
-            List<Word> newSet = new ArrayList<>(samples);
-            Set<String> wordSet = new HashSet<>(samples);
-            for(int i = 0; i < samples; i++) {
-                Word randomWord = words.get(ProbabilityHelper.getLowNumberWithMaxUpTo(words.size()));
-                if(wordSet.contains(randomWord.getWord())) {
-                    i--;
-                } else {
-                    newSet.add(randomWord);
-                    wordSet.add(randomWord.getWord());
+            for(int solutionNum = 0; solutionNum < num; solutionNum++) {
+                Map<String,List<Word>> randomTechToWordMap = randomTechToWordMapList.get(solutionNum);
+                Map<String,Set<String>> alreadyAddedMap = alreadyAddedMapList.get(solutionNum);
+                int samples = Math.max(minWordsPerTechnology, Math.round((float) samplingProbability * words.size()));
+                List<Word> newSet = new ArrayList<>(samples);
+                Set<String> wordSet = new HashSet<>(samples);
+                for (int i = 0; i < samples; i++) {
+                    int randIdx = ProbabilityHelper.getLowNumberWithMaxUpTo(words.size());
+                    Word randomWord = words.get(randIdx);
+                    if (wordSet.contains(randomWord.getWord())) {
+                        boolean ended = false;
+                        randIdx--;
+                        while(randIdx>=0) {
+                            randomWord=words.get(randIdx);
+                            if(!wordSet.contains(randomWord.getWord())) {
+                                wordSet.add(randomWord.getWord());
+                                newSet.add(randomWord);
+                                ended=true;
+                            }
+                            randIdx--;
+                        }
+                        if(!ended) {
+                            i--;
+                        }
+                    } else {
+                        newSet.add(randomWord);
+                        wordSet.add(randomWord.getWord());
+                    }
                 }
-                System.out.println("Words found so far: "+newSet.size());
+                size.getAndAdd(newSet.size());
+                randomTechToWordMap.put(tech, newSet);
+                alreadyAddedMap.put(tech, wordSet);
             }
-            size.getAndAdd(newSet.size());
-            randomTechToWordMap.put(tech,newSet);
-            alreadyAddedMap.put(tech,wordSet);
         });
-        Solution solution = new KeywordSolution(randomTechToWordMap,alreadyAddedMap,minWordsPerTechnology);
-        solution.calculateFitness();
-        System.out.println("Solution Size: "+size.get());
-        System.out.println("Solution score: "+solution.fitness());
-        return solution;
+        Collection<Solution> solutions = new HashSet<>(num);
+        for(int i = 0; i < num; i++) {
+            Solution solution = new KeywordSolution(randomTechToWordMapList.get(i),alreadyAddedMapList.get(i),minWordsPerTechnology);
+            solution.calculateFitness();
+            solutions.add(solution);
+            System.out.println("Solution score: "+solution.fitness());
+        }
+        return solutions;
     }
 }
