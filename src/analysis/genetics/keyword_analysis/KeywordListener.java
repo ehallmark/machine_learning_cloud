28744e90cfbce9c9cbec402d65a83c1a724a1518
Evan Hallmark <ehallmar@usc.edu>
2017-02-24 08:52:02 -0800

    changes

--- src/analysis/genetics/keyword_analysis/KeywordListener.java
+++ src/analysis/genetics/keyword_analysis/KeywordListener.java
@@ -5,6 +5,7 @@ import analysis.genetics.Solution;
 
 import java.util.Map;
 import java.util.Set;
+import java.util.SortedSet;
 
 /**
  * Created by ehallmark on 2/23/17.
@@ -13,7 +14,7 @@ public class KeywordListener implements Listener {
     @Override
     public void print(Solution solution) {
         KeywordSolution keywordSolution = (KeywordSolution)solution;
-        Map<String,Set<String>> map = keywordSolution.getTechnologyToWordsMap();
+        Map<String,SortedSet<Word>> map = keywordSolution.getTechnologyToWordsMap();
         map.keySet().stream().sorted().limit(20).forEach(tech->{
             System.out.println("Top keywords for "+tech+": "+String.join("; ",keywordSolution.topKeywordsFromTechnology(tech,3)));
         });
