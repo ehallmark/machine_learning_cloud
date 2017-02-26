5579765d79d5b935aa19324f10fe9b5807442b68
Evan Hallmark <ehallmar@usc.edu>
2017-02-26 14:26:15 -0800

    changes

--- src/analysis/genetics/lead_development/CompanySolution.java
+++ src/analysis/genetics/lead_development/CompanySolution.java
@@ -11,6 +11,7 @@ import seeding.Database;
 import value_estimation.PortfolioSizeEvaluator;
 
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 /**
@@ -20,10 +21,16 @@ public class CompanySolution implements Solution {
     private double fitness;
     private List<Map.Entry<String,Double>>  companyScores;
     private List<Attribute> attributes;
+    private Set<String> companyNames;
 
     public CompanySolution( List<Map.Entry<String,Double>>  companyScores, List<Attribute> attributes) {
+        this(companyScores,attributes,new HashSet<>());
+    }
+
+    private CompanySolution(List<Map.Entry<String,Double>>  companyScores, List<Attribute> attributes, Set<String> companyNames) {
         this.companyScores=companyScores;
         this.attributes=attributes;
+        this.companyNames=companyNames;
         fitness=companyScores.stream().collect(Collectors.averagingDouble(e->e.getValue()));
     }
 
@@ -42,21 +49,53 @@ public class CompanySolution implements Solution {
     @Override
     public Solution mutate() {
         List<Map.Entry<String,Double>> newScores = new ArrayList<>(companyScores);
-        int rand = ProbabilityHelper.getHighNumberWithMaxUpTo(newScores.size());
-        newScores.remove(rand);
 
-        String randomAssignee = Database.getRandomAssignee();
-        double score = getScoreFromCompanyAndAttrs(randomAssignee,attributes);
-        newScores.add(Maps.immutableEntry(randomAssignee,score));
+        for(int i = 0; i < Math.max(1,companyScores.size()/10); i++) {
+            int rand = ProbabilityHelper.getHighNumberWithMaxUpTo(newScores.size());
+            newScores.remove(rand);
+        }
+
+        fillAssigneesBack(newScores,this.companyNames);
+
         Collections.sort(newScores,(e1,e2)->(e2.getValue().compareTo(e1.getValue())));
-        return new CompanySolution(newScores,attributes);
+        return new CompanySolution(newScores,attributes,companyNames);
+    }
+
+    private void fillAssigneesBack(List<Map.Entry<String,Double>> newScores, Set<String> alreadyIncludedSet) {
+        while(newScores.size()<companyScores.size()) {
+            String randomAssignee = Database.getRandomAssignee();
+            if(!alreadyIncludedSet.contains(randomAssignee)) {
+                alreadyIncludedSet.add(randomAssignee);
+                double score = getScoreFromCompanyAndAttrs(randomAssignee,attributes);
+                newScores.add(Maps.immutableEntry(randomAssignee,score));
+            }
+        }
+    }
+    private void addToMap(List<Map.Entry<String,Double>> list, List<Map.Entry<String,Double>> newScores, Set<String> alreadyIncludedSet) {
+        Set<Integer> alreadyAddedMap = new HashSet<>();
+        for(int i = 0; i < list.size()/2; i++) {
+            int num = ProbabilityHelper.getLowNumberWithMaxUpTo(list.size());
+            if(!alreadyAddedMap.contains(new Integer(num))) {
+                alreadyAddedMap.add(new Integer(num));
+                Map.Entry<String,Double> entry = list.get(num);
+                if(!alreadyIncludedSet.contains(entry.getKey())) {
+                    alreadyIncludedSet.add(entry.getKey());
+                    newScores.add(entry);
+                }
+            }
+        }
     }
 
     @Override
-    public Solution crossover(Solution other) {
-        return null;
+    public Solution crossover(Solution _other) {
+        CompanySolution other = (CompanySolution)_other;
+        List<Map.Entry<String,Double>> newScores = new ArrayList<>(companyScores.size());
+        addToMap(companyScores,newScores,companyNames);
+        addToMap(other.companyScores,newScores,companyNames);
+        fillAssigneesBack(newScores,companyNames);
+        Collections.sort(newScores,(e1,e2)->(e2.getValue().compareTo(e1.getValue())));
+        return new CompanySolution(newScores,attributes,companyNames);
     }
-
     @Override
     public int compareTo(Solution o) {
         return Double.compare(o.fitness(),fitness);
