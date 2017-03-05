a109003a267b007b3cde1f47fd110cf1f29b4571
Evan Hallmark <ehallmar@usc.edu>
2017-03-05 09:42:07 -0800

    debug

--- src/analysis/genetics/keyword_analysis/ProbabilityHelper.java
+++ src/analysis/genetics/keyword_analysis/ProbabilityHelper.java
@@ -10,9 +10,14 @@ import java.util.Random;
 public class ProbabilityHelper {
     private static Random random = new Random(76);
     public static int getLowNumberWithMaxUpTo(int max) {
-        GeometricDistribution dist = new GeometricDistribution(1.0/Math.cbrt(max));
-        int num = Math.max(0, Math.min(dist.inverseCumulativeProbability(random.nextDouble())-1,max-1));
-        return num;
+        try {
+            GeometricDistribution dist = new GeometricDistribution(1.0 / Math.cbrt(max));
+            int num = Math.max(0, Math.min(dist.inverseCumulativeProbability(random.nextDouble()) - 1, max - 1));
+            return num;
+        } catch(Exception e) {
+            e.printStackTrace();
+            System.out.println("Max: "+max);
+        }
     }
 
     public static int getHighNumberWithMaxUpTo(int max){
