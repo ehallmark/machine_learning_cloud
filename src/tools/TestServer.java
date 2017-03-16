0f6262e5a4aa1a75cb15c60ceff8e0c0e9dc7a91
Evan Hallmark <ehallmark@gttgrp.com>
2017-03-16 11:41:07 -0700

    smallc hange

--- src/tools/TestServer.java
+++ src/tools/TestServer.java
@@ -6,7 +6,7 @@ import static spark.Spark.*;
  */
 public class TestServer {
     public static void main(String[] args) {
-        port(8000);
+        port(4567);
         get("/hello",(req,res)->{
             return "Hello World!";
         });
