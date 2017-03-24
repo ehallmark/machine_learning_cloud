06e78d97c601e2a379b0de7816f819fa70a64140
Evan Hallmark <ehallmark@gttgrp.com>
2017-03-24 12:17:40 -0700

    chagnes

--- src/analysis/tech_tagger/TechTaggerNormalizer.java
+++ src/analysis/tech_tagger/TechTaggerNormalizer.java
@@ -35,30 +35,31 @@ public class TechTaggerNormalizer extends TechTagger {
 
     @Override
     public double getTechnologyValueFor(Collection<String> items, String technology, PortfolioList.Type type) {
-        return taggers.stream().collect(Collectors.averagingDouble(tagger->tagger.getTechnologyValueFor(items,technology,type)*tagger.getWeight()));
+        return taggers.stream().filter(tagger->tagger.getWeight()>0).collect(Collectors.averagingDouble(tagger->tagger.getTechnologyValueFor(items,technology,type)*tagger.getWeight()));
     }
 
 
     private List<Pair<String,Double>> technologyHelper(Collection<String> items, PortfolioList.Type type, int n) {
         Map<String,Double> technologyScores = new HashMap<>();
         taggers.forEach(tagger->{
-            List<Pair<String,Double>> data = tagger.getTechnologiesFor(items,type,tagger.size());
-
-            if(data!=null&&data.size()>=10) {
-                // normalize data
-                double mean = data.stream().map(pair->pair.getSecond()).collect(Collectors.averagingDouble(d->d));
-                double stddev = Math.sqrt(data.stream().map(pair->pair.getSecond()).collect(Collectors.summingDouble(d->Math.pow(d-mean,2.0)))/(data.size()-1));
-                if(stddev>0) {
-                    for(Pair<String,Double> pair : data) {
-                        double val = ((pair.getSecond() - mean) / stddev) * tagger.getWeight();
-                        if(val>0) {
-                            if (technologyScores.containsKey(pair.getFirst())) {
-                                technologyScores.put(pair.getFirst(), Math.max(technologyScores.get(pair.getFirst()), val));
+            if(tagger.getWeight()>0) {
+                List<Pair<String, Double>> data = tagger.getTechnologiesFor(items, type, tagger.size());
+                if (data != null && data.size() >= 10) {
+                    // normalize data
+                    double mean = data.stream().map(pair -> pair.getSecond()).collect(Collectors.averagingDouble(d -> d));
+                    double stddev = Math.sqrt(data.stream().map(pair -> pair.getSecond()).collect(Collectors.summingDouble(d -> Math.pow(d - mean, 2.0))) / (data.size() - 1));
+                    if (stddev > 0) {
+                        for (Pair<String, Double> pair : data) {
+                            double val = ((pair.getSecond() - mean) / stddev) * tagger.getWeight();
+                            if (val > 0) {
+                                if (technologyScores.containsKey(pair.getFirst())) {
+                                    technologyScores.put(pair.getFirst(), Math.max(technologyScores.get(pair.getFirst()), val));
+                                } else {
+                                    technologyScores.put(pair.getFirst(), val);
+                                }
                             } else {
-                                technologyScores.put(pair.getFirst(), val);
+                                break; // since everything is sorted
                             }
-                        } else {
-                            break; // since everything is sorted
                         }
                     }
                 }
