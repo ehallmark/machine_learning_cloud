56ef14786d4c7786f2c825585fd887c4c74c9f40
Evan Hallmark <ehallmar@usc.edu>
2017-03-27 14:15:29 -0700

    changes

--- src/analysis/patent_view_api/PatentsViewMonthlyIterator.java
+++ src/analysis/patent_view_api/PatentsViewMonthlyIterator.java
@@ -1,8 +1,88 @@
 package analysis.patent_view_api;
 
+import org.apache.spark.api.java.JavaRDD;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.dataset.api.DataSetPreProcessor;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+
+import java.util.List;
+
 /**
  * Created by Evan on 3/27/2017.
  */
-public class PatentsViewMonthlyIterator {
+public class PatentsViewMonthlyIterator implements DataSetIterator {
+    @Override
+    public DataSet next(int i) {
+        return null;
+    }
 
+    @Override
+    public int totalExamples() {
+        return 0;
+    }
+
+    @Override
+    public int inputColumns() {
+        return 0;
+    }
+
+    @Override
+    public int totalOutcomes() {
+        return 0;
+    }
+
+    @Override
+    public boolean resetSupported() {
+        return false;
+    }
+
+    @Override
+    public boolean asyncSupported() {
+        return false;
+    }
+
+    @Override
+    public void reset() {
+
+    }
+
+    @Override
+    public int batch() {
+        return 0;
+    }
+
+    @Override
+    public int cursor() {
+        return 0;
+    }
+
+    @Override
+    public int numExamples() {
+        return 0;
+    }
+
+    @Override
+    public void setPreProcessor(DataSetPreProcessor dataSetPreProcessor) {
+
+    }
+
+    @Override
+    public DataSetPreProcessor getPreProcessor() {
+        return null;
+    }
+
+    @Override
+    public List<String> getLabels() {
+        return null;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public DataSet next() {
+        return null;
+    }
 }
