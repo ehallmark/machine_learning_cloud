e93d7641514aa71f5acbebc4d9eccf2be072ef4f
Evan Hallmark <ehallmar@usc.edu>
2017-05-05 10:35:23 -0700

    changes

--- src/model_testing/TrainModelsWithLatestGatherData.java
+++ src/model_testing/TrainModelsWithLatestGatherData.java
@@ -15,20 +15,18 @@ import java.util.Map;
 public class TrainModelsWithLatestGatherData {
     public static void main(String[] args) throws Exception{
         Database.initializeDatabase();
-//        SimilarPatentServer.loadLookupTable();
+        SimilarPatentServer.loadLookupTable();
 
         Map<String,Collection<String>> trainingData = SplitModelData.getGatherTechnologyTrainingDataMap();
         Map<String,Collection<String>> testingData = SplitModelData.getGatherTechnologyTestDataMap();
         Map<String,Collection<String>> validationData = SplitModelData.getGatherTechnologyValidationDataMap();
 
         // train gather keyword model
-//        GatherKeywordTechTagger.trainAndSaveLatestModel(trainingData,testingData,validationData);
+        GatherKeywordTechTagger.trainAndSaveLatestModel(trainingData,testingData,validationData);
 
 
         // train gather cpc model
         CPCTagger.trainAndSaveLatestModel(trainingData,testingData,validationData);
-
-
 
     }
 }
