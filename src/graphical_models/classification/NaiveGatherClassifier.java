74c85e4f1c141028c2fe08062d787ab64e5e5db5
Evan Hallmark <ehallmar@usc.edu>
2017-05-14 07:03:19 -0700

    changes

--- src/graphical_models/classification/NaiveGatherClassifier.java
+++ src/graphical_models/classification/NaiveGatherClassifier.java
@@ -126,11 +126,11 @@ public class NaiveGatherClassifier extends ClassificationAttr{
         //graph.addFactorNode(null,cpcNode);
         graph.connectNodes(cpcNode,techNode);
         graph.addFactorNode(null,techNode,cpcNode);
-        graph.addFactorNode(null,techNode);
+        graph.addFactorNode(null,cpcNode);
         //graph.addFactorNode(null,cpcNode);
 
         // learn
-        graph.applyLearningAlgorithm(new ExpectationMaximizationAlgorithm(graph,5d, new BeliefPropagation()),5000);
+        graph.applyLearningAlgorithm(new ExpectationMaximizationAlgorithm(graph,20d, new BeliefPropagation()),5000);
 
         // peek
         //graph.getFactorNodes().forEach(factor->{
