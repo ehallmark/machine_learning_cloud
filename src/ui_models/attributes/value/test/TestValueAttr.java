d9ceb761d2170cf73b9b959be2cb57029cb38b53
Evan Hallmark <ehallmark@gttgrp.com>
2017-05-15 16:39:56 -0700

    Changes

--- src/ui_models/attributes/value/test/TestValueAttr.java
+++ src/ui_models/attributes/value/test/TestValueAttr.java
@@ -15,16 +15,6 @@ import java.util.Map;
  */
 public class TestValueAttr {
     public static void main(String[] args) {
-        Map<String,Float> map = new PageRank.Loader().loadRankTable(PageRankHelper.file);
-        Map<String,Double> doubleMap = new HashMap<>(map.size());
-        map.forEach((k,v)->{
-            if(Database.isPatent(k)) {
-                doubleMap.put(k, v.doubleValue());
-            }
-        });
-        doubleMap.forEach((k,v)->{
-            System.out.println(k+": "+v);
-        });
 
         Database.initializeDatabase();
         ValueAttr attr = new PageRankEvaluator();
