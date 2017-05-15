ae0ca6a120235df7ea5be58d97d34c2bc3da3a94
Evan Hallmark <ehallmark@gttgrp.com>
2017-05-15 11:11:27 -0700

    Changes

---
+++ src/seeding/PrintGatherTechnologiesWithAssets.java
@@ -0,0 +1,18 @@
+package seeding;
+
+import model_testing.SplitModelData;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * Created by ehallmark on 5/1/17.
+ */
+public class PrintGatherTechnologiesWithAssets {
+    public static void main(String[] args) {
+        Map<String,Collection<String>> gatherTechnologies = SplitModelData.getGatherTechnologyTrainingDataMap();
+        System.out.println(String.join("\n",gatherTechnologies.entrySet().stream().map(e->"\""+e.getKey()+"\","+e.getValue()).collect(Collectors.toList())));
+    }
+}
