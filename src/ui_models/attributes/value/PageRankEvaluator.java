1c09957d1381564da2ef9f8b5a57298da32b2dad
Evan Hallmark <ehallmark@gttgrp.com>
2017-05-17 15:43:20 -0700

    hcnages

--- src/ui_models/attributes/value/PageRankEvaluator.java
+++ src/ui_models/attributes/value/PageRankEvaluator.java
@@ -17,7 +17,7 @@ import java.util.stream.Collectors;
  */
 public class PageRankEvaluator extends ValueAttr {
     public PageRankEvaluator() {
-        super(ValueMapNormalizer.DistributionType.Uniform, "Page Rank");
+        super(ValueMapNormalizer.DistributionType.Normal, "Page Rank");
         DateHelper.addScoresToAssigneesFromPatents(Database.getAssignees(), model);
     }
 
