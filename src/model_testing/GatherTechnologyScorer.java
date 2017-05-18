4387a6f790b9b798171b02754595231ef8984c92
Evan Hallmark <ehallmark@gttgrp.com>
2017-05-17 17:10:31 -0700

    hcnages

--- src/model_testing/GatherTechnologyScorer.java
+++ src/model_testing/GatherTechnologyScorer.java
@@ -4,6 +4,7 @@ import ui_models.attributes.classification.ClassificationAttr;
 import ui_models.portfolios.PortfolioList;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -29,10 +30,12 @@ public class GatherTechnologyScorer {
     }
 
     public static double scoreAssets(ClassificationAttr model, String tech, Collection<String> assets, int numPredictions) {
-       return ((double)(assets.stream().map(asset->{
-           Collection<String> predictions = model.attributesFor(PortfolioList.asList(asset, PortfolioList.Type.patents),numPredictions).stream().map(pair->pair.getFirst()).collect(Collectors.toSet());
-           System.out.println("Predictions for "+asset+" (Should be "+tech+" ): "+ String.join("; ",predictions));
-           return predictions;
-       }).filter(collection->collection.contains(tech)).count()))/assets.size();
+        if(assets.isEmpty()) return 0d;
+        return ((double)(assets.stream().map(asset->{
+            Collection<String> predictions = model.attributesFor(PortfolioList.asList(asset, PortfolioList.Type.patents),numPredictions).stream().map(pair->pair.getFirst()).collect(Collectors.toSet());
+            if(predictions==null||predictions.isEmpty()) return Collections.emptyList();
+            System.out.println("Predictions for "+asset+" (Should be "+tech+" ): "+ String.join("; ",predictions));
+            return predictions;
+        }).filter(collection->collection.contains(tech)).count()))/assets.size();
     }
 }
