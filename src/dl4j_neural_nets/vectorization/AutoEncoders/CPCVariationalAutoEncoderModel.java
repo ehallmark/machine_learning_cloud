ea7bdb61e2edd93035b4cc411137abd9e7d697d4
Evan Hallmark <ehallmar@usc.edu>
2017-06-11 11:06:56 -0700

    changes

--- src/dl4j_neural_nets/vectorization/AutoEncoders/CPCVariationalAutoEncoderModel.java
+++ src/dl4j_neural_nets/vectorization/AutoEncoders/CPCVariationalAutoEncoderModel.java
@@ -119,10 +119,10 @@ public class CPCVariationalAutoEncoderModel {
                 .list()
                 .layer(0, new VariationalAutoencoder.Builder()
                         .activation(Activation.RELU)
-                        .encoderLayerSizes(numInputs/2,numInputs/2) // encoder layers
-                        .decoderLayerSizes(numInputs/2,numInputs/2)  // decoder layers
+                        .encoderLayerSizes(numInputs/2) // encoder layers
+                        .decoderLayerSizes(numInputs/2)  // decoder layers
                         .pzxActivationFunction(Activation.IDENTITY)  //p(z|data) activation function
-                        .reconstructionDistribution(new BernoulliReconstructionDistribution(Activation.SIGMOID.getActivationFunction()))     //Bernoulli distribution for p(data|z) (binary or 0 to 1 data only)
+                        .reconstructionDistribution(new BernoulliReconstructionDistribution(Activation.SOFTMAX.getActivationFunction()))     //Bernoulli distribution for p(data|z) (binary or 0 to 1 data only)
                         .nIn(numInputs)                       //Input size: 28x28
                         .nOut(vectorSize)                            //Size of the latent variable space: p(z|x). 2 dimensions here for plotting, use more in general
                         .build())
