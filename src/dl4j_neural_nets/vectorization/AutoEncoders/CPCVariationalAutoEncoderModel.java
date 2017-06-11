782941e0cf610c6282ab2e9dbd6c3e6e48971922
Evan Hallmark <ehallmar@usc.edu>
2017-06-11 11:26:11 -0700

    changes

--- src/dl4j_neural_nets/vectorization/AutoEncoders/CPCVariationalAutoEncoderModel.java
+++ src/dl4j_neural_nets/vectorization/AutoEncoders/CPCVariationalAutoEncoderModel.java
@@ -96,8 +96,8 @@ public class CPCVariationalAutoEncoderModel {
         // Get Classifications
         List<String> classifications = CPCKMeans.getClassifications(patents,cpcDepth,true);
         final int numInputs = classifications.size();
-        final int vectorSize = numInputs/6;
-        final int hiddenLayerSize = numInputs/3;
+        final int vectorSize = numInputs/2;
+        final int hiddenLayerSize = numInputs/10;
 
         System.out.println("Num Inputs: "+numInputs);
         System.out.println("Vector Size: "+vectorSize);
