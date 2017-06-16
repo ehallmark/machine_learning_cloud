9f5b827814e890a7439abccc3ea4adf8806e3277
Evan Hallmark <ehallmark@gttgrp.com>
2017-06-16 13:43:20 -0700

    changes

--- src/server/SimilarPatentServer.java
+++ src/server/SimilarPatentServer.java
@@ -567,7 +567,7 @@ public class SimilarPatentServer {
                         div().withClass("overSelect")
                 ), div().withId("checkboxes").with(
                         getTechTagger().getClassifications().stream().map(technology-> {
-                            return div().with(label(technology),input().withType("checkbox").withName(TECHNOLOGIES_TO_SEARCH_FOR_ARRAY_FIELD).withValue(technology));
+                            return div().with(label(technology).with(input().withType("checkbox").attr("style","float: right;").withName(TECHNOLOGIES_TO_SEARCH_FOR_ARRAY_FIELD).withValue(technology)));
                         }).collect(Collectors.toList())
                 )
         );
