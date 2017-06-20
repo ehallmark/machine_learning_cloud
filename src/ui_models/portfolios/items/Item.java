2885fb1de1c430edff9868bf122ac273f55a4f4c
Evan Hallmark <ehallmar@usc.edu>
2017-06-20 16:03:53 -0700

    Changes

--- src/ui_models/portfolios/items/Item.java
+++ src/ui_models/portfolios/items/Item.java
@@ -26,6 +26,7 @@ public class Item implements Comparable<Item> {
     }
 
     public void setSimilarity(double sim) {
+        this.similarityCache=sim;
         addData(Constants.SIMILARITY, sim*100d);
     }
 
