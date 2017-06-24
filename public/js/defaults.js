c10392bc881d753602060da8a372d37d68ee6a2d
Evan Hallmark <ehallmar@usc.edu>
2017-06-24 09:28:01 -0700

    Changes

--- public/js/defaults.js
+++ public/js/defaults.js
@@ -115,7 +115,7 @@ var setupDataTable = function (dataTable) {
             var A = getVal(a,sortOnField);
             var B = getVal(b,sortOnField);
 
-            return B.localCompare(A);
+            return B.localeCompare(A, 'kn');
         });
 
 
