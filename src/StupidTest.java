28f7c03e0619f6ba3c21fdb81e003a859b9941ae
Evan Hallmark <ehallmark@gttgrp.com>
2017-07-12 15:53:59 -0700

    Chagnes

---
+++ src/StupidTest.java
@@ -0,0 +1,10 @@
+import seeding.Database;
+
+/**
+ * Created by ehallmark on 7/12/17.
+ */
+public class StupidTest {
+    public static void main(String[] args) {
+        Database.getAppToClassificationMap();
+    }
+}
