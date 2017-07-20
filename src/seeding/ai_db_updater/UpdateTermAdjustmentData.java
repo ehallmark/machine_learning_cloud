10dfd181e42a0382071dc49b9c4571d538740b69
Evan Hallmark <ehallmark@gttgrp.com>
2017-07-20 11:02:25 -0700

    Chagnes

--- src/seeding/ai_db_updater/UpdateTermAdjustmentData.java
+++ src/seeding/ai_db_updater/UpdateTermAdjustmentData.java
@@ -9,6 +9,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by Evan on 1/22/2017.
@@ -18,9 +19,11 @@ public class UpdateTermAdjustmentData {
     public static void main(String[] args) {
         LineHandler handler = new TermAdjustmentHandler();
         File dataFolder = new File("data/patent_term_adjustments/");
+        AtomicInteger cnt = new AtomicInteger(0);
         Arrays.stream(dataFolder.listFiles()).parallel().forEach(file -> {
             try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
                 reader.lines().parallel().forEach(line->{
+                    if(cnt.getAndIncrement()%10000==0) System.out.println("Cnt: "+cnt.get());
                     handler.handleLine(line);
                 });
             } catch (Exception e) {
