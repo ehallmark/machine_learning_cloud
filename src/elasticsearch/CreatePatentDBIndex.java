8bff3547da6d0d29b1765da8b558af767e1da53d
Evan Hallmark <ehallmar@usc.edu>
2017-07-25 00:28:26 -0700

    Chagnes

--- src/elasticsearch/CreatePatentDBIndex.java
+++ src/elasticsearch/CreatePatentDBIndex.java
@@ -13,19 +13,25 @@ public class CreatePatentDBIndex {
     public static void main(String[] args) {
         TransportClient client = MyClient.get();
         CreateIndexRequestBuilder builder = client.admin().indices().prepareCreate(DataIngester.INDEX_NAME);
-        Map<String,String> mapping = new HashMap<>();
-        mapping.put("doc_type","keyword");
-        mapping.put("pub_doc_number","keyword");
-        mapping.put("tokens","text");
-        mapping.put("assignee","keyword");
-        mapping.put("wipoTechnology","keyword");
-        mapping.put("name","keyword");
-        mapping.put("cpcTechnology","keyword");
-        mapping.put("technologyValue","keyword");
+        Map<String,Object> mapping = new HashMap<>();
+        mapping.put("doc_type",typeMap("keyword"));
+        mapping.put("pub_doc_number",typeMap("keyword"));
+        mapping.put("tokens",typeMap("text"));
+        mapping.put("assignee",typeMap("keyword"));
+        mapping.put("wipoTechnology",typeMap("keyword"));
+        mapping.put("name",typeMap("keyword"));
+        mapping.put("cpcTechnology",typeMap("keyword"));
+        mapping.put("technologyValue",typeMap("keyword"));
         Map<String,Object> properties = new HashMap<>();
         properties.put("properties",mapping);
         builder.addMapping(DataIngester.TYPE_NAME, properties);
         System.out.println("Query: "+builder.toString());
         builder.get();
+    }
+
+    private static Object typeMap(String type) {
+        Map<String,String> typeMap = new HashMap<>();
+        typeMap.put("type",type);
+        return typeMap;
     }
 }
