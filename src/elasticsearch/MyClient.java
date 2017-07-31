f3d5ca15520663f5b18c2bd410070fb663de0fed
Evan Hallmark <ehallmark@gttgrp.com>
2017-07-31 13:40:15 -0700

    chagnes

--- src/elasticsearch/MyClient.java
+++ src/elasticsearch/MyClient.java
@@ -24,8 +24,8 @@ public class MyClient {
                     .put("client.transport.sniff", sniff).build();
 
             TransportClient client = new PreBuiltTransportClient(settings)
-                    .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9300))
-                    .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9301));
+                    .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9300));
+                    //.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9301));
             CLIENT = client;
         } catch (UnknownHostException e) {
             e.printStackTrace();
