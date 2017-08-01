e740efb46c12ecfed0b43debcbdafa51f4cb1f08
Evan Hallmark <ehallmark@gttgrp.com>
2017-08-01 15:32:34 -0700

    changes

--- setup.sh
+++ setup.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
-sudo su - ehallmark1122
 cd /home/ehallmark1122/machine_learning_cloud
+echo "Changed Dirs"
 git pull origin master
+echo "Pulled from git"
 mvn clean install
+echo "Mvn clean installed"
 gcsfuse machine_learning_cloud_data data
-cd src/elasticsearch
+echo "Finished gcsfuse"
+cd /home/ehallmark1122/machine_learning_cloud/src/elasticsearch
+echo "in elasticsearch"
 #docker-compose up -d
-cd ../../
+echo "Docer compose up"
+cd /home/ehallmark1122/machine_learning_cloud
+echo "Back to src dir"
 java -cp target/classes:"target/dependency/*" -Xms20000m -Xmx20000m user_interface.server.SimilarPatentServer &
-
+echo "Starting Java server"
 
