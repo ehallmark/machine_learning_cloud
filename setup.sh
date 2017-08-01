3ddd534a60c01210c233734ac4a860f21c524616
Evan Hallmark <ehallmark@gttgrp.com>
2017-08-01 16:03:01 -0700

    Changes

--- setup.sh
+++ setup.sh
@@ -1,18 +1,16 @@
 #!/usr/bin/env bash
 cd /home/ehallmark1122/machine_learning_cloud
-echo "Changed Dirs"
-git pull origin master
-echo "Pulled from git"
+sudo -u ehallmark1122 bash -c '    echo "Changed Dirs"'
+sudo -u ehallmark1122 bash -c '    git pull origin master'
+sudo -u ehallmark1122 bash -c '    echo "Pulled from git"'
 mvn clean install
-echo "Mvn clean installed"
-gcsfuse machine_learning_cloud_data data
-echo "Finished gcsfuse"
+sudo -u ehallmark1122 bash -c '    echo "Mvn clean installed"'
+sudo -u ehallmark1122 bash -c '    gcsfuse machine_learning_cloud_data data'
+sudo -u ehallmark1122 bash -c '    echo "Finished gcsfuse"'
 cd /home/ehallmark1122/machine_learning_cloud/src/elasticsearch
-echo "in elasticsearch"
-#docker-compose up -d
-echo "Docer compose up"
+sudo -u ehallmark1122 bash -c '    echo "in elasticsearch"'
+#sudo -u ehallmark1122 bash -c '    docker-compose up -d'
+sudo -u ehallmark1122 bash -c '    echo "Docker compose up"'
 cd /home/ehallmark1122/machine_learning_cloud
-echo "Back to src dir"
-java -cp target/classes:"target/dependency/*" -Xms20000m -Xmx20000m user_interface.server.SimilarPatentServer &
-echo "Starting Java server"
-
+sudo -u ehallmark1122 bash -c '    echo "Back to src dir" '
+sudo -u ehallmark1122 bash -c '    java -cp target/classes:"target/dependency/*" -Xms20000m -Xmx20000m user_interface.server.SimilarPatentServer &'
