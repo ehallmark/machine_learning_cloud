c47572d6c79d67e6d27b7f1cb6b0bd7e7f6117cd
Evan Hallmark <ehallmark@gttgrp.com>
2017-08-01 12:57:45 -0700

    chagnes

--- setup.sh
+++ setup.sh
@@ -1,4 +1,12 @@
-gcloud auth application-default login
-sudo mkdir data
-sudo chmod a+w data
+#!/usr/bin/env bash
+sudo su - ehallmark1122
+cd /home/ehallmark1122/machine_learning_cloud
+git pull origin master
+mvn clean install
 gcsfuse machine_learning_cloud_data data
+cd src/elasticsearch
+#docker-compose up -d
+cd ../../
+java -cp target/classes:"target/dependency/*" -Xms20000m -Xmx20000m user_interface.server.SimilarPatentServer &
+
+
