07c8f597418f5ff551e7ea8a6d621a20f113644b
Evan Hallmark <ehallmark@gttgrp.com>
2017-08-02 09:48:26 -0700

    changes

--- scripts/mini-server-script.sh
+++ scripts/mini-server-script.sh
@@ -3,6 +3,6 @@ cd /home/ehallmark1122/machine_learning_cloud
 sudo -u ehallmark1122 bash -c '    echo "Changed Dirs"'
 sudo -u ehallmark1122 bash -c '    git pull origin master'
 sudo -u ehallmark1122 bash -c '    echo "Pulled from git"'
-mvn clean install
+sudo -u ehallmark1122 bash -c '    mvn clean install'
 sudo -u ehallmark1122 bash -c '    echo "Mvn clean installed"'
 sudo -u ehallmark1122 bash -c '    java -cp target/classes:"target/dependency/*" mini_server.Server &'
