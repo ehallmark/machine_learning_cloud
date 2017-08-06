5a7b971acd1a5c7695f7d4b66e1c35d2058d6e0e
Evan Hallmark <ehallmar@usc.edu>
2017-08-06 13:22:09 -0700

    changes

--- src/pair_bulk_data/DataMassager.java
+++ src/pair_bulk_data/DataMassager.java
@@ -25,7 +25,7 @@ public class DataMassager {
 
     public static void getApplicationNumbersFromGrantsAndPublications(String assignee) throws Exception {
         Connection conn = Database.getConn();
-        PreparedStatement ps = conn.prepareStatement("select count(application_number),date_part('year',filing_date') from pair_applications where filing_date is not null and assignee is not null and upper(assignee) like ? || '%' group by date_part('year',filing_date) order by date_part('year',filing_date)");
+        PreparedStatement ps = conn.prepareStatement("select count(application_number) as count,date_part('year',filing_date) as year from pair_applications where filing_date is not null and assignee is not null and upper(assignee) like (upper(?) || '%') group by date_part('year',filing_date) order by date_part('year',filing_date)");
         ps.setString(1,assignee);
         ps.setFetchSize(10);
         System.out.println(ps);
