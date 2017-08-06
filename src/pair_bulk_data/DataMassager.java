c36d389bec0063e7b3bb507724d2faff7344b825
Evan Hallmark <ehallmar@usc.edu>
2017-08-06 07:19:46 -0700

    changes

--- src/pair_bulk_data/DataMassager.java
+++ src/pair_bulk_data/DataMassager.java
@@ -16,6 +16,10 @@ import java.util.stream.Collectors;
  */
 public class DataMassager {
     public static void main(String[] args) throws Exception {
+        getApplicationNumbersFromGrantsAndPublications(args);
+    }
+
+    public static void getApplicationNumbersFromGrantsAndPublications(String[] args) throws Exception {
         if(args.length == 0) throw new RuntimeException("Please include excel filename");
         final int offset = 7;
         final int colIdx = 2;
@@ -24,7 +28,7 @@ public class DataMassager {
         Collection<String> publications = assets.stream().filter(asset->Database.isApplication(asset)).collect(Collectors.toList());
         Connection conn = Database.getConn();
 
-        PreparedStatement ps = conn.prepareStatement("select count(application_number), date_part('year',filing_date) as year from pair_applications where filing_date is not null and ( grant_number = any(?) or publication_number like any (?) ) group by date_part('year', filing_date) order by date_part('year',filing_date)");
+        PreparedStatement ps = conn.prepareStatement("select application_number,filing_date from pair_applications where filing_date is not null and ( grant_number = any(?) or publication_number like any (?) ) order by filing_date");
         ps.setArray(1, conn.createArrayOf("varchar",patents.toArray()));
         ps.setArray(2, conn.createArrayOf("varchar",addWildCards(publications).toArray()));
 
@@ -33,7 +37,7 @@ public class DataMassager {
 
         System.out.println("Count,Year");
         while(rs.next()) {
-            System.out.println(""+rs.getInt(1)+","+rs.getInt(2));
+            System.out.println(""+rs.getString(1)+","+rs.getDate(2));
         }
 
         rs.close();
