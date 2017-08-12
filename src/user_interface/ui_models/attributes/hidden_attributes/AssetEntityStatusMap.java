8882017c7d97ecd1e667b96bc92bee7b59804c52
Evan Hallmark <ehallmar@usc.edu>
2017-08-12 05:37:35 -0700

    changes

--- src/user_interface/ui_models/attributes/hidden_attributes/AssetEntityStatusMap.java
+++ src/user_interface/ui_models/attributes/hidden_attributes/AssetEntityStatusMap.java
@@ -1,7 +1,10 @@
 package user_interface.ui_models.attributes.hidden_attributes;
 
 import seeding.Constants;
+import seeding.Database;
+import user_interface.ui_models.attributes.computable_attributes.ComputableAttribute;
 
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -17,5 +20,27 @@ public class AssetEntityStatusMap extends HiddenAttribute<Set<String>> {
     @Override
     public String getName() {
         return Constants.NAME+"_to_"+Constants.ASSIGNEE_ENTITY_TYPE;
+    }
+
+    @Override
+    public synchronized Map<String,Set<String>> getPatentDataMap() {
+        patentDataMap = super.getPatentDataMap();
+        if(patentDataMap!=null&&patentDataMap.isEmpty()) {
+            patentDataMap.put(Constants.SMALL,new HashSet<>());
+            patentDataMap.put(Constants.MICRO,new HashSet<>());
+            patentDataMap.put(Constants.LARGE,new HashSet<>());
+        }
+        return patentDataMap;
+    }
+
+    @Override
+    public synchronized Map<String,Set<String>> getApplicationDataMap() {
+        applicationDataMap = super.getApplicationDataMap();
+        if(applicationDataMap!=null&&applicationDataMap.isEmpty()) {
+            applicationDataMap.put(Constants.SMALL,new HashSet<>());
+            applicationDataMap.put(Constants.MICRO,new HashSet<>());
+            applicationDataMap.put(Constants.LARGE,new HashSet<>());
+        }
+        return applicationDataMap;
     }
 }
--- src/user_interface/ui_models/attributes/hidden_attributes/ExpiredAssetsMap.java
+++ src/user_interface/ui_models/attributes/hidden_attributes/ExpiredAssetsMap.java
@@ -2,6 +2,7 @@ package user_interface.ui_models.attributes.hidden_attributes;
 
 import seeding.Constants;
 
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -17,5 +18,23 @@ public class ExpiredAssetsMap extends HiddenAttribute<Set<String>> {
     @Override
     public String getName() {
         return Constants.NAME+"_to_"+Constants.EXPIRED;
+    }
+
+    @Override
+    public synchronized Map<String,Set<String>> getPatentDataMap() {
+        patentDataMap = super.getPatentDataMap();
+        if(patentDataMap!=null&&patentDataMap.isEmpty()) {
+            patentDataMap.put(Constants.EXPIRED,new HashSet<>());
+        }
+        return patentDataMap;
+    }
+
+    @Override
+    public synchronized Map<String,Set<String>> getApplicationDataMap() {
+        applicationDataMap = super.getApplicationDataMap();
+        if(applicationDataMap!=null&&applicationDataMap.isEmpty()) {
+            applicationDataMap.put(Constants.EXPIRED,new HashSet<>());
+        }
+        return applicationDataMap;
     }
 }
