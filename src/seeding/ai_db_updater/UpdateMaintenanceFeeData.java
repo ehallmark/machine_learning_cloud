51e85a8f8704e89660edfc5247a7c4435bfa70c5
Evan Hallmark <ehallmark@gttgrp.com>
2017-08-14 14:33:30 -0700

    c

--- src/seeding/ai_db_updater/UpdateMaintenanceFeeData.java
+++ src/seeding/ai_db_updater/UpdateMaintenanceFeeData.java
@@ -58,7 +58,7 @@ public class UpdateMaintenanceFeeData {
                 });
             } catch (Exception e) {
                 e.printStackTrace();
-            } 
+            }
         });
         handler.save();
     }
