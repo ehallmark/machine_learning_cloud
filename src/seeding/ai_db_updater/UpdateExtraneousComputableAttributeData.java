f4b4e4d0aeca23174210100b6e0e5520308939bb
Evan Hallmark <ehallmar@usc.edu>
2017-08-19 15:42:53 -0700

    c

--- src/seeding/ai_db_updater/UpdateExtraneousComputableAttributeData.java
+++ src/seeding/ai_db_updater/UpdateExtraneousComputableAttributeData.java
@@ -2,8 +2,10 @@ package seeding.ai_db_updater;
 
 import user_interface.server.SimilarPatentServer;
 import user_interface.ui_models.attributes.computable_attributes.*;
+import user_interface.ui_models.attributes.hidden_attributes.HiddenAttribute;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 /**
  * Created by Evan on 7/23/2017.
@@ -12,7 +14,7 @@ public class UpdateExtraneousComputableAttributeData {
     private static final int batchSize = 10000;
     public static void main(String[] args) {
         SimilarPatentServer.initialize(true,false);
-        SimilarPatentServer.loadAndIngestAllItemsWithAttributes(SimilarPatentServer.getAllComputableAttributes(),batchSize);
+        SimilarPatentServer.loadAndIngestAllItemsWithAttributes(SimilarPatentServer.getAllComputableAttributes().stream().filter(a->!(a instanceof HiddenAttribute)).collect(Collectors.toList()), batchSize);
     }
 
 }
