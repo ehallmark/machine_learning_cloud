32b0b01adb335538744b3e97c8897c38be50ea36
Evan Hallmark <ehallmar@usc.edu>
2017-08-20 09:45:11 -0700

    c

--- src/user_interface/ui_models/templates/PortfolioAssessment.java
+++ src/user_interface/ui_models/templates/PortfolioAssessment.java
@@ -2,10 +2,14 @@ package user_interface.ui_models.templates;
 
 import seeding.Constants;
 import user_interface.server.SimilarPatentServer;
+import user_interface.ui_models.attributes.AbstractAttribute;
+import user_interface.ui_models.attributes.NestedAttribute;
 import user_interface.ui_models.filters.AbstractFilter;
+import user_interface.ui_models.filters.AbstractNestedFilter;
 import user_interface.ui_models.portfolios.PortfolioList;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,14 +24,40 @@ public class PortfolioAssessment extends FormTemplate {
 
     private static Map<String,Object> getParams() {
         Map<String,Object> map = new HashMap<>();
+
         map.put(Constants.HISTOGRAM,Arrays.asList(Constants.AI_VALUE, Constants.REMAINING_LIFE));
         map.put(Constants.LINE_CHART,Arrays.asList(Constants.PRIORITY_DATE));
         map.put(Constants.PIE_CHART,Arrays.asList(Constants.WIPO_TECHNOLOGY,Constants.TECHNOLOGY));
-        map.put(SimilarPatentServer.ATTRIBUTES_ARRAY_FIELD,Arrays.asList(Constants.AI_VALUE,Constants.NAME,Constants.LATEST_ASSIGNEE,Constants.PORTFOLIO_SIZE,Constants.WIPO_TECHNOLOGY,Constants.TECHNOLOGY));
-        map.put(Constants.NAME + Constants.FILTER_SUFFIX,"");
-        map.put(Constants.LATEST_ASSIGNEE + Constants.FILTER_SUFFIX,"");
-        map.put(Constants.DOC_TYPE + AbstractFilter.FilterType.Include.toString() + Constants.FILTER_SUFFIX, Arrays.asList(PortfolioList.Type.patents.toString()));
+        map.put(SimilarPatentServer.ATTRIBUTES_ARRAY_FIELD,Arrays.asList(Constants.AI_VALUE,Constants.NAME,Constants.LATEST_ASSIGNEE+"."+Constants.ASSIGNEE,Constants.PORTFOLIO_SIZE,Constants.WIPO_TECHNOLOGY,Constants.TECHNOLOGY));
+        map.put(simpleNameToFilterName(AbstractFilter.FilterType.Include,Constants.NAME),"");
+        map.put(simpleNameToFilterName(AbstractFilter.FilterType.AdvancedKeyword,Constants.LATEST_ASSIGNEE,Constants.ASSIGNEE),"");
+        map.put(simpleNameToFilterName(AbstractFilter.FilterType.Include,Constants.DOC_TYPE),PortfolioList.Type.patents.toString());
         return map;
+    }
+
+    public static String simpleNameToFilterName(AbstractFilter.FilterType targetFilterType, String... attrStrs) {
+        AbstractAttribute attribute = getChildHelper(attrStrs,0);
+        return attribute.createFilters().stream().filter(filter->filter.getFilterType().equals(targetFilterType)).map(filter->filter.getName()).findAny().orElse("");
+    }
+
+    private static AbstractAttribute getChildHelper(String[] attrStrs, int i) {
+        if(i >= attrStrs.length) {
+            return null;
+        } else {
+            AbstractAttribute attr = SimilarPatentServer.attributesMap.get(String.join(".",Arrays.copyOf(attrStrs,i)));
+            if (attr != null) {
+                AbstractAttribute child = getChildHelper(attrStrs, i + 1);
+                if(child != null) {
+                    return new NestedAttribute(Arrays.asList(child)) {
+                        @Override
+                        public String getName() {
+                            return attrStrs[i];
+                        }
+                    };
+                }
+            }
+            return attr;
+        }
     }
 
 }
