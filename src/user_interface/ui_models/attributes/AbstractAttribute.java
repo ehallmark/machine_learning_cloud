03e1bb9218bb62fe41a0f6de2a694ffbaddbd39d
Evan Hallmark <ehallmar@usc.edu>
2017-08-19 19:38:06 -0700

    c

--- src/user_interface/ui_models/attributes/AbstractAttribute.java
+++ src/user_interface/ui_models/attributes/AbstractAttribute.java
@@ -29,7 +29,7 @@ public abstract class AbstractAttribute {
     public abstract String getName();
 
     public String getFullName() {
-        return parent==null? getName() : (parent.getName() + ".0." + getName());
+        return parent==null? getName() : (parent.getName() + "." + getName());
     }
 
     public Tag getOptionsTag() { return div(); }
