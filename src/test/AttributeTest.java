b7b9ea65fed1c65e6f57f5be6b43889da16e6cee
Evan Hallmark <ehallmar@usc.edu>
2017-08-19 18:04:59 -0700

    c

--- src/test/AttributeTest.java
+++ src/test/AttributeTest.java
@@ -13,10 +13,10 @@ import java.util.Collection;
 public class AttributeTest {
     public static void main(String[] args) {
         AssetToAssigneeMap assetToAssigneeMap = new AssetToAssigneeMap();
-        assetToAssigneeMap.getApplicationDataMap();
-        assetToAssigneeMap.getPatentDataMap();
+        //assetToAssigneeMap.getApplicationDataMap();
+        //assetToAssigneeMap.getPatentDataMap();
         AssigneeToAssetsMap assigneeToAssetsMap = new AssigneeToAssetsMap();
-        assetToAssigneeMap.save();
+        //assetToAssigneeMap.save();
         System.out.println("Size of app map: "+assigneeToAssetsMap.getApplicationDataMap().size());
         System.out.println("Size of patent map: "+assigneeToAssetsMap.getApplicationDataMap().size());
         assigneeToAssetsMap.getPatentDataMap().entrySet().stream().limit(5).forEach(e->{
