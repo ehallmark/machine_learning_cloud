8a3028308a98da563afc7531db77a303a8e7dd8e
Evan Hallmark <ehallmar@usc.edu>
2017-08-20 10:10:47 -0700

    c

--- src/user_interface/ui_models/templates/PortfolioAssessment.java
+++ src/user_interface/ui_models/templates/PortfolioAssessment.java
@@ -28,7 +28,8 @@ public class PortfolioAssessment extends FormTemplate {
         map.put(Constants.HISTOGRAM,Arrays.asList(Constants.AI_VALUE, Constants.REMAINING_LIFE));
         map.put(Constants.LINE_CHART,Arrays.asList(Constants.PRIORITY_DATE));
         map.put(Constants.PIE_CHART,Arrays.asList(Constants.WIPO_TECHNOLOGY,Constants.TECHNOLOGY));
-        map.put(SimilarPatentServer.ATTRIBUTES_ARRAY_FIELD,Arrays.asList(Constants.AI_VALUE,Constants.NAME,Constants.LATEST_ASSIGNEE+"."+Constants.ASSIGNEE,Constants.PORTFOLIO_SIZE,Constants.WIPO_TECHNOLOGY,Constants.TECHNOLOGY));
+        map.put(SimilarPatentServer.ATTRIBUTES_ARRAY_FIELD,Arrays.asList(Constants.AI_VALUE,Constants.NAME,Constants.PORTFOLIO_SIZE,Constants.WIPO_TECHNOLOGY,Constants.TECHNOLOGY));
+        map.put(Constants.LATEST_ASSIGNEE+"."+Constants.ASSIGNEE,"");
         map.put(simpleNameToFilterName(AbstractFilter.FilterType.Include,Constants.NAME),"");
         map.put(simpleNameToFilterName(AbstractFilter.FilterType.AdvancedKeyword,Constants.LATEST_ASSIGNEE,Constants.ASSIGNEE),"");
         map.put(simpleNameToFilterName(AbstractFilter.FilterType.Include,Constants.DOC_TYPE),PortfolioList.Type.patents.toString());
@@ -37,7 +38,7 @@ public class PortfolioAssessment extends FormTemplate {
 
     public static String simpleNameToFilterName(AbstractFilter.FilterType targetFilterType, String... attrStrs) {
         AbstractAttribute attribute = getChildHelper(attrStrs);
-        return attribute.createFilters().stream().filter(filter->filter.getFilterType().equals(targetFilterType)).map(filter->filter.getName()).findAny().orElse("");
+        return attribute.createFilters().stream().filter(filter->filter!=null&&filter.getFilterType().equals(targetFilterType)).map(filter->filter.getName()).findAny().orElse("");
     }
 
     private static AbstractAttribute getChildHelper(String[] attrStrs) {
@@ -53,6 +54,7 @@ public class PortfolioAssessment extends FormTemplate {
                 }
             }
         }
+        return attr;
     }
 
 }
