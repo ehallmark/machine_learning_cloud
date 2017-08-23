65503985cbfed4535733d20fdba271102b34e02c
Evan Hallmark <ehallmar@usc.edu>
2017-08-23 03:26:15 -0700

    c

--- src/seeding/ai_db_updater/handlers/USPTOAssignmentHandler.java
+++ src/seeding/ai_db_updater/handlers/USPTOAssignmentHandler.java
@@ -235,12 +235,12 @@ public class USPTOAssignmentHandler extends NestedHandler {
                 Map<String,Object> mergedDataMap = new HashMap<>();
                 List<Map<String, Object>> latestAssigneeData = (List<Map<String, Object>>) assignmentMap.get(Constants.LATEST_ASSIGNEE);
                 if (latestAssigneeData != null && latestAssigneeData.size() > 0) {
-                    mergeDataMapHelper(mergedDataMap, latestAssigneeData.stream().findAny().get(), Constants.LATEST_ASSIGNEE);
+                    mergeDataMapHelper(mergedDataMap, latestAssigneeData.stream().limit(1).collect(Collectors.toList()), Constants.LATEST_ASSIGNEE);
                 }
                 // add assignor data
                 List<Map<String, Object>> latestAssignorData = (List<Map<String, Object>>) assignmentMap.get(Constants.ASSIGNORS);
                 if (latestAssignorData != null && latestAssignorData.size() > 0) {
-                    mergeDataMapHelper(mergedDataMap, latestAssignorData.stream().findAny().get(), Constants.ASSIGNORS);
+                    mergeDataMapHelper(mergedDataMap, latestAssignorData.stream().limit(1).collect(Collectors.toList()), Constants.ASSIGNORS);
                 }
                 // add conveyance text (helpful to fiend liens)
                 Object conveyanceText = assignmentMap.get(Constants.CONVEYANCE_TEXT);
