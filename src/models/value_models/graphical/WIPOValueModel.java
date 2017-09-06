f405844bc8ede6df2a19f23e8916be184ef3afdd
Evan Hallmark <ehallmark@gttgrp.com>
2017-09-06 12:58:14 -0700

    c

--- src/models/value_models/graphical/WIPOValueModel.java
+++ src/models/value_models/graphical/WIPOValueModel.java
@@ -110,7 +110,7 @@ public class WIPOValueModel extends ValueAttr {
 
         bayesianValueModel.graph.setCurrentAssignment(new HashMap<>());
         FactorNode wipoFactor = bayesianValueModel.graph.getFactorNodes().get(0);
-        wipoFactor.reNormalize(new DivideByPartition());
+       // wipoFactor.reNormalize(new DivideByPartition());
         List<String> technologies = bayesianValueModel.getVariableToValuesMap().get(Constants.WIPO_TECHNOLOGY);
         technologyToFactorMap = Collections.synchronizedMap(new HashMap<>());
         if(technologies.size()!=wipoFactor.getWeights().length) throw new RuntimeException("Illegal number of weights: "+technologies.size()+" != "+wipoFactor.getWeights().length);
