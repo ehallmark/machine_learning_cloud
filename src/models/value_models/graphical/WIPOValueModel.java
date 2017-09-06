f05c2659a952f7bd19d1f5ebf4a37ea38f871532
Evan Hallmark <ehallmark@gttgrp.com>
2017-09-06 13:46:32 -0700

    c

--- src/models/value_models/graphical/WIPOValueModel.java
+++ src/models/value_models/graphical/WIPOValueModel.java
@@ -48,39 +48,46 @@ public class WIPOValueModel extends ValueAttr {
 
     public void init() {
         final double alpha = 10d;
+        final String valueVariableName = "gatherValue";
         final int maxLimit = 100000;
         AbstractAttribute wipo = new WIPOTechnologyAttribute();
+        AbstractAttribute filingCountry = new LatestAssigneeNestedAttribute().getAttributes().stream().filter(attr->attr.getName().equals(Constants.COUNTRY)).findFirst().get();
         Collection<AbstractAttribute> attributes = Arrays.asList(
-                wipo//,
-                //filingCountry
+                wipo,
+                filingCountry
         );
         Set<String> attrNameSet = attributes.stream().map(attr->attr.getFullName()).collect(Collectors.toSet());
         AbstractIncludeFilter gatherFilter = new ExistsInGatherFilter();
         Collection<AbstractFilter> filters = Arrays.asList(gatherFilter, new AbstractIncludeFilter(new ResultTypeAttribute(), AbstractFilter.FilterType.Include, AbstractFilter.FieldType.Text, Arrays.asList("patents")));
-
         Map<String,NestedAttribute> nestedMap = new HashMap<>();
         nestedMap.put(Constants.LATEST_ASSIGNEE, new LatestAssigneeNestedAttribute());
         List<Item> items = new ArrayList<>(Arrays.asList(DataSearcher.searchForAssets(attributes, filters, Constants.NAME, SortOrder.ASC, maxLimit, nestedMap)).stream().filter(item->Database.getGatherValueMap().containsKey(item.getName())).collect(Collectors.toList()));
         System.out.println("Num items: "+items.size());
         allItems = items.toArray(new Item[items.size()]);
 
+        Map<String,Boolean> gatherValueMap = Database.getGatherValueMap();
         final Map<String,List<String>> variableToValuesMap = Collections.synchronizedMap(new HashMap<>());
         Arrays.stream(allItems).parallel().forEach(item->{
+            // add gather value
+            Boolean value = gatherValueMap.get(item.getName());
+            if(value==null) return;
+            item.addData(valueVariableName, value ? 1 : 0);
+
             // add values
             attributes.forEach(attr->{
                 Object obj = item.getData(attr.getFullName());
                 if(obj!=null) {
-                    nestedHelper(attr.getFullName(), obj, attrNameSet, variableToValuesMap);
+                    nestedHelper(attr.getFullName(), obj, attrNameSet, variableToValuesMap, valueVariableName);
                 }
             });
             item.getDataMap().forEach((attr,obj)->{
                 if(obj instanceof Map) {
                     System.out.println("Checking map: "+attr);
                     ((Map<String,Object>)obj).forEach((innerAttr,innerObj)->{
-                        nestedHelper(attr+"."+innerAttr, innerObj, attrNameSet, variableToValuesMap);
+                        nestedHelper(attr+"."+innerAttr, innerObj, attrNameSet, variableToValuesMap, valueVariableName);
                     });
                 } else {
-                    nestedHelper(attr, obj, attrNameSet, variableToValuesMap);
+                    nestedHelper(attr, obj, attrNameSet, variableToValuesMap, valueVariableName);
                 }
             });
         });
@@ -102,16 +109,24 @@ public class WIPOValueModel extends ValueAttr {
             graph.addNode(attr,values.size());
         });
 
+        Node valueNode = graph.findNode(valueVariableName);
         Node wipoNode = graph.findNode(wipo.getFullName());
-
+        Node filingCountryNode = graph.findNode(filingCountry.getFullName());
+// connect and add factors
+        graph.connectNodes(wipoNode, valueNode);
+        graph.connectNodes(filingCountryNode, valueNode);
+        graph.connectNodes(filingCountryNode,wipoNode);
         graph.addFactorNode(null, wipoNode);
+        graph.addFactorNode(null, valueNode, wipoNode, filingCountryNode);
 
-        bayesianValueModel = new GraphicalValueModel(getName(),graph,alpha,allItems,variableToValuesMap, null);
+
+        bayesianValueModel = new GraphicalValueModel(getName(),graph,alpha,allItems,variableToValuesMap, valueVariableName);
         bayesianValueModel.train();
 
         bayesianValueModel.graph.setCurrentAssignment(new HashMap<>());
-        FactorNode wipoFactor = bayesianValueModel.graph.getFactorNodes().get(0);
-       // wipoFactor.reNormalize(new DivideByPartition());
+        FactorNode wipoFactor = bayesianValueModel.graph.variableElimination(new String[]{Constants.WIPO_TECHNOLOGY});
+        wipoFactor.reNormalize(new DivideByPartition());
+
         List<String> technologies = bayesianValueModel.getVariableToValuesMap().get(Constants.WIPO_TECHNOLOGY);
         technologyToFactorMap = Collections.synchronizedMap(new HashMap<>());
         if(technologies.size()!=wipoFactor.getWeights().length) throw new RuntimeException("Illegal number of weights: "+technologies.size()+" != "+wipoFactor.getWeights().length);
@@ -121,8 +136,8 @@ public class WIPOValueModel extends ValueAttr {
         }
     }
 
-    private static void nestedHelper(String attr, Object obj, Collection<String> attrNameSet, Map<String,List<String>> variableToValuesMap) {
-        if(obj==null|!attrNameSet.contains(attr)) return;
+    private static void nestedHelper(String attr, Object obj, Collection<String> attrNameSet, Map<String,List<String>> variableToValuesMap, String valueVariableName) {
+        if(obj==null||(!attrNameSet.contains(attr)&&!attr.equals(valueVariableName))) return;
         System.out.println("Checking attr: "+attr);
         Collection<String> objects = obj.toString().contains("; ") ? Arrays.asList(obj.toString().split("; ")) : Arrays.asList(obj.toString());
         if(variableToValuesMap.containsKey(attr)) {
@@ -145,11 +160,12 @@ public class WIPOValueModel extends ValueAttr {
         System.out.println("Writing values");
         File csv = new File(Constants.DATA_FOLDER+"value-graph-wipo.csv");
         try(BufferedWriter writer = new BufferedWriter(new FileWriter(csv))) {
-            writer.write("asset,wipoValue,gatherValue\n");
+            writer.write("asset,wipoPercent,wipoValue,gatherValue\n");
             for (Item item : model.allItems) {
                 double value = model.evaluate(item);
+                double modelValue = model.bayesianValueModel.evaluate(item);
                // System.out.println(item.getName() + "," + value);
-                writer.write(item.getName() + "," + value + "," + (Database.getGatherValueMap().get(item.getName()) ? 0 : 1)+"\n");
+                writer.write(item.getName() + "," + value + "," + modelValue + "," + (Database.getGatherValueMap().get(item.getName()) ? 1 : 0)+"\n");
             }
             writer.flush();
         } catch(Exception e) {
