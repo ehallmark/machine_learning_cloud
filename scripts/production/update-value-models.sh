19ce1a7b3a1b0231822cc3c6197d38644ae3a774
Evan Hallmark <ehallmark@gttgrp.com>
2017-09-07 14:36:49 -0700

    c

--- scripts/production/update-value-models.sh
+++ scripts/production/update-value-models.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #ufw allow 22
 cd /home/ehallmark1122/machine_learning_cloud
-sudo -u ehallmark1122 bash -c '    sudo systemctl start mongodb'
+#sudo -u ehallmark1122 bash -c '    sudo systemctl start mongodb'
 sudo -u ehallmark1122 bash -c '    echo "Changed Dirs"'
 sudo -u ehallmark1122 bash -c '    git pull origin master'
 sudo -u ehallmark1122 bash -c '    echo "Pulled from git"'
@@ -15,4 +15,4 @@ sudo -u ehallmark1122 bash -c '    sudo docker-compose up -d'
 sudo -u ehallmark1122 bash -c '    echo "Docker compose up"'
 cd /home/ehallmark1122/machine_learning_cloud
 sleep 20s
-sudo -u ehallmark1122 bash -c '    java -cp target/classes:"target/dependency/*" -Xms10000m -Xmx10000m -Dcom.sun.management.jmxremote.port=4567 -Dcom.sun.management.jmxremote.rmi.port=4567 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=127.0.0.1 models.value_models.UpdateValueModels &'
+sudo -u ehallmark1122 bash -c '    java -cp target/classes:"target/dependency/*" -Xms20000m -Xmx20000m -Dcom.sun.management.jmxremote.port=4567 -Dcom.sun.management.jmxremote.rmi.port=4567 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=127.0.0.1 models.value_models.UpdateValueModels &'
