35a1c855506cbfa2c9498f616a06817d2f1a21f7
Evan Hallmark <ehallmar@usc.edu>
2017-09-06 17:45:06 -0700

    c

---
+++ scripts/production/update-value-models.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#ufw allow 22
+cd /home/ehallmark1122/machine_learning_cloud
+sudo -u ehallmark1122 bash -c '    sudo systemctl start mongodb'
+sudo -u ehallmark1122 bash -c '    echo "Changed Dirs"'
+sudo -u ehallmark1122 bash -c '    git pull origin master'
+sudo -u ehallmark1122 bash -c '    echo "Pulled from git"'
+sudo -u ehallmark1122 bash -c '    mvn clean install'
+sudo -u ehallmark1122 bash -c '    echo "Mvn clean installed"'
+sudo -u ehallmark1122 bash -c '    gcsfuse machine_learning_cloud_data data'
+sudo -u ehallmark1122 bash -c '    echo "Finished gcsfuse"'
+cd /home/ehallmark1122/machine_learning_cloud/scripts/production
+sudo -u ehallmark1122 bash -c '    echo "in elasticsearch"'
+sudo -u ehallmark1122 bash -c '    sudo docker-compose up -d'
+sudo -u ehallmark1122 bash -c '    echo "Docker compose up"'
+cd /home/ehallmark1122/machine_learning_cloud
+sleep 90s
+sudo -u ehallmark1122 bash -c '    java -cp target/classes:"target/dependency/*" -Xms10000m -Xmx10000m -Dcom.sun.management.jmxremote.port=4567 -Dcom.sun.management.jmxremote.rmi.port=4567 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=127.0.0.1 models.value_models.UpdateValueModels'
