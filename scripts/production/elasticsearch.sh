1238430cc8ad5a6585190516e55a2eb117196976
Evan Hallmark <ehallmar@usc.edu>
2017-09-07 18:15:51 -0700

    c

--- scripts/production/elasticsearch.sh
+++ scripts/production/elasticsearch.sh
@@ -14,7 +14,10 @@ sudo -u ehallmark1122 bash -c '    echo "in elasticsearch"'
 sudo -u ehallmark1122 bash -c '    sudo docker-compose up -d'
 sudo -u ehallmark1122 bash -c '    echo "Docker compose up"'
 cd /home/ehallmark1122/machine_learning_cloud
-sleep 2m
+sleep 90s
+curl -XDELETE localhost:9200/ai_db
+sleep 5s
+sudo -u ehallmark1122 bash -c '    java -cp target/classes:"target/dependency/*" -Xms10000m -Xmx10000m -Dcom.sun.management.jmxremote.port=4567 -Dcom.sun.management.jmxremote.rmi.port=4567 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=127.0.0.1 elasticsearch.CreatePatentDBIndex'
 curl -XPUT 'localhost:9200/ai_db/_settings?pretty' -H 'Content-Type: application/json' -d'
 {
     "index" : {
