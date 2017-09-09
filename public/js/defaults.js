74a8596a0beb2fc28268b69430a9380196a3bbff
Evan Hallmark <ehallmar@usc.edu>
2017-09-08 20:26:01 -0700

    c

--- public/js/defaults.js
+++ public/js/defaults.js
@@ -238,14 +238,14 @@ $(document).ready(function() {
 
     $('select.nested-filter-select').on("select2:select", function(e) {
         var id = e.params.data.id;
-        $('.draggable[data-model="'+id+'"]').parent().show().find('input, select, textarea').not('select.nested-filter-select').prop('disabled', false);
+        $('.draggable[data-model="'+id+'"]').parent().show().find('input, select, textarea').prop('disabled', false);
         return true;
     });
 
     $('select.nested-filter-select').on("select2:unselect", function(e) {
         var id = e.params.data.id;
         var $element = $('.draggable[data-model="'+id+'"]').parent().hide();
-        $element.find('input, select, textarea').not('select.nested-filter-select').prop('disabled', true).val(null).trigger('change');
+        $element.find('input, select, textarea').prop('disabled', true).not('select.nested-filter-select').val(null).trigger('change');
         return true;
     });
 
