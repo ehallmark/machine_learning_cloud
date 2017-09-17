1ae62d2b80a69140bd3bac0b9710996d48c5b0b0
Evan Hallmark <ehallmar@usc.edu>
2017-09-16 19:22:12 -0700

    c

--- scripts/production/setup-no-start.sh
+++ scripts/production/setup-no-start.sh
@@ -10,7 +10,7 @@ sudo -u ehallmark1122 bash -c '    gcsfuse machine_learning_cloud_data data'
 sudo -u ehallmark1122 bash -c '    echo "Finished gcsfuse"'
 cd /home/ehallmark1122/machine_learning_cloud/scripts/production
 sudo -u ehallmark1122 bash -c '    echo "in elasticsearch"'
-sudo -u ehallmark1122 bash -c '    sudo docker-compose up -d'
+#sudo -u ehallmark1122 bash -c '    sudo docker-compose up -d'
 sudo -u ehallmark1122 bash -c '    echo "Docker compose up"'
 cd /home/ehallmark1122/machine_learning_cloud
 sudo -u ehallmark1122 bash -c '    echo "Back to src dir" '
