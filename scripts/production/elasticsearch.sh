a11553fe581abe560b6e06fa0227446342b34817
Evan Hallmark <ehallmar@usc.edu>
2017-09-23 17:44:09 -0700

    c

--- scripts/production/elasticsearch.sh
+++ scripts/production/elasticsearch.sh
@@ -16,7 +16,7 @@ sudo -u ehallmark1122 bash -c '    echo "Docker compose up"'
 cd /home/ehallmark1122/machine_learning_cloud
 sleep 120s
 curl -XDELETE localhost:9200/ai_db
-sleep 5s
+sleep 10s
 sudo -u ehallmark1122 bash -c '    java -cp target/classes:"target/dependency/*" -Xms15000m -Xmx15000m -Dcom.sun.management.jmxremote.port=4567 -Dcom.sun.management.jmxremote.rmi.port=4567 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=127.0.0.1 elasticsearch.CreatePatentDBIndex'
 curl -XPUT 'localhost:9200/ai_db/_settings?pretty' -H 'Content-Type: application/json' -d'
 {
