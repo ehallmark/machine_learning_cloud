006732f4534c4f3ca9e772d7efd7f6ed8fc38413
Evan Hallmark <ehallmark@gttgrp.com>
2017-10-02 10:24:07 -0700

    c

--- project-box.sh
+++ project-box.sh
@@ -2,4 +2,4 @@
 git pull origin master
 mvn clean install
 gcsfuse machine_learning_cloud_data data
-java -cp target/classes:"target/dependency/*" project_box.PGDumpLatest &
+java -cp target/classes:"target/dependency/*" project_box.PGDumpLatest
