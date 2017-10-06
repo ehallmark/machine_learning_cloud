3abaedbab6818dab2c778422c9e17d47a2cd15a4
Evan Hallmark <ehallmark@gttgrp.com>
2017-10-06 13:03:13 -0700

    c

--- src/user_interface/server/SimilarPatentServer.java
+++ src/user_interface/server/SimilarPatentServer.java
@@ -1216,8 +1216,8 @@ public class SimilarPatentServer {
                                                         )
                                                 ),
                                                 div().withClass("col-12 tab-pane fade").attr("role","tabpanel").withId("tab2").with(
-                                                        div().withClass("collapsible-form row").with(
-                                                                div().withClass("col-12 attributeElement").withId("highlightForm").with(
+                                                        div().withClass("collapsible-form row").withId("highlightForm").with(
+                                                                div().withClass("col-12 attributeElement").with(
                                                                         h5("Highlighting").attr("style","width: 100%;").with(
                                                                                 input().withId("main-options-"+USE_HIGHLIGHTER_FIELD).withClass("form-control").withType("checkbox").attr("style","margin-top: 5px; margin-left: auto; width: 20px; margin-right: auto;").withValue("on").attr("checked","checked").withName(USE_HIGHLIGHTER_FIELD)
                                                                         )
