0461ce910986545cab6ee8902827f84fb5f66079
Evan Hallmark <ehallmark@gttgrp.com>
2017-10-19 11:10:25 -0700

    c

--- public/js/defaults.js
+++ public/js/defaults.js
@@ -89,7 +89,7 @@ $(document).ready(function() {
          $("#attributesForm .attributeElement").each(function() {
             var name = $(this).attr('data-model');
             if(typeof name === 'undefined') return;
-            var index = $(this).attr('index');
+            var index = $(this).index();
             var $hiddenOrder = $('<input class="hidden-remove" type="hidden" name="order_'+ name +'" value="'+ index+'" />');
             $form.append($hiddenOrder);
          });
@@ -297,17 +297,7 @@ $(document).ready(function() {
 
     resetSearchForm();
 
-    $('.nested-form-list').sortable({
-        update: function(event, ui) {
-            var pos = ui.item.index();
-            $(ui.item).children().attr('index',pos);
-        }
-    }).each(function() {
-        $(this).children().each(function(i,elem) {
-            $(elem).children().attr('index',i);
-        });
-    });
-
+    $('.nested-form-list').sortable();
     $('.nested-form-list').disableSelection();
 
     $('#main-content-id').addClass('show');
