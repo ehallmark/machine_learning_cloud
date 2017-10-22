28a02f5e0a3301a11f659eddd56771827a0f08be
Evan Hallmark <ehallmar@usc.edu>
2017-10-22 11:13:55 -0700

    c

--- src/models/keyphrase_prediction/CPCKeywordModel.java
+++ src/models/keyphrase_prediction/CPCKeywordModel.java
@@ -7,6 +7,7 @@ import elasticsearch.DataIngester;
 import elasticsearch.DataSearcher;
 import models.keyphrase_prediction.models.Model;
 import models.keyphrase_prediction.models.TimeDensityModel;
+import models.keyphrase_prediction.scorers.TechnologyScorer;
 import models.keyphrase_prediction.stages.*;
 import org.elasticsearch.action.search.SearchRequestBuilder;
 import org.elasticsearch.action.search.SearchResponse;
@@ -23,7 +24,9 @@ import org.elasticsearch.search.sort.SortBuilders;
 import seeding.Constants;
 import seeding.Database;
 import tools.ClassCodeHandler;
+import tools.OpenMapBigRealMatrix;
 import user_interface.ui_models.portfolios.items.Item;
+import util.Pair;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -32,6 +35,7 @@ import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 /**
@@ -61,9 +65,29 @@ public class CPCKeywordModel {
             }
         });
         System.out.println("Missing: "+missing.get());
-
         List<String> allWords = cpcToTitleMap.values().parallelStream().flatMap(t-> Stream.of(t.split("\\s+"))).distinct().collect(Collectors.toList());
 
+        Map<String,Integer> cpcToIdx = IntStream.range(0,CPCs.size()).mapToObj(i->new Pair<>(CPCs.get(i),i)).collect(Collectors.toMap(p->p._1, p->p._2));
+        Map<String,Integer> wordToIdx = IntStream.range(0,allWords.size()).mapToObj(i->new Pair<>(allWords.get(i),i)).collect(Collectors.toMap(p->p._1, p->p._2));
+
+        OpenMapBigRealMatrix matrix = new OpenMapBigRealMatrix(allWords.size(),CPCs.size());
+        CPCs.forEach(cpc->{
+           int cpcIdx = cpcToIdx.get(cpc);
+           titlesTrie.getValuesForKeysStartingWith(cpc).forEach(title->{
+               for(String word : title.split("\\s+")) {
+                   matrix.addToEntry(wordToIdx.get(word), cpcIdx, 1d);
+               }
+           });
+        });
+
+        Collection<MultiStem> stems = allWords.stream().map(word->new MultiStem(new String[]{word},wordToIdx.get(word))).collect(Collectors.toList());
+        System.out.println("Stems before: "+stems.size());
+        Map<MultiStem,Double> stemMap = new TechnologyScorer().scoreKeywords(stems,matrix);
+        System.out.println("Found: "+stemMap.size());
+
+        stemMap.entrySet().stream().sorted((e1,e2)->e2.getValue().compareTo(e1.getValue())).limit(30).forEach(word->{
+            System.out.println("BEST: "+word.getKey());
+        });
     }
 
 }
