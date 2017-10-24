e0e1ae0361036eff2683d3184b7cec613bc36fb4
Evan Hallmark <ehallmar@usc.edu>
2017-10-24 07:26:39 -0700

    c

---
+++ src/cpc_normalization/CPC.java
@@ -0,0 +1,80 @@
+package cpc_normalization;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by Evan on 10/24/2017.
+ */
+public class CPC {
+    @Getter
+    private Set<CPC> children;
+    @Getter @Setter
+    private CPC parent;
+    @Getter
+    private String name;
+    @Getter
+    private String[] parts;
+    public CPC(@NonNull String name) {
+        this.name=name;
+        this.children = new HashSet<>();
+        this.parts = cpcToParts(name);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if(other==null)return false;
+        return this.toString().equals(other.toString());
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public void addChild(CPC child) {
+        children.add(child);
+    }
+
+    public static String[] cpcToParts(String cpc) {
+        String[] parts = new String[5];
+        if(cpc.length()>=1) parts[0] = cpc.substring(0,1);
+        if(cpc.length()>=3) parts[1] = cpc.substring(1,3);
+        if(cpc.length()>=4) parts[2] = cpc.substring(3,4);
+        if(cpc.length()>4) {
+            String groupStr = cpc.substring(4);
+            String[] groups = groupStr.split("/");
+            for(int i = 0; i < groups.length; i++) {
+                parts[3+i] = groups[i].trim();
+            }
+        }
+        return parts;
+    }
+
+    public boolean isParentOf(CPC child) {
+        String[] parentParts = parts;
+        String[] childParts = child.parts;
+
+        if(parentParts.length!=childParts.length-1) return false;
+
+        boolean same = true;
+        for(int i = 0; i < 5; i++) {
+            if(parentParts[i]==null||childParts[i]==null) break;
+            if(!parentParts[i].equals(childParts[i])) {
+                same = false;
+                break;
+            }
+        }
+
+        return same;
+    }
+}
--- src/cpc_normalization/CPCHierarchy.java
+++ src/cpc_normalization/CPCHierarchy.java
@@ -8,7 +8,7 @@ import seeding.Constants;
 import seeding.Database;
 
 import java.io.File;
-import java.util.Collection;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
@@ -16,24 +16,31 @@ import java.util.stream.Collectors;
  * Created by Evan on 10/24/2017.
  */
 public class CPCHierarchy {
-    private static final File cpcHierarchyFile = new File(Constants.DATA_FOLDER+"cpc_hierarchy.jobj");
+    private static final File cpcHierarchyTopLevelFile = new File(Constants.DATA_FOLDER+"cpc_toplevel_hierarchy.jobj");
+    private static final File cpcHiearchyMapFile = new File(Constants.DATA_FOLDER+"cpc_map_hierarchy.jobj");
     @Getter
-    protected BayesianNet graph;
+    protected Collection<CPC> topLevel;
+    protected Map<String,CPC> labelToCPCMap;
     public CPCHierarchy() {
     }
 
     public void run(Collection<String> allCPCs) {
-        graph = new BayesianNet();
-        Collection<Node> allNodes = allCPCs.stream().map(cpc->{
-            return graph.addBinaryNode(cpc);
+        topLevel = Collections.synchronizedCollection(new HashSet<>());
+        labelToCPCMap = Collections.synchronizedMap(new HashMap<>());
+
+        Collection<CPC> allNodes = allCPCs.parallelStream().map(cpc->{
+            CPC c = new CPC(cpc);
+            labelToCPCMap.put(cpc,c);
+            return c;
         }).collect(Collectors.toList());
 
         AtomicInteger i = new AtomicInteger(0);
         allNodes.parallelStream().forEach(n1->{
             allNodes.forEach(n2->{
                 if(!n1.equals(n2)) {
-                    if(isParentOf(n1.getLabel(),n2.getLabel())) {
-                        graph.connectNodes(n1,n2);
+                    if(n1.isParentOf(n2)) {
+                        n2.setParent(n1);
+                        n1.addChild(n2);
                         if(i.getAndIncrement() % 10000==9999) {
                             System.out.println("Completed "+i.get()+" cpcs.");
                         }
@@ -44,46 +51,19 @@ public class CPCHierarchy {
     }
 
     public void save() {
-        Database.trySaveObject(graph,cpcHierarchyFile);
+        Database.trySaveObject(topLevel,cpcHierarchyTopLevelFile);
+        Database.trySaveObject(labelToCPCMap,cpcHiearchyMapFile);
     }
 
     public void loadGraph() {
-        graph = (BayesianNet) Database.tryLoadObject(cpcHierarchyFile);
+        topLevel = (Collection<CPC> ) Database.tryLoadObject(cpcHierarchyTopLevelFile);
+        labelToCPCMap = (Map<String,CPC>) Database.tryLoadObject(cpcHiearchyMapFile);
+
     }
 
-    private static String[] cpcToParts(String cpc) {
-        String[] parts = new String[5];
-        if(cpc.length()>=1) parts[0] = cpc.substring(0,1);
-        if(cpc.length()>=3) parts[1] = cpc.substring(1,3);
-        if(cpc.length()>=4) parts[2] = cpc.substring(3,4);
-        if(cpc.length()>4) {
-            String groupStr = cpc.substring(4);
-            if(groupStr.endsWith("/00")) groupStr = groupStr.substring(0,groupStr.length()-3); // main group
-            String[] groups = groupStr.split("/");
-            for(int i = 0; i < groups.length; i++) {
-                parts[3+i] = groups[i].trim();
-            }
-        }
-        return parts;
-    }
 
-    private static boolean isParentOf(String parent, String child) {
-        String[] parentParts = cpcToParts(parent);
-        String[] childParts = cpcToParts(child);
 
-        if(parent.length()>=child.length()) return false;
 
-        boolean same = true;
-        for(int i = 0; i < 5; i++) {
-            if(parentParts[i]==null||childParts[i]==null) break;
-            if(!parentParts[i].equals(childParts[i])) {
-                same = false;
-                break;
-            }
-        }
-
-        return same;
-    }
 
     public static void main(String[] args) {
         CPCHierarchy hierarchy = new CPCHierarchy();
