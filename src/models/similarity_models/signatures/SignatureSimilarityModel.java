4fd5d046ba6afc91b9b406ad2db9de0b2f0a8d6c
Evan Hallmark <ehallmark@gttgrp.com>
2017-10-26 13:16:03 -0700

    c

--- src/models/similarity_models/signatures/SignatureSimilarityModel.java
+++ src/models/similarity_models/signatures/SignatureSimilarityModel.java
@@ -41,6 +41,7 @@ public class SignatureSimilarityModel {
     private Map<String,? extends Collection<CPC>> cpcMap;
     private List<String> allAssets;
     private List<String> testAssets;
+    private List<String> smallTestSet;
     private List<String> trainAssets;
     private MultiLayerNetwork net;
     private int numInputs;
@@ -54,10 +55,12 @@ public class SignatureSimilarityModel {
     }
 
     public void init() {
-        allAssets = allAssets.parallelStream().filter(asset->cpcMap.containsKey(asset)).collect(Collectors.toList());
+        allAssets = new ArrayList<>(allAssets.parallelStream().filter(asset->cpcMap.containsKey(asset)).collect(Collectors.toList()));
+        Collections.shuffle(allAssets);
         Random rand = new Random(69);
         testAssets = Collections.synchronizedList(new ArrayList<>());
         trainAssets = Collections.synchronizedList(new ArrayList<>());
+        smallTestSet = Collections.synchronizedList(new ArrayList<>());
         System.out.println("Splitting test and train");
         allAssets.parallelStream().forEach(asset->{
             if(rand.nextBoolean()&&rand.nextBoolean()) {
@@ -66,6 +69,8 @@ public class SignatureSimilarityModel {
                 trainAssets.add(asset);
             }
         });
+        smallTestSet.addAll(testAssets.subList(0,1000));
+
         {
             AtomicInteger idx = new AtomicInteger(0);
             cpcToIdxMap = hierarchy.getLabelToCPCMap().entrySet().parallelStream().filter(e -> e.getValue().getNumParts() <= MAX_CPC_DEPTH).collect(Collectors.toMap(e -> e.getKey(), e -> idx.getAndIncrement()));
@@ -75,15 +80,17 @@ public class SignatureSimilarityModel {
         System.out.println("Finished splitting test and train.");
     }
 
-    private Stream<DataSet> getIterator(boolean train) {
-        List<String> assets = (train ? trainAssets : testAssets);
-        Collections.shuffle(assets);
+    private Stream<DataSet> getIterator(List<String> assets) {
+        if(assets.equals(trainAssets)) {
+            System.out.println("Shuffling training data...");
+            Collections.shuffle(assets);
+        }
         return IntStream.range(0,assets.size()/batchSize).parallel().mapToObj(i->{
             INDArray features = createVector(IntStream.range(i,i+batchSize).mapToObj(j->{
                 String asset = assets.get(j);
                 return cpcMap.get(asset);
             }));
-            return new DataSet(features, features.dup());
+            return new DataSet(features, features);
         }).sequential();
     }
 
@@ -144,31 +151,42 @@ public class SignatureSimilarityModel {
         int printIterations = 10000;
         AtomicInteger iterationCount = new AtomicInteger(0);
         for (int i = 0; i < nEpochs; i++) {
-            Stream<DataSet> trainIter = getIterator(true);
+            Stream<DataSet> trainIter = getIterator(trainAssets);
             System.out.println("Starting epoch {"+(i+1)+"} of {"+nEpochs+"}");
             trainIter.forEach(ds->{
                 net.fit(ds);
                 if(iterationCount.get() % 1000 == 999) {
-                    System.out.print("Completed: "+iterationCount.get());
+                    test(getIterator(smallTestSet),vae);
+                    System.out.println("Completed: "+iterationCount.get()+ " batches.");
                 }
                 //Every N=100 minibatches:
                 // (a) collect the test set latent space values for later plotting
                 // (b) collect the reconstructions at each point in the grid
                 if (iterationCount.getAndIncrement() % printIterations == printIterations-1) {
-                    System.out.println("Testing...");
-                    Stream<DataSet> testIter = getIterator(false);
-                    AtomicDouble testError = new AtomicDouble(0d);
-                    testIter.forEach(test->{
-                        INDArray testInput = test.getFeatures();
-                        INDArray latentSpace = vae.activate(testInput,false);
-                        INDArray testOutput = vae.generateAtMeanGivenZ(latentSpace);
-                        double error = 1d - Transforms.cosineSim(testInput,testOutput);
-                        testError.addAndGet(error);
-                    });
-                    System.out.println("Overall test error: "+testError.get()/testAssets.size());
+
                 }
             });
         }
+        System.out.println("Testing overall model");
+        test(getIterator(testAssets),vae);
+    }
+
+    private void test(Stream<DataSet> dataStream, org.deeplearning4j.nn.layers.variational.VariationalAutoencoder vae) {
+        System.out.println("Testing...");
+        AtomicDouble testError = new AtomicDouble(0d);
+        AtomicInteger cnt = new AtomicInteger(0);
+        dataStream.forEach(test->{
+            INDArray testInput = test.getFeatures();
+            INDArray latentSpace = vae.activate(testInput,false);
+            INDArray testOutput = vae.generateAtMeanGivenZ(latentSpace);
+            for(int i = 0; i < batchSize; i++) {
+                double sim = Transforms.cosineSim(testInput.getRow(i), testOutput.getRow(i));
+                if(Double.isNaN(sim)) sim = -1d;
+                testError.addAndGet(1d-sim);
+                cnt.getAndIncrement();
+            }
+        });
+        System.out.println("Test Error for "+cnt.get()+" assets: "+testError.get()/cnt.get());
     }
 
     public void save() throws IOException {
@@ -178,7 +196,7 @@ public class SignatureSimilarityModel {
     }
 
     public static void main(String[] args) throws Exception {
-        int batchSize = 5;
+        int batchSize = 20;
 
         Map<String,Set<String>> patentToCPCStringMap = Collections.synchronizedMap(new HashMap<>());
         patentToCPCStringMap.putAll(new AssetToCPCMap().getApplicationDataMap());
