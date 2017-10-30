c6478626d035be23d5b921e322e39768e6ca4546
Evan Hallmark <ehallmark@gttgrp.com>
2017-10-30 10:40:56 -0700

    c

--- src/models/similarity_models/signatures/TestVectors.java
+++ src/models/similarity_models/signatures/TestVectors.java
@@ -16,7 +16,7 @@ import java.util.stream.IntStream;
  */
 public class TestVectors {
     public static void main(String[] args) {
-        CPCSimilarityVectorizer vectorizer = new CPCSimilarityVectorizer();
+        CPCSimilarityVectorizer vectorizer = new CPCSimilarityVectorizer(true);
 
         List<String> assets = vectorizer.getAssetToIdxMap().entrySet().stream().map(e->e.getKey()).limit(20).collect(Collectors.toList());
 
