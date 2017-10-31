1dab09bcbee0c75d6a43e2b84cfc238f9f1f125e
Evan Hallmark <ehallmark@gttgrp.com>
2017-10-31 14:33:45 -0700

    c

--- src/models/similarity_models/signatures/NDArrayHelper.java
+++ src/models/similarity_models/signatures/NDArrayHelper.java
@@ -1,8 +1,16 @@
 package models.similarity_models.signatures;
 
+import org.nd4j.linalg.api.buffer.DoubleBuffer;
 import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.api.ops.RandomOp;
+import org.nd4j.linalg.api.ops.ScalarOp;
+import org.nd4j.linalg.api.ops.impl.broadcast.BroadcastCopyOp;
+import org.nd4j.linalg.api.ops.impl.broadcast.BroadcastMulOp;
+import org.nd4j.linalg.api.rng.distribution.impl.NormalDistribution;
 import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.linalg.ops.transforms.Transforms;
+
+import java.util.stream.IntStream;
 
 /**
  * Created by ehallmark on 10/31/17.
@@ -19,16 +27,22 @@ public class NDArrayHelper {
     }
 
     public static INDArray createProbabilityVectorFromGaussian(INDArray in) {
-        return Transforms.exp(Transforms.pow(in,2,true).negi().divi(2),false).divi(ROOT_2_PI);
+        NormalDistribution distribution = new NormalDistribution(0,1);
+        int[] shape = in.shape();
+        double[] data = in.data().asDouble();
+        IntStream.range(0,data.length).parallel().forEach(i->{
+            data[i] = distribution.cumulativeProbability(data[i]);
+        });
+        return Nd4j.create(data,shape);
     }
 
     public static void main(String[] args) {
         // test
         INDArray vec = Nd4j.create(new double[][]{
                 new double[]{
-                        0, -1, 2, 5, 0.1, -0.0001, 0.5
+                        0, -1, 2, 5, 0.1, -0.0001, 0.5, -2
                 }, new double[] {
-                        0, -1, 2, 5, 0.1, -0.0001, 0.5
+                        0, -1, 2, 5, 0.1, -0.0001, 0.5, -2
                 }
         });
 
