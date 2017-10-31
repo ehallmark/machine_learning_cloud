5a90198c07556bbe0ebf7466c2913b5ee916c978
Evan Hallmark <ehallmark@gttgrp.com>
2017-10-31 16:30:49 -0700

    c

--- scripts/production/keyword-graph.sh
+++ scripts/production/keyword-graph.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 source /home/ehallmark1122/machine_learning_cloud/scripts/production/helper/init.sh
 source /home/ehallmark1122/machine_learning_cloud/scripts/production/helper/setup-elasticsearch.sh
-sudo -u ehallmark1122 bash -c '    java -cp target/classes:"target/dependency/*" -Xms50000m -Xmx50000m -Dcom.sun.management.jmxremote.port=4567 -Dcom.sun.management.jmxremote.rmi.port=4567 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=127.0.0.1 visualization.KeywordGraph &'
+sudo -u ehallmark1122 bash -c '    java -cp target/classes:"target/dependency/*" -Xms50000m -Xmx50000m -Dcom.sun.management.jmxremote.port=4567 -Dcom.sun.management.jmxremote.rmi.port=4567 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=127.0.0.1 visualization.KeywordGraph'
