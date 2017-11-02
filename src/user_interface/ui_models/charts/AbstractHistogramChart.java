f15e717bfb0eece0bfad762230e7e596cef64e1f
Evan Hallmark <ehallmar@usc.edu>
2017-11-01 19:45:18 -0700

    c

--- src/user_interface/ui_models/charts/AbstractHistogramChart.java
+++ src/user_interface/ui_models/charts/AbstractHistogramChart.java
@@ -97,7 +97,7 @@ public class AbstractHistogramChart extends ChartAttribute {
             final double _max = max;
             final int _nBins = nBins;
             final String _xAxisSuffix = xAxisSuffix;
-            return portfolioList.groupedBy(groupedBy).map(groupPair->{
+            return portfolioList.groupedBy(groupedBy).sorted((p1,p2)->Integer.compare(p2.getSecond().getItemList().size(),p1.getSecond().getItemList().size())).limit(10).map(groupPair->{
                 String name = groupPair.getFirst();
                 PortfolioList group = groupPair.getSecond();
                 return new ColumnChart(title, collectDistributionData(group.getItemList(),_min,_max,_nBins, attribute, title, categories), 0d, null, _xAxisSuffix, yAxisSuffix, humanAttr, humanSearchType, name,  0,categories);
