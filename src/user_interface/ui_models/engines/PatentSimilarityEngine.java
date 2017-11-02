2b6cc4f6d37d29bc35ce3d78f40ebf394998196c
Evan Hallmark <ehallmark@gttgrp.com>
2017-11-01 17:21:42 -0700

    c

--- src/user_interface/ui_models/engines/PatentSimilarityEngine.java
+++ src/user_interface/ui_models/engines/PatentSimilarityEngine.java
@@ -32,7 +32,7 @@ public class PatentSimilarityEngine extends AbstractSimilarityEngine {
         System.out.println("Collecting inputs to search for...");
         // get input data
         Collection<String> patents = preProcess(extractString(req, PATENTS_TO_SEARCH_FOR_FIELD, ""), "\\s+", "[^0-9]");
-        patents = patents.stream().map(patent->assetToFilingMap.getPatentDataMap().getOrDefault(patent, assetToFilingMap.getApplicationDataMap().getOrDefault(patent,patent))).collect(Collectors.toList());
+        patents = patents.stream().filter(patent->assetToFilingMap.getPatentDataMap().containsKey(patent)||assetToFilingMap.getApplicationDataMap().containsKey(patent)).collect(Collectors.toList());
         System.out.println("Found "+patents.size()+" patents...");
         return patents;
     }
