ccdefb7c21dc95ca00c382feedb596d471026e0c
Evan Hallmark <ehallmark@gttgrp.com>
2017-11-14 09:41:38 -0800

    c

--- src/models/similarity_models/word_to_cpc_encoding_model/WordToCPCPipelineManager.java
+++ src/models/similarity_models/word_to_cpc_encoding_model/WordToCPCPipelineManager.java
@@ -126,7 +126,7 @@ public class WordToCPCPipelineManager extends DefaultPipelineManager<INDArray> {
         Nd4j.setDataType(DataBuffer.Type.FLOAT);
         boolean rebuildDatasets = false;
         boolean runModels = true;
-        boolean forceRecreateModels = false;
+        boolean forceRecreateModels = true;
         boolean runPredictions = false; // NO PREDICTIONS FOR THIS MODEL
         boolean rebuildPrerequisites = false;
 
