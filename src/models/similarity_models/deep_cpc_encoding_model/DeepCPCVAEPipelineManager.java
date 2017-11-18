099bfc6e48656d34db0ab7c2ec945a868c9dc9fc
Evan Hallmark <ehallmar@usc.edu>
2017-11-18 11:04:24 -0800

    c

--- src/models/similarity_models/deep_cpc_encoding_model/DeepCPCVAEPipelineManager.java
+++ src/models/similarity_models/deep_cpc_encoding_model/DeepCPCVAEPipelineManager.java
@@ -29,7 +29,7 @@ public class DeepCPCVAEPipelineManager extends DefaultPipelineManager<INDArray> 
     public static final String MODEL_NAME = "deep_cpc_autoencoder";
     public static final int MAX_CPC_DEPTH = 5;
     private static final int BATCH_SIZE = 64;
-    private static final int MIN_CPC_APPEARANCES = 2;
+    private static final int MIN_CPC_APPEARANCES = 10;
     private static final File INPUT_DATA_FOLDER = new File("deep_cpc_vae_data");
     private static final File PREDICTION_DATA_FILE = new File(Constants.DATA_FOLDER+"deep_cpc_vae_predictions/predictions_map.jobj");
     private Map<String,? extends Collection<CPC>> cpcMap;
