e76797f5160e28e6fa4e50448e16179c6fb3a683
Evan Hallmark <ehallmar@usc.edu>
2017-11-19 18:48:42 -0800

    c

--- src/models/text_streaming/BOWVectorFromTextTransformer.java
+++ src/models/text_streaming/BOWVectorFromTextTransformer.java
@@ -1,5 +1,6 @@
 package models.text_streaming;
 
+import com.google.common.util.concurrent.AtomicDouble;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.DataSet;
 import org.nd4j.linalg.dataset.api.DataSetPreProcessor;
@@ -9,6 +10,8 @@ import org.nd4j.linalg.factory.Nd4j;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -24,13 +27,16 @@ public class BOWVectorFromTextTransformer implements Function<Collection<String>
     @Override
     public INDArray apply(Collection<String> tokens) {
         INDArray vec = Nd4j.zeros(wordToIdxMap.size());
+        AtomicLong cnt = new AtomicLong(0);
         tokens.stream()
                 .filter(word->wordToIdxMap.containsKey(word))
                 .collect(Collectors.groupingBy(word->word,Collectors.counting()))
                 .entrySet().forEach(e->{
                     int idx = wordToIdxMap.get(e.getKey());
+                    cnt.getAndAdd(e.getValue());
                     vec.putScalar(idx,e.getValue());
                 });
+        if(cnt.get()>0) vec.divi(cnt);
         return vec;
     }
 }
