87a3653cc378c5c624ea5d61aa03e9d2b776a527
Evan Hallmark <ehallmark@gttgrp.com>
2017-11-20 17:56:59 -0800

    c

--- src/models/text_streaming/BOWVectorFromCountMapTransformer.java
+++ src/models/text_streaming/BOWVectorFromCountMapTransformer.java
@@ -23,9 +23,11 @@ public class BOWVectorFromCountMapTransformer implements Function<Map<String,Int
         INDArray vec = Nd4j.zeros(wordToIdxMap.size());
         AtomicLong cnt = new AtomicLong(0);
         tokenCounts.entrySet().forEach(e->{
-                    int idx = wordToIdxMap.get(e.getKey());
-                    cnt.getAndAdd(e.getValue());
-                    vec.putScalar(idx,e.getValue());
+                    Integer idx = wordToIdxMap.get(e.getKey());
+                    if(idx!=null) {
+                        cnt.getAndAdd(e.getValue());
+                        vec.putScalar(idx, e.getValue());
+                    }
                 });
         if(cnt.get()>0) vec.divi(cnt);
         return vec;
