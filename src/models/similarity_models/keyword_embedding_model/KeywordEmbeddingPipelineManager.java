d7ab79349bc2d9cedad9684f3459e8b1883f294a
Evan Hallmark <ehallmark@gttgrp.com>
2017-11-21 09:54:43 -0800

    c

--- src/models/similarity_models/keyword_embedding_model/KeywordEmbeddingPipelineManager.java
+++ src/models/similarity_models/keyword_embedding_model/KeywordEmbeddingPipelineManager.java
@@ -5,10 +5,16 @@ import data_pipeline.vectorize.DataSetManager;
 import models.keyphrase_prediction.KeywordModelRunner;
 import org.nd4j.linalg.api.ndarray.INDArray;
 
+import java.io.File;
+
 /**
  * Created by ehallmark on 11/21/17.
  */
 public class KeywordEmbeddingPipelineManager extends DefaultPipelineManager<INDArray> {
+    public KeywordEmbeddingPipelineManager(File dataFolder, File finalPredictionsFile) {
+        super(dataFolder, finalPredictionsFile);
+    }
+
     @Override
     public DataSetManager getDatasetManager() {
         return null;
