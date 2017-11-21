4e26a6f6cf147d58aa6559b952c627defb0dc1cb
Evan Hallmark <ehallmar@usc.edu>
2017-11-21 08:01:25 -0800

    c

--- src/models/keyphrase_prediction/scorers/TechnologyScorer.java
+++ src/models/keyphrase_prediction/scorers/TechnologyScorer.java
@@ -5,6 +5,7 @@ import org.apache.commons.math3.linear.RealMatrix;
 
 import java.util.Set;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
@@ -22,18 +23,22 @@ public class TechnologyScorer implements KeywordScorer {
         double[] squaredRowSums = new double[length];
         double[] sumOfSquares = new double[length];
         double[] sums = new double[length];
+        AtomicInteger idx = new AtomicInteger(0);
         IntStream.range(0,length).parallel().forEach(i->{
             double[] row = matrix.getRow(i);
             sums[i] = DoubleStream.of(row).sum();
             squaredRowSums[i] = Math.pow(sums[i],2);
             sumOfSquares[i] = DoubleStream.of(row).map(d->d*d).sum();
+            if(idx.getAndIncrement()%10000==9999)System.out.println("Finished sumOfSquares: "+idx.get());
         });
 
+        idx.set(0);
         double[] scores = new double[sumOfSquares.length];
         IntStream.range(0,squaredRowSums.length).parallel().forEach(i->{
             double score = sumOfSquares[i]/squaredRowSums[i];
             if(Double.isNaN(score)) score = 0d;
             scores[i]=score;
+            if(idx.getAndIncrement()%10000==9999) System.out.println("Finished scoring: "+idx.get());
         });
         return keywords.parallelStream().collect(Collectors.toMap(keyword->keyword,keyword->scores[keyword.getIndex()]));
     }
---
+++ src/models/keyphrase_prediction/stages/ValidWordStage.java
@@ -0,0 +1,71 @@
+package models.keyphrase_prediction.stages;
+
+import models.keyphrase_prediction.KeywordModelRunner;
+import models.keyphrase_prediction.MultiStem;
+import models.keyphrase_prediction.models.Model;
+import models.keyphrase_prediction.scorers.KeywordScorer;
+import models.keyphrase_prediction.scorers.TermhoodScorer;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.LineIterator;
+import org.apache.commons.math3.linear.RealMatrix;
+import seeding.Database;
+
+import java.io.File;
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * Created by Evan on 11/21/2017.
+ */
+public class ValidWordStage extends Stage<Set<MultiStem>>  {
+    private static final File dictionaryFile = new File("data/word_list.txt");
+    public ValidWordStage(Collection<MultiStem> multiStems, Model model) {
+        super(model);
+        this.data = new HashSet<>(multiStems);
+    }
+
+    @Override
+    public Set<MultiStem> run(boolean alwaysRerun) {
+        if(alwaysRerun || !getFile().exists()) {
+            // apply filter 2
+            System.out.println("Num keywords before valid word stage: " + data.size());
+            System.out.println("Reading dictionary...");
+            Set<String> dictionary = Collections.synchronizedSet(new HashSet<>());
+            try {
+                LineIterator iter = FileUtils.lineIterator(dictionaryFile);
+                while (iter.hasNext()) {
+                    dictionary.add(iter.next().toLowerCase().trim());
+                }
+                LineIterator.closeQuietly(iter);
+                System.out.println("Finished reading dictionary. Size: " + dictionary.size());
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+
+            data = data.parallelStream().filter(stem->{
+                String bestPhrase = stem.getBestPhrase();
+                String[] words = bestPhrase.split("\\s+");
+                boolean valid = true;
+                for(String word : words) {
+                    if(!dictionary.contains(word)) {
+                        valid = false;
+                        break;
+                    }
+                }
+                return valid;
+            }).collect(Collectors.toSet());
+            System.out.println("Num keywords after valid word stage: " + data.size());
+
+            Database.saveObject(data, getFile());
+            // write to csv for records
+            KeywordModelRunner.writeToCSV(data, new File(getFile().getAbsoluteFile() + ".csv"));
+        } else {
+            try {
+                loadData();
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return data;
+    }
+}
