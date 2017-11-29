afdc8327b820e5282e85ef72a9204d822c093249
Evan Hallmark <ehallmark@gttgrp.com>
2017-11-28 20:06:49 -0800

    c

--- scripts/production/elasticsearch-no-init.sh
+++ scripts/production/elasticsearch-no-init.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-source /home/ehallmark1122/machine_learning_cloud/scripts/production/helper/init.sh
 source /home/ehallmark1122/machine_learning_cloud/scripts/production/helper/setup-elasticsearch.sh
 source /home/ehallmark1122/machine_learning_cloud/scripts/production/helper/recreate-elasticsearch.sh
