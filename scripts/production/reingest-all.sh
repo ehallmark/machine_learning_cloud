9b9b6a32e586c5b04635ff13fc22b232ec484e48
Evan Hallmark <ehallmar@usc.edu>
2017-12-03 09:32:48 -0800

    c

--- scripts/production/reingest-all.sh
+++ scripts/production/reingest-all.sh
@@ -20,10 +20,10 @@ sleep 10s
 cd /home/ehallmark1122/machine_learning_cloud
 
 # run similarity encoding (with more memory)
-sudo -u ehallmark1122 bash -c '    java -cp target/classes:"target/dependency/*" -Xms100000m -Xmx100000m -Dcom.sun.management.jmxremote.port=4567 -Dcom.sun.management.jmxremote.rmi.port=4567 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=127.0.0.1 models.similarity_models.UpdateSimilarityModels'
+sudo -u ehallmark1122 bash -c '    java -cp target/classes:"target/dependency/*" -Xms160000m -Xmx160000m -Dcom.sun.management.jmxremote.port=4567 -Dcom.sun.management.jmxremote.rmi.port=4567 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=127.0.0.1 models.similarity_models.UpdateSimilarityModels'
 
 # reingest computable attributes
-sudo -u ehallmark1122 bash -c '    java -cp target/classes:"target/dependency/*" -Xms100000m -Xmx100000m -Dcom.sun.management.jmxremote.port=4567 -Dcom.sun.management.jmxremote.rmi.port=4567 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=127.0.0.1 seeding.ai_db_updater.UpdateAll 11 '
+sudo -u ehallmark1122 bash -c '    java -cp target/classes:"target/dependency/*" -Xms80000m -Xmx80000m -Dcom.sun.management.jmxremote.port=4567 -Dcom.sun.management.jmxremote.rmi.port=4567 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=127.0.0.1 seeding.ai_db_updater.UpdateAll 11 '
 
 # turn on elasticsearch
 source /home/ehallmark1122/machine_learning_cloud/scripts/production/helper/setup-elasticsearch.sh
