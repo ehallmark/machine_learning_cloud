44611d1d8517ac648b5c254c371fd0747bdb3a76
Evan Hallmark <ehallmark@gttgrp.com>
2017-12-07 13:53:20 -0800

    c

--- src/tools/ClassCodeHandler.java
+++ src/tools/ClassCodeHandler.java
@@ -11,9 +11,17 @@ public class ClassCodeHandler {
 
     public static String convertToHumanFormat(String code) {
         if(code==null)return null;
-        if(code.length()<14) return code.replace(" ","");
-        String cpc = code.substring(0,4).trim()+code.substring(4,8).trim() + "/" + code.substring(8,14).trim();
-        return cpc.replace(" ","");
+        if(code.length()<14) return null;
+
+        String g2 = code.substring(4,8).trim();
+        while(g2.startsWith("0")&&g2.length()>0) g2 = g2.substring(1);
+
+        String g3 = code.substring(8,14).trim();
+        while(g3.startsWith("0")&&g3.length()>0) g3 = g3.substring(1);
+
+        String cpc = code.substring(0,4) + g2 + "/" + g3;
+
+        return cpc;
     }
 
 }
