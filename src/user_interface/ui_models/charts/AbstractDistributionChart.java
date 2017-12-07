484d955e7b3dbf2d4ad0eda88a57d2f9b8931b74
Evan Hallmark <ehallmark@gttgrp.com>
2017-12-07 13:47:35 -0800

    c

--- src/user_interface/ui_models/charts/AbstractDistributionChart.java
+++ src/user_interface/ui_models/charts/AbstractDistributionChart.java
@@ -3,6 +3,7 @@ package user_interface.ui_models.charts;
 import com.googlecode.wickedcharts.highcharts.options.series.Point;
 import com.googlecode.wickedcharts.highcharts.options.series.PointSeries;
 import com.googlecode.wickedcharts.highcharts.options.series.Series;
+import elasticsearch.DataSearcher;
 import j2html.tags.Tag;
 import org.nd4j.linalg.primitives.Pair;
 import seeding.Constants;
@@ -68,21 +69,35 @@ public class AbstractDistributionChart extends ChartAttribute {
 
         if(portfolio.getItemList().size()==0) return Collections.emptyList();
 
-        List<String> items = portfolio.getItemList().stream().map(item->{
-            return (String)item.getData(attribute);
+        List<Object> items = portfolio.getItemList().stream().flatMap(item-> {
+            Object r = item.getData(attribute);
+            if (r != null) {
+                if (r instanceof Collection) {
+                    return ((Collection) r).stream();
+                } else if (r.toString().contains(DataSearcher.ARRAY_SEPARATOR)) {
+                    return Stream.of(r.toString().split(DataSearcher.ARRAY_SEPARATOR));
+                } else {
+                    return Stream.of(r);
+                }
+            }
+            return Stream.empty();
         }).filter(attr->attr!=null).collect(Collectors.toList());
 
         if(items.isEmpty()) return Collections.emptyList();
 
         int limit = 15;
-        List<Pair<String,Long>> itemPairs = items.stream().collect(Collectors.groupingBy(t->t,Collectors.counting())).entrySet().stream().sorted((e1, e2)->e2.getValue().compareTo(e1.getValue())).map(e->new Pair<>(e.getKey(),e.getValue())).collect(Collectors.toList());
+        List<Pair<Object,Long>> itemPairs = items.stream()
+                .collect(Collectors.groupingBy(t->t,Collectors.counting()))
+                .entrySet().stream().sorted((e1, e2)->e2.getValue().compareTo(e1.getValue()))
+                .map(e->new Pair<>(e.getKey(),e.getValue())).collect(Collectors.toList());
+
         if(itemPairs.size()>limit+5) {
-            Pair<String,Long> remaining = itemPairs.subList(limit,itemPairs.size()).stream().reduce((p1,p2)->new Pair<>("Remaining",p1.getSecond()+p2.getSecond())).get();
+            Pair<Object,Long> remaining = itemPairs.subList(limit,itemPairs.size()).stream().reduce((p1,p2)->new Pair<>("Remaining",p1.getSecond()+p2.getSecond())).get();
             itemPairs = itemPairs.subList(0,limit);
             itemPairs.add(remaining);
         }
         itemPairs.forEach(e->{
-            String tech = e.getFirst();
+            String tech = e.getFirst().toString();
             if(tech.isEmpty()) tech = "Unknown";
             double prob = e.getSecond();
             Point point = new Point(tech,prob);
