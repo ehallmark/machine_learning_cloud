4f8eb6dfd311ff5276178b73a577b7506424dcd8
Evan Hallmark <ehallmark@gttgrp.com>
2017-12-07 17:51:48 -0800

    c

--- src/tools/ClassCodeHandler.java
+++ src/tools/ClassCodeHandler.java
@@ -13,15 +13,13 @@ public class ClassCodeHandler {
         if(code==null)return null;
         if(code.length()<14) return null;
 
+        String g1 = code.substring(0,4);
+
         String g2 = code.substring(4,8).trim();
-        while(g2.startsWith("0")&&g2.length()>0) g2 = g2.substring(1);
 
         String g3 = code.substring(8,14).trim();
-        while(g3.startsWith("0")&&g3.length()>0) g3 = g3.substring(1);
 
-        String cpc = code.substring(0,4) + g2 + "/" + g3;
-        if(cpc.endsWith("/")) cpc = cpc.substring(0,cpc.length()-1);
-        return cpc;
+        return g1 + g2 + "/" + g3;
     }
 
 }
