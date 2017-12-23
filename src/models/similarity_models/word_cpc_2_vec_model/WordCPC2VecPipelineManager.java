dc14ad9049b907e30e64da73a4687350db82312c
Evan Hallmark <ehallmark@gttgrp.com>
2017-12-22 16:06:54 -0800

    c

--- src/models/similarity_models/word_cpc_2_vec_model/WordCPC2VecPipelineManager.java
+++ src/models/similarity_models/word_cpc_2_vec_model/WordCPC2VecPipelineManager.java
@@ -165,7 +165,7 @@ public class WordCPC2VecPipelineManager extends DefaultPipelineManager<WordCPCIt
 
         rebuildPrerequisites = rebuildPrerequisites || !Stage.getTransformedDataFolder().exists();
 
-        int nEpochs = 2; //10;
+        int nEpochs = 5; //10;
         String modelName = MODEL_NAME;
         WordCPC2VecPipelineManager pipelineManager = new WordCPC2VecPipelineManager(modelName,nEpochs,windowSize,maxSamples);
         pipelineManager.runPipeline(rebuildPrerequisites, rebuildDatasets, runModels, forceRecreateModels, nEpochs, runPredictions);
