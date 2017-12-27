0a2882bc417002b40ffc347ba1180c1532db40d5
Evan Hallmark <ehallmark@gttgrp.com>
2017-12-27 15:20:40 -0800

    c

--- src/models/similarity_models/combined_similarity_model/CombinedSimilarityModel.java
+++ src/models/similarity_models/combined_similarity_model/CombinedSimilarityModel.java
@@ -167,7 +167,7 @@ public class CombinedSimilarityModel extends CombinedNeuralNetworkPredictionMode
             return null;
         };
 
-        final int printIterations = 200;
+        final int printIterations = 500;
         final AtomicBoolean stoppingCondition = new AtomicBoolean(false);
 
         System.gc();
