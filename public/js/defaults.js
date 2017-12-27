51addaf9ae0517755b17598a97c4980dfcba2f1f
Evan Hallmark <ehallmar@usc.edu>
2017-12-27 09:11:40 -0800

    c

--- public/js/defaults.js
+++ public/js/defaults.js
@@ -51,8 +51,8 @@ $(document).ready(function() {
              $(window).scrollTop(tempScrollTop);
            },
            error: function(jqxhr,status,error) {
-             if(jqxhr.status==404) {
-                alert("Unable to establish connection to platform. Try refreshing page.");
+             if(jqxhr.status==404 || jqxhr.status==502) {
+                alert("Unable to establish connection to platform. Try refreshing page. Error code: "+jqxhr.status.toString());
              }
              $('#results .tab-pane .content').html('<div style="color: red;">Server error during ajax request:'+error+'</div>');
            },
