fab7ec174852003c13d7f0e7c44b950f09db8e59
Evan Hallmark <ehallmar@usc.edu>
2018-01-09 08:38:40 -0800

    c

--- src/user_interface/ui_models/attributes/script_attributes/SimilarityAttribute.java
+++ src/user_interface/ui_models/attributes/script_attributes/SimilarityAttribute.java
@@ -22,7 +22,7 @@ import static user_interface.server.SimilarPatentServer.extractArray;
  */
 public class SimilarityAttribute extends AbstractScriptAttribute implements DependentAttribute<AbstractScriptAttribute> {
     public static final int vectorSize = 32;
-    public static final int dimensionsForSort = 24;
+    public static final int dimensionsForSort = 32;
     public static final String VECTOR_NAME = "cvec";
 
     public static final String EXPRESSION_SIMILARITY_SCRIPT;
