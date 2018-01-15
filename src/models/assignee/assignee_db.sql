df0436a535ae19ca9e7c82ef7780c85d421b4eb1
Evan Hallmark <ehallmark@gttgrp.com>
2018-01-15 14:56:45 -0800

    c

--- src/models/assignee/assignee_db.sql
+++ src/models/assignee/assignee_db.sql
@@ -11,6 +11,8 @@ create table if not exists assignees_raw (
     human boolean
 );
 
+truncate assignees_raw;
+
 create index assignees_raw_normalized_name_idx on assignees_raw (normalized_name);
 
 select name, top[1] from (select name, array_agg(normalized_name) as top from assignees_raw where normalized_name is not null group by name,normalized_name order by name,count(*) desc) as temp limit 1000;
