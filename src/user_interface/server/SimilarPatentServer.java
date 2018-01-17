20bd8013934efe59254533ae1cce0d858ea6c291
Evan Hallmark <ehallmark@gttgrp.com>
2018-01-17 15:44:23 -0800

    c

--- src/user_interface/server/SimilarPatentServer.java
+++ src/user_interface/server/SimilarPatentServer.java
@@ -2675,11 +2675,14 @@ public class SimilarPatentServer {
         System.out.println("Starting user_interface.server...");
 
         // perform quick search
-        try {
-            DataSearcher.searchForAssets(attributesMap.values(),Collections.emptyList(),Constants.AI_VALUE, SortOrder.DESC,100,getNestedAttrMap(),false,true);
-        } catch(Exception e) {
-            System.out.println("Error during presearch: "+e.getMessage());
-        }
+        //try {
+        //    DataSearcher.searchForAssets(attributesMap.values(),Collections.emptyList(),Constants.AI_VALUE, SortOrder.DESC,100,getNestedAttrMap(),false,true);
+        //} catch(Exception e) {
+        //    System.out.println("Error during presearch: "+e.getMessage());
+        //}
+
+        pool.execute(keyphrasePredictionPipelineManagerTask);
+        pool.execute(similarityEngine);
 
         server();
 
@@ -2690,9 +2693,6 @@ public class SimilarPatentServer {
         }
         long t2 = System.currentTimeMillis();
         System.out.println("Time to start user_interface.server: "+ ((t2-t1)/(1000*60)) + " minutes");
-
-        pool.execute(keyphrasePredictionPipelineManagerTask);
-        pool.execute(similarityEngine);
 
     }
 }
