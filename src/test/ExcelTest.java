c276447eb4c0ebdba5055a197801275ba25c43f3
Evan Hallmark <ehallmark@gttgrp.com>
2018-01-23 14:10:28 -0800

    c

--- src/test/ExcelTest.java
+++ src/test/ExcelTest.java
@@ -1,41 +1,35 @@
 package test;
 
 import java.io.*;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.*;
+import java.util.stream.Stream;
 
 /**
  * Created by ehallmark on 12/13/17.
  */
 public class ExcelTest {
     public static void main(String[] args) throws Exception {
-        File excelFile = new File("ipel.csv");
-        File outputFile = new File("ipel-output.csv");
+        File excelFile = new File("huawei.csv");
+        File outputFile = new File("huawei-output.csv");
 
         BufferedReader reader = new BufferedReader(new FileReader(excelFile));
         BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile));
 
         Map<String,Set<String>> map = new HashMap<>();
-        AtomicReference<String> currentPatent = new AtomicReference<>(null);
         reader.lines().skip(1).forEach(line->{
             System.out.println("line: "+line);
             String[] cells = line.split(",",2);
-            String asset = cells[0];
-            String name = asset.replace("\"","");
-            if(cells.length>1) {
-                if(asset.replaceAll("[^0-9]","").length()>5) {
-                    name = cells[1].replace("\"", "");
-                    currentPatent.set(asset);
-                }
-            }
-            map.putIfAbsent(name,new HashSet<>());
-            map.get(name).add(currentPatent.get());
+            if(cells.length<2) return;
+            String asset = cells[0].replace("\"","");
+            String names = cells[1].replace("\"","");
+            Stream.of(names.split(",")).forEach(name->{
+                name = name.trim();
+                map.putIfAbsent(name,new HashSet<>());
+                map.get(name).add(asset);
+            });
         });
 
-        map.entrySet().forEach(e->{
+        map.entrySet().stream().sorted(Comparator.comparing(e->e.getKey())).forEach(e->{
             try {
                 writer.write("\"" + e.getKey() + "\","+e.getValue().size()+",\""+String.join("; ",e.getValue())+"\"\n");
             } catch(Exception ex) {
