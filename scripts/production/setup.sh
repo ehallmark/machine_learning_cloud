77f02ba3456ae802c47673479d000bd6c8392405
Evan Hallmark <ehallmark@gttgrp.com>
2018-01-24 15:24:50 -0800

    c

--- scripts/production/setup.sh
+++ scripts/production/setup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 source /home/ehallmark1122/machine_learning_cloud/scripts/production/helper/init-no-mongo.sh
 source /home/ehallmark1122/machine_learning_cloud/scripts/production/helper/setup-elasticsearch-no-timeout.sh
-sudo -u ehallmark1122 bash -c '    java -cp target/classes:"target/dependency/*" -Xms70000m -Xmx70000m -Dcom.sun.management.jmxremote.port=4567 -Dcom.sun.management.jmxremote.rmi.port=4567 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=127.0.0.1 user_interface.server.SimilarPatentServer &'
+sudo -u ehallmark1122 bash -c '    java -cp target/classes:"target/dependency/*" -Xms80000m -Xmx80000m -Dcom.sun.management.jmxremote.port=4567 -Dcom.sun.management.jmxremote.rmi.port=4567 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=127.0.0.1 user_interface.server.SimilarPatentServer &'
