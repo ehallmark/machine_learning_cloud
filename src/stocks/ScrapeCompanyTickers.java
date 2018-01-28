5002631a308d1960f9375997a493cec6feeeeb6b
Evan Hallmark <ehallmar@usc.edu>
2018-01-28 08:30:29 -0800

    c

--- src/stocks/ScrapeCompanyTickers.java
+++ src/stocks/ScrapeCompanyTickers.java
@@ -30,41 +30,28 @@ public class ScrapeCompanyTickers {
 
         //Set<String> allAssignees = new HashSet<>(Database.getAssignees());
 
-        RadixTree<String> trie = Database.getNormalizedAssigneePrefixTrie();
-        JaroWinkler distance = new JaroWinkler();
+        //RadixTree<String> trie = Database.getNormalizedAssigneePrefixTrie();
+        //JaroWinkler distance = new JaroWinkler();
 
-        NormalizeAssignees normalizer = new NormalizeAssignees();
+        //NormalizeAssignees normalizer = new NormalizeAssignees();
 
         AtomicInteger idx = new AtomicInteger(0);
-        Map<String,Set<String>> companyToTickersMap = reader.lines().parallel().map(line->{
+        Set<String> tickers = reader.lines().parallel().map(line->{
             if(idx.getAndIncrement()%10000==9999) {
                 System.out.println("Read: "+idx.get());
             }
             String[] cells = line.split(",");
             if(cells.length<2) return null;
             String symbol = cells[0];
-            String company = cells[1];
             if(symbol!=null) symbol = symbol.trim();
-            if(company!=null) company = AssigneeTrimmer.standardizedAssignee(company);
-            if(symbol.isEmpty()||company.isEmpty()||!trie.getValuesForClosestKeys(company).iterator().hasNext()) {
+            if(symbol.isEmpty()) {
                 return null;
             }
-            final String normalizedCompany = normalizer.normalizedAssignee(company);
-            if(normalizedCompany == null || company.isEmpty()) return null;
-            List<String> possible = new ArrayList<>();
-            trie.getValuesForClosestKeys(company).forEach(a->possible.add(a));
-            Pair<String,Double> companyScorePair = possible.stream()
-                    .map(p->new Pair<>(p,distance.similarity(p,normalizedCompany)))
-                    .sorted((p1,p2)->p2.getSecond().compareTo(p1.getSecond())).findFirst().get();
 
-            double score = companyScorePair.getSecond();
-            String assignee = companyScorePair.getFirst();
-            if(score>=0.9 && Math.min(normalizedCompany.length(),assignee.length()) > 3) {
-                return new Pair<>(assignee, symbol);
-            } else return null;
-        }).filter(p->p!=null).collect(Collectors.groupingBy(p->p.getFirst(),Collectors.mapping(p->p.getSecond(),Collectors.toSet())));
+            return symbol;
+        }).filter(p->p!=null).collect(Collectors.toSet());
 
-        System.out.println("Companies found: "+companyToTickersMap.size());
+        System.out.println("Companies found: "+tickers.size());
 
         // no pull stock info
         final long to = System.currentTimeMillis()/1000;
@@ -74,14 +61,14 @@ public class ScrapeCompanyTickers {
 
         BufferedWriter writer = new BufferedWriter(new FileWriter(assigneeToStockPriceOverTimeMapCSVFile));
         AtomicInteger cnt = new AtomicInteger(0);
-        companyToTickersMap.entrySet().parallelStream().forEach(e->{
-            System.out.println(""+cnt.getAndIncrement()+" / "+companyToTickersMap.size());
+        tickers.parallelStream().forEach(ticker->{
+            System.out.println(""+cnt.getAndIncrement()+" / "+tickers.size());
             try {
-                List<Pair<LocalDate, Double>> data = stockDataFor(e.getValue(), from, to);
+                List<Pair<LocalDate, Double>> data = stockDataFor(ticker, from, to);
                 if (data != null && data.size()>0) {
                     List<String> dataStr = data.stream().map(d->d.getFirst().toString()+":"+d.getSecond()).collect(Collectors.toList());
-                    writer.write("\""+e.getKey()+"\","+String.join(",",dataStr)+"\n");
-                    assigneeToStockPriceOverTimeMap.put(e.getKey(), data);
+                    writer.write("\""+ticker+"\","+String.join(",",dataStr)+"\n");
+                    assigneeToStockPriceOverTimeMap.put(ticker, data);
                 }
             } catch(Exception e2) {
 
@@ -99,16 +86,9 @@ public class ScrapeCompanyTickers {
         return (Map<String,List<Pair<LocalDate,Double>>>)Database.tryLoadObject(assigneeToStockPriceOverTimeMapFile);
     }
 
-    private static List<Pair<LocalDate,Double>> stockDataFor(Set<String> symbols, long from, long to) throws Exception {
+    private static List<Pair<LocalDate,Double>> stockDataFor(String symbol, long from, long to) throws Exception {
         // find best symbol
-        List<Pair<LocalDate,Double>> data = null;
-        List<String> sortedSymbols = new ArrayList<>(symbols);
-        Collections.sort(sortedSymbols,(s1,s2)->Integer.compare(s1.length(),s2.length()));
-        for(String symbol : sortedSymbols) {
-            List<Pair<LocalDate,Double>> tmp = ScrapeYahooStockPrices.getStocksFromSymbols(symbol, from, to);
-            if(tmp==null) continue;
-            if(data==null || tmp.size()>data.size()) data = tmp;
-        }
+        List<Pair<LocalDate,Double>> data = ScrapeYahooStockPrices.getStocksFromSymbols(symbol, from, to);
         return data;
     }
 }
