b58ecf716c10c91d7e55a37d007611cfc7e66edc
Evan Hallmark <ehallmar@usc.edu>
2018-02-03 04:06:44 -0800

    c

--- src/models/similarity_models/deep_cpc_encoding_model/DeepCPCVariationalAutoEncoderNN.java
+++ src/models/similarity_models/deep_cpc_encoding_model/DeepCPCVariationalAutoEncoderNN.java
@@ -70,8 +70,9 @@ public class DeepCPCVariationalAutoEncoderNN extends CPCVariationalAutoEncoderNN
         if(net==null) {
             //Neural net configuration
             int[] hiddenLayerEncoder = new int[]{
-                    4096,
-                    4096
+                    2048,
+                    2048,
+                    2048
             };
             int[] hiddenLayerDecoder = new int[hiddenLayerEncoder.length];
             for(int i = 0; i < hiddenLayerEncoder.length; i++) {
