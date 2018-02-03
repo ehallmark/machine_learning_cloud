81aa7dc29c61a5a23aa73c699fe418752f636f8b
Evan Hallmark <ehallmar@usc.edu>
2018-02-03 15:56:25 -0800

    c

--- src/models/similarity_models/combined_similarity_model/DeepCPC2VecEncodingPipelineManager.java
+++ src/models/similarity_models/combined_similarity_model/DeepCPC2VecEncodingPipelineManager.java
@@ -84,7 +84,8 @@ public class DeepCPC2VecEncodingPipelineManager extends DefaultPipelineManager<M
             manager.setMultiDataSetPreProcessor(new MultiDataSetPreProcessor() {
                 @Override
                 public void preProcess(MultiDataSet dataSet) {
-                    dataSet.setLabels(null);
+                    dataSet.getFeatures(1).reshape(dataSet.getFeatures(1).length(),1);
+                    dataSet.setLabels(dataSet.getFeatures());
                 }
             });
             datasetManager = manager;
@@ -141,8 +142,7 @@ public class DeepCPC2VecEncodingPipelineManager extends DefaultPipelineManager<M
         manager.setMultiDataSetPreProcessor(new MultiDataSetPreProcessor() {
             @Override
             public void preProcess(MultiDataSet dataSet) {
-                dataSet.getFeatures(1).reshape(dataSet.getFeatures(1).length(),1);
-                dataSet.setLabels(dataSet.getFeatures());
+                dataSet.setLabels(null);
             }
         });
         datasetManager = manager;
