0ffdb1312014e3bdd5dae0491e6df178f145f542
Evan Hallmark <ehallmar@usc.edu>
2018-02-04 18:30:13 -0800

    c

--- src/models/similarity_models/deep_cpc_encoding_model/DeepCPCVariationalAutoEncoderNN.java
+++ src/models/similarity_models/deep_cpc_encoding_model/DeepCPCVariationalAutoEncoderNN.java
@@ -70,10 +70,9 @@ public class DeepCPCVariationalAutoEncoderNN extends CPCVariationalAutoEncoderNN
         if(net==null) {
             //Neural net configuration
             int[] hiddenLayerEncoder = new int[]{
-                    1024,
-                    1024,
-                    1024,
-                    1024
+                    2048,
+                    2048,
+                    2048
             };
             int[] hiddenLayerDecoder = new int[hiddenLayerEncoder.length];
             for(int i = 0; i < hiddenLayerEncoder.length; i++) {
@@ -84,7 +83,7 @@ public class DeepCPCVariationalAutoEncoderNN extends CPCVariationalAutoEncoderNN
             Nd4j.getRandom().setSeed(rngSeed);
             MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
                     .seed(rngSeed)
-                    .learningRate(0.01)
+                    .learningRate(0.001)
                     .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
                     .updater(Updater.RMSPROP)
                     //.updater(Updater.ADAM)
