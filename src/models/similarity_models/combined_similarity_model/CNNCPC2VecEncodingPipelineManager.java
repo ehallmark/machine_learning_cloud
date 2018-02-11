d5b32e3fe50db20a5ff5acd268c19b09a19bcc6c
Evan Hallmark <ehallmar@usc.edu>
2018-02-10 18:20:37 -0800

    c

--- src/models/similarity_models/combined_similarity_model/CNNCPC2VecEncodingPipelineManager.java
+++ src/models/similarity_models/combined_similarity_model/CNNCPC2VecEncodingPipelineManager.java
@@ -36,7 +36,7 @@ public class CNNCPC2VecEncodingPipelineManager extends DefaultPipelineManager<Mu
     private static final File INPUT_DATA_FOLDER_WORD = new File("deep_cpc_word_2_vec_encoding_input_data");
     private static final int VECTOR_SIZE = 32;
     protected static final int BATCH_SIZE = 1024;
-    protected static final int MINI_BATCH_SIZE = 64;
+    protected static final int MINI_BATCH_SIZE = 32;
     protected static final Random rand = new Random(235);
     private static CNNCPC2VecEncodingPipelineManager MANAGER;
     protected String modelName;
