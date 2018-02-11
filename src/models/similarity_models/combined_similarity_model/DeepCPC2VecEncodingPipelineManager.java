d55df66e278e048a77c4d506f66edf01a2f98177
Evan Hallmark <ehallmar@usc.edu>
2018-02-11 12:16:34 -0800

    c

--- src/models/similarity_models/combined_similarity_model/DeepCPC2VecEncodingPipelineManager.java
+++ src/models/similarity_models/combined_similarity_model/DeepCPC2VecEncodingPipelineManager.java
@@ -306,7 +306,7 @@ public class DeepCPC2VecEncodingPipelineManager extends DefaultPipelineManager<M
         boolean forceRecreateModels = false;
         boolean runPredictions = false;
         boolean rebuildPrerequisites = false;
-        boolean trainOnWords = true;
+        boolean trainOnWords = false;
         int nEpochs = 3;
 
         if(trainOnWords && !new File(INPUT_DATA_FOLDER_WORD.getAbsolutePath()+maxSample).exists()&&runModels) {
