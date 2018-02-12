a132ba932af260ccebb3d5af7abc3f345defd60a
Evan Hallmark <ehallmar@usc.edu>
2018-02-12 08:12:44 -0800

    c

--- src/models/similarity_models/combined_similarity_model/ReverseDeepCPC2VecEncodingPipelineManager.java
+++ src/models/similarity_models/combined_similarity_model/ReverseDeepCPC2VecEncodingPipelineManager.java
@@ -80,11 +80,8 @@ public class ReverseDeepCPC2VecEncodingPipelineManager extends AbstractEncodingP
                 dataSet.setLabelsMaskArray(null);
                 dataSet.setFeaturesMaskArrays(null);
                 // added
-
                 INDArray result = encodingModel.encode(dataSet.getFeatures(0),null);
                 dataSet.setFeatures(0, result);
-                System.out.println("Shape of labels: "+Arrays.toString(dataSet.getLabels(0).shape()));
-                System.out.println("Shape of features: "+Arrays.toString(dataSet.getFeatures(0).shape()));
 
             }
         };
