f7d7d9deb2aeae0d248e48db26f812fd79e6efd4
Evan Hallmark <ehallmark@gttgrp.com>
2018-02-22 11:23:40 -0800

    c

--- src/models/similarity_models/combined_similarity_model/TestSimModels.java
+++ src/models/similarity_models/combined_similarity_model/TestSimModels.java
@@ -2,34 +2,16 @@ package models.similarity_models.combined_similarity_model;
 
 import com.google.common.util.concurrent.AtomicDouble;
 import data_pipeline.helpers.Function2;
-import elasticsearch.DataSearcher;
-import org.elasticsearch.index.query.QueryBuilder;
-import org.elasticsearch.search.sort.SortOrder;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.primitives.Pair;
-import seeding.Constants;
-import seeding.Database;
-import user_interface.acclaim_compatibility.Parser;
-import user_interface.server.SimilarPatentServer;
-import user_interface.ui_models.attributes.AbstractAttribute;
-import user_interface.ui_models.attributes.FilingNameAttribute;
-import user_interface.ui_models.attributes.computable_attributes.IsGrantedApplicationAttribute;
+import user_interface.ui_models.attributes.hidden_attributes.AssetToCPCMap;
 import user_interface.ui_models.engines.TextSimilarityEngine;
-import user_interface.ui_models.filters.AbstractBooleanExcludeFilter;
-import user_interface.ui_models.filters.AbstractFilter;
-import user_interface.ui_models.filters.AcclaimExpertSearchFilter;
-import user_interface.ui_models.portfolios.items.Item;
-import wiki.ScrapeWikipedia;
 
-import java.io.File;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
-public class TestTextModels extends TestModelHelper {
-
-
+public class TestSimModels extends TestModelHelper {
 
     public static double testModel(Map<String,Pair<String[],Set<String>>> keywordToWikiAndAssetsMap, Function2<String[],Integer,Set<String>> model, int n) {
         AtomicInteger cnt = new AtomicInteger(0);
@@ -48,63 +30,20 @@ public class TestTextModels extends TestModelHelper {
         return sum.get()/cnt.get();
     }
 
-    private static Map<String,Set<String>> keywordToFilingMap;
-    private static final File keywordToFilingMapFile = new File("keyword_to_filing_map_for_test_text_models.jobj");
-    private static Map<String,Set<String>> loadKeywordToFilingsMap(List<String> keywords) {
-        if(keywordToFilingMap==null) {
-            keywordToFilingMap = (Map<String,Set<String>>) Database.tryLoadObject(keywordToFilingMapFile);
-            if(keywordToFilingMap==null) {
-                System.out.println("Rebuilding keyword to filing map...");
-                keywordToFilingMap = Collections.synchronizedMap(new HashMap<>(keywords.size()));
-                keywords.forEach(keyword->{
-                    Set<String> results = searchForFilings(keyword);
-                    if(results!=null&&results.size()>0) {
-                        System.out.println("Num results for "+keyword+": "+results.size());
-                        keywordToFilingMap.put(keyword,results);
-                    }
-                });
-                System.out.println("Saving...");
-                Database.trySaveObject(keywordToFilingMap,keywordToFilingMapFile);
-            }
-        }
-        return keywordToFilingMap;
+    private static Map<String,Set<String>> loadFilingCPCData(Map<String,Set<String>> cpcMap) {
+        return null;
     }
-
-    private static Set<String> searchForFilings(String keyword) {
-        String comparator = Constants.SCORE;
-        Collection<AbstractAttribute> attributes = Collections.singleton(new FilingNameAttribute());
-        AcclaimExpertSearchFilter filter = new AcclaimExpertSearchFilter();
-        QueryBuilder builder = new Parser(SimilarPatentServer.SUPER_USER).parseAcclaimQuery("TAC:("+keyword+")");
-        filter.setQuery(builder);
-        Collection<AbstractFilter> filters = Arrays.asList(filter, new AbstractBooleanExcludeFilter(new IsGrantedApplicationAttribute(), AbstractFilter.FilterType.BoolFalse));
-        List<Item> items = DataSearcher.searchForAssets(attributes,filters,comparator, SortOrder.DESC, 10000, new HashMap<>(),false,false);
-        if(items==null) return null;
-        return items.stream().map(item->item.getData(Constants.FILING_NAME)).filter(obj->obj!=null).map(obj->obj.toString()).collect(Collectors.toSet());
-    };
 
     public static void main(String[] args) {
         // load input data
-        Map<String,List<String>> wikipediaData = ScrapeWikipedia.loadWikipediaMap();
-        Map<String,Set<String>> filingData = loadKeywordToFilingsMap(new ArrayList<>(wikipediaData.keySet()));
+        Map<String,Set<String>> filingData = loadFilingCPCData(new AssetToCPCMap().getPatentDataMap());
 
         final int maxSentences = 20;
         // need to run searches on the keys
 
         Set<String> allFilings = Collections.synchronizedSet(new HashSet<>());
 
-        final Map<String,Pair<String[],Set<String>>> keywordToWikiAndAssetsMap = wikipediaData.entrySet().stream()
-                .map(e->{
-                    String[] text = e.getValue().stream().limit(maxSentences)
-                            .flatMap(sentence-> Stream.of(sentence.split("\\s+")))
-                            .toArray(size->new String[size]);
-                    if(text.length==0) return null;
-                    Set<String> relevantFilings = filingData.get(e.getKey());
-                    if(relevantFilings!=null&&relevantFilings.size()>0) {
-                        allFilings.addAll(relevantFilings);
-                        return new Pair<>(e.getKey(), new Pair<>(text, relevantFilings));
-                    } else return null;
-                }).filter(p->p!=null)
-                .collect(Collectors.toMap(e->e.getFirst(),e->e.getSecond()));
+        final Map<String,Pair<String[],Set<String>>> keywordToWikiAndAssetsMap = null;
 
         // new model
         CombinedCPC2Vec2VAEEncodingPipelineManager encodingPipelineManager1 = CombinedCPC2Vec2VAEEncodingPipelineManager.getOrLoadManager(true);
