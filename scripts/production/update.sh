7bb89e0d1df4a807038fe6f3bb47fc4cd6b7a44f
Evan Hallmark <ehallmark@gttgrp.com>
2018-03-02 12:21:30 -0800

    c

--- scripts/production/update.sh
+++ scripts/production/update.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
-source /home/ehallmark1122/machine_learning_cloud/scripts/production/helper/init.sh
+source /home/ehallmark/machine_learning_cloud/scripts/production/helper/init.sh
 
 # run part 1 of updates
-sudo -u ehallmark1122 bash -c '    java -cp target/classes:"target/dependency/*" -Xms100000m -Xmx100000m -Dcom.sun.management.jmxremote.port=4567 -Dcom.sun.management.jmxremote.rmi.port=4567 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=127.0.0.1 seeding.IngestRecentUpdatesPart1'
+sudo -u ehallmark bash -c '    java -cp target/classes:"target/dependency/*" -Xms100000m -Xmx100000m -Dcom.sun.management.jmxremote.port=4567 -Dcom.sun.management.jmxremote.rmi.port=4567 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=127.0.0.1 seeding.IngestRecentUpdatesPart1'
 
 # turn on elastic search
-source /home/ehallmark1122/machine_learning_cloud/scripts/production/helper/setup-elasticsearch.sh
+source /home/ehallmark/machine_learning_cloud/scripts/production/helper/setup-elasticsearch.sh
 
 # then run part 2 of updates
-sudo -u ehallmark1122 bash -c '    java -cp target/classes:"target/dependency/*" -Xms30000m -Xmx30000m -Dcom.sun.management.jmxremote.port=4567 -Dcom.sun.management.jmxremote.rmi.port=4567 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=127.0.0.1 seeding.IngestRecentUpdatesPart2'
+sudo -u ehallmark bash -c '    java -cp target/classes:"target/dependency/*" -Xms30000m -Xmx30000m -Dcom.sun.management.jmxremote.port=4567 -Dcom.sun.management.jmxremote.rmi.port=4567 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=127.0.0.1 seeding.IngestRecentUpdatesPart2'
 
 // backup
 
