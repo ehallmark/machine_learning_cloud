993eec3d401745964ff82c5ebd566cbd400d3658
Evan Hallmark <ehallmark@gttgrp.com>
2018-03-02 12:08:41 -0800

    c

--- scripts/production/update.sh
+++ scripts/production/update.sh
@@ -10,3 +10,6 @@ source /home/ehallmark1122/machine_learning_cloud/scripts/production/helper/setu
 # then run part 2 of updates
 sudo -u ehallmark1122 bash -c '    java -cp target/classes:"target/dependency/*" -Xms30000m -Xmx30000m -Dcom.sun.management.jmxremote.port=4567 -Dcom.sun.management.jmxremote.rmi.port=4567 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=127.0.0.1 seeding.IngestRecentUpdatesPart2'
 
+// backup
+
+rsync -arv /home/ehallmark/repos/machine_learning_cloud/data/ /usb/data/
