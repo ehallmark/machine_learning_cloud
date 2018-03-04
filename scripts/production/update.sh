451ba9f1b04250c71f07a0aad118538bc8571a55
ehallmark <ehallmark@gttgrp.com>
2018-03-04 14:36:38 -0800

    c

--- scripts/production/update.sh
+++ scripts/production/update.sh
@@ -54,12 +54,11 @@ cd /home/ehallmark/repos/machine_learning_cloud/scripts/production
 sudo docker-compose down
 cd /home/ehallmark/repos/machine_learning_cloud
 
+# backup mongodb
+mv /usb/data/ai_db.gz /usb/data/ai_db.backup.gz
+mongodump --archive=/usb/data/ai_db.gz --gzip --db ai_db
 
 # backup
 rsync -arv /home/ehallmark/repos/machine_learning_cloud/data/ /usb/data/
 
-
-# backup mongodb
-mv /usb/data/ai_db.gz /usb/data/ai_db.backup.gz
-mongodump --archive=/usb/data/ai_db.gz --gzip --db ai_db
 
