a88aa3e3b657277a0e2e8eebf23a7c1374dbf9e1
ehallmark <ehallmark@gttgrp.com>
2018-03-04 12:51:29 -0800

    c

--- scripts/production/update.sh
+++ scripts/production/update.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
-source /home/ehallmark/machine_learning_cloud/scripts/production/helper/init.sh
 
 # run part 1 of updates
-sudo -u ehallmark bash -c '    java -cp target/classes:"target/dependency/*" -Xms100000m -Xmx100000m -Dcom.sun.management.jmxremote.port=4567 -Dcom.sun.management.jmxremote.rmi.port=4567 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=127.0.0.1 seeding.IngestRecentUpdatesPart1'
+java -cp target/classes:"target/dependency/*" -Xms100000m -Xmx100000m -Dcom.sun.management.jmxremote.port=4567 -Dcom.sun.management.jmxremote.rmi.port=4567 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=127.0.0.1 seeding.IngestRecentUpdatesPart1
 
 # turn on elastic search
-source /home/ehallmark/machine_learning_cloud/scripts/production/helper/setup-elasticsearch.sh
+cd /home/ehallmark/machine_learning_cloud/scripts/production
+sudo docker-compose up -d
+cd /home/ehallmark/machine_learning_cloud
+sleep 180s
 
 # then run part 2 of updates
-sudo -u ehallmark bash -c '    java -cp target/classes:"target/dependency/*" -Xms30000m -Xmx30000m -Dcom.sun.management.jmxremote.port=4567 -Dcom.sun.management.jmxremote.rmi.port=4567 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=127.0.0.1 seeding.IngestRecentUpdatesPart2'
+sudo -u ehallmark 'java -cp target/classes:"target/dependency/*" -Xms30000m -Xmx30000m -Dcom.sun.management.jmxremote.port=4567 -Dcom.sun.management.jmxremote.rmi.port=4567 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=127.0.0.1 seeding.IngestRecentUpdatesPart2
 
 // backup
 
-rsync -arv /home/ehallmark/repos/machine_learning_cloud/data/ /usb/data/
+sudo -u ehallmark 'rsync -arv /home/ehallmark/repos/machine_learning_cloud/data/ /usb/data/'
