16b129869fca09c8f3f8babd641eaf4433219bb0
Evan Hallmark <ehallmark@gttgrp.com>
2018-03-12 11:19:58 -0700

    c

--- src/models/value_models/PullAttributesForRegression.java
+++ src/models/value_models/PullAttributesForRegression.java
@@ -20,6 +20,7 @@ import java.util.*;
 
 public class PullAttributesForRegression {
     private static final File regressionDataFile = new File(Constants.DATA_FOLDER+"regression_data_file_excel.csv");
+    private static final File regressionDataFile = new File(Constants.DATA_FOLDER+"regression_data_file_excel.test.csv");
     public static void main(String[] args) {
         Set<String> gatherAssets = Database.getGatherAssets();
         System.out.println("Num gather assets: "+gatherAssets.size());
@@ -31,19 +32,36 @@ public class PullAttributesForRegression {
 
         StringJoiner header = new StringJoiner("\",\"","\"","\"\n");
         attributes.forEach(attr->header.add(attr.getName()));
+
+        Random rand = new Random(2352);
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(regressionDataFile))) {
-            writer.write(header.toString());
-            for(Item item : items) {
-                StringJoiner line = new StringJoiner(",", "", "\n");
-                attributes.forEach(attr -> {
-                    Number num = RegressionValueModel.valueFromAttribute(item, attr, 0);
-                    line.add(num.toString());
-                });
-                writer.write(line.toString());
+            try(BufferedWriter writer2 = new BufferedWriter(new FileWriter(regressionTestDataFile))) {
+                writer.write(header.toString());
+                writer2.write(header.toString());
+                for (Item item : items) {
+                    if (!Database.getGatherValueMap().containsKey(item.getName())) continue;
+                    StringJoiner line = new StringJoiner(",", "", "\n");
+                    attributes.forEach(attr -> {
+                        Number num = RegressionValueModel.valueFromAttribute(item, attr, 0);
+                        line.add(num.toString());
+                    });
+                    if(rand.nextDouble()<0.75) {
+                        writer.write(line.toString());
+                    } else {
+                        writer2.write(line.toString());
+                    }
+                }
+                writer2.flush();
+            } catch(Exception d) {
+                d.printStackTrace();
+                System.exit(1);
             }
             writer.flush();
         } catch(Exception e) {
             e.printStackTrace();
+            System.exit(1);
         }
+
+        System.out.println("Finished successfully.");
     }
 }
