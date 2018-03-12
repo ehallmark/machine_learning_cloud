1b64a5a6135fde3c95758e898051f069e24d6d68
Evan Hallmark <ehallmark@gttgrp.com>
2018-03-12 12:00:33 -0700

    c

--- src/models/value_models/regression/AIValueModel.java
+++ src/models/value_models/regression/AIValueModel.java
@@ -11,14 +11,14 @@ import java.util.List;
  * Created by ehallmark on 3/10/17.
  */
 public class AIValueModel extends RegressionValueModel {
-    private static final double INTERCEPT = -7.899e-1;
+    private static final double INTERCEPT = -1.147;
     private static final List<Double> WEIGHTS = Arrays.asList(
-            -9.028e-1,
-            1.377e-2,
-            -8.002e-2,
-            7.559e-2,
-            -5.715e-3,
-            4.852e5
+            -5.103e-01,//-9.028e-1,
+            1.562e-02,//1.377e-2,
+            3.670e-02,//-8.002e-2,
+            3.952e-02,//7.559e-2,
+            -6.050e-03,//-5.715e-3,
+            9.151e+05//4.852e5
     );
 
     public static final List<AbstractAttribute> MODELS = Arrays.asList(
