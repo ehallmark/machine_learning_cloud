9fdde64ad10119addd9bc49ef02c651b3aaa4bff
Evan Hallmark <ehallmark@gttgrp.com>
2018-03-13 10:12:06 -0700

    c

--- src/models/assignee/normalization/subsidiary/DrawGraph.java
+++ src/models/assignee/normalization/subsidiary/DrawGraph.java
@@ -1,13 +1,13 @@
 package models.assignee.normalization.subsidiary;
 
 import org.gephi.graph.api.Node;
+import seeding.Database;
 import visualization.Visualizer;
 
 import java.awt.*;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 public class DrawGraph {
     public static void main(String[] args) {
@@ -16,14 +16,27 @@ public class DrawGraph {
 
         Map<String,String> childToParentMap = ParentSubsidiaryGraph.loadChildToParentMap();
 
+        Set<String> allAssignees = Collections.synchronizedSet(new HashSet<>(Database.getAssignees()));
+        allAssignees.addAll(Database.getNormalizedAssignees());
+
+        childToParentMap = childToParentMap.entrySet().parallelStream().filter(e->allAssignees.contains(e.getValue()))
+                .collect(Collectors.toMap(e->e.getKey(),e->e.getValue()));
+
+        Set<String> matchedAssignees = new HashSet<>(childToParentMap.keySet());
+        Map<String,Integer> assigneeToPortfolioSizeMap = matchedAssignees.parallelStream()
+                .collect(Collectors.toMap(assignee->assignee,assignee->Math.max(Database.getNormalizedAssetCountFor(assignee),Database.getAssetCountFor(assignee))));
+
+        System.out.println("Num assignees matched: "+childToParentMap.size());
+
         Map<String,Node> nodeMap = Collections.synchronizedMap(new HashMap<>());
         AtomicInteger cnt = new AtomicInteger(0);
         childToParentMap.forEach((child,parent)->{
             if(child.equals(parent)) return;
             try {
+                Node childNode = nodeMap.containsKey(child) ? nodeMap.get(child) : visualizer.addNode(child, assigneeToPortfolioSizeMap.getOrDefault(child,1), Color.RED);
+                Node parentNode = nodeMap.containsKey(parent) ? nodeMap.get(parent) : visualizer.addNode(parent, assigneeToPortfolioSizeMap.getOrDefault(parent,1), Color.BLUE);
+                if(!childNode.getColor().equals(Color.RED)) childNode.setColor(Color.RED);
 
-                Node childNode = nodeMap.containsKey(child) ? nodeMap.get(child) : visualizer.addNode(child, 2f, Color.RED);
-                Node parentNode = nodeMap.containsKey(parent) ? nodeMap.get(parent) : visualizer.addNode(parent, 4f, Color.BLUE);
                 nodeMap.putIfAbsent(child, childNode);
                 nodeMap.putIfAbsent(parent, parentNode);
                 visualizer.addEdge(parentNode, childNode, 1f, Color.BLACK);
