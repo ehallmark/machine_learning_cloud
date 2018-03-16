ed85c26ee13ee946b89a9bb66289de5dcfd3a278
Evan Hallmark <ehallmark@gttgrp.com>
2018-03-16 11:31:04 -0700

    c

--- scripts/production/start.sh
+++ scripts/production/start.sh
@@ -3,6 +3,5 @@ cd /home/ehallmark/repos/machine_learning_cloud
 
 # start server and save PID to /home/ehallmark/repos/machine_learning_cloud/app.pid
 
-
-java -cp target/classes:"target/dependency/*" -Xms90000m -Xmx90000m user_interface.server.SimilarPatentServer &
+java -cp target/classes:"target/dependency/*" -Xms950000m -Xmx950000m user_interface.server.SimilarPatentServer &
 echo "$!" > /home/ehallmark/repos/machine_learning_cloud/app.pid
