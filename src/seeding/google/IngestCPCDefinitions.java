3f5c4c07edade26be6213777a6e6d15f1b19f0db
ehallmark <ehallmark@gttgrp.com>
2018-03-24 06:50:36 -0700

    c

--- src/seeding/google/IngestCPCDefinitions.java
+++ src/seeding/google/IngestCPCDefinitions.java
@@ -14,7 +14,7 @@ public class IngestCPCDefinitions {
 
     public static void main(String[] args) {
         final String idField = "symbol";
-        final File dataDir = new File("/media/ehallmark/My Passport/data/google-big-query/cpc/");
+        final File dataDir = new File("/usb2/data/google-big-query/cpc/");
         final MongoClient client = MongoDBClient.get();
         final MongoCollection collection = client.getDatabase(INDEX_NAME).getCollection(TYPE_NAME);
 
