0d9ee54f1ffe2137c5539c6d60168f54e3da25c3
ehallmark <ehallmark@gttgrp.com>
2018-04-07 17:59:12 -0700

    c

--- src/seeding/google/postgres/attribute_tables/assignment_documentid.sql
+++ src/seeding/google/postgres/attribute_tables/assignment_documentid.sql
@@ -6,10 +6,8 @@ create table big_query_assignment_documentid (
     doc_kind varchar(8) not null,
     is_filing boolean not null, -- convenience field
     country_code varchar(8) not null default('US'),
-    date date -- could be useful for matching
-
-    constraint big_query_assignment_documentid_id
-        primary key (reel_frame,doc_number)
+    date date, -- could be useful for matching
+    primary key (reel_frame,doc_number)
 );
 
 create index big_query_assignment_documentid_doc_number_idx on big_query_assignment_documentid (doc_number);
