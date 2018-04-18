69efa00aef3412087316a22aa065b2939fd97683
Evan Hallmark <ehallmark@gttgrp.com>
2018-04-17 17:32:01 -0700

    c

---
+++ src/seeding/google/postgres/IngestPTABData.java
@@ -0,0 +1,16 @@
+package seeding.google.postgres;
+
+/**
+ * Created by Evan on 1/22/2017.
+ */
+public class IngestPTABData {
+
+    private static void ingestData()throws Exception {
+
+  
+    }
+
+    public static void main(String[] args) throws Exception {
+        ingestData();
+    }
+}
