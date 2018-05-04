fd27f97385cd942662fb222b3280d033c29dd193
ehallmark <ehallmark@gttgrp.com>
2018-05-04 00:08:58 -0700

    c

--- src/public_pair/DownloadPDFsFromReedTech.java
+++ src/public_pair/DownloadPDFsFromReedTech.java
@@ -49,7 +49,7 @@ public class DownloadPDFsFromReedTech {
         // go through data folder and ingest any files that are missing
         final AtomicLong cnt = new AtomicLong(0);
         Collections.shuffle(applicationNumbers, new Random(System.currentTimeMillis()));
-        applicationNumbers.stream().forEach(p->{
+        applicationNumbers.parallelStream().forEach(p->{
             String appNum = p._1;
             long bytes = p._2;
             File file = fileFromApplicationNumber(appNum);
