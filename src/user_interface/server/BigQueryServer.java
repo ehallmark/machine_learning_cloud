6ef4240f5db6fc6786adcb74bbc08f6827a54a10
Evan Hallmark <ehallmark@gttgrp.com>
2018-05-07 15:36:22 -0700

    c

--- src/user_interface/server/BigQueryServer.java
+++ src/user_interface/server/BigQueryServer.java
@@ -2514,7 +2514,7 @@ public class BigQueryServer {
                                                                 )
                                                         ), div().withClass("col-12 attributeElement").with(
                                                                 label("List Item Separator").attr("style","width: 100%;").with(
-                                                                        input().withId("main-options-"+LIST_ITEM_SEPARATOR_FIELD).withClass("form-control").withType("text").attr("style","margin-top: 5px; margin-left: auto; width: 100px; margin-right: auto;").withPlaceholder("Defaults to '; '").withName(LIST_ITEM_SEPARATOR_FIELD)
+                                                                        input().withId("main-options-"+LIST_ITEM_SEPARATOR_FIELD).withClass("form-control").withType("text").attr("style","margin-top: 5px; margin-left: auto; width: 100px; margin-right: auto;").withPlaceholder("; ").withName(LIST_ITEM_SEPARATOR_FIELD)
                                                                 )
                                                         )
                                                 )
--- src/user_interface/server/SimilarPatentServer.java
+++ src/user_interface/server/SimilarPatentServer.java
@@ -2875,7 +2875,7 @@ public class SimilarPatentServer {
                                                                 )
                                                         ), div().withClass("col-12 attributeElement").with(
                                                                 label("List Item Separator").attr("style","width: 100%;").with(
-                                                                        input().withId("main-options-"+LIST_ITEM_SEPARATOR_FIELD).withClass("form-control").withType("text").attr("style","margin-top: 5px; margin-left: auto; width: 100px; margin-right: auto;").withPlaceholder("Defaults to '; '").withName(LIST_ITEM_SEPARATOR_FIELD)
+                                                                        input().withId("main-options-"+LIST_ITEM_SEPARATOR_FIELD).withClass("form-control").withType("text").attr("style","margin-top: 5px; margin-left: auto; width: 100px; margin-right: auto;").withPlaceholder("; ").withName(LIST_ITEM_SEPARATOR_FIELD)
                                                                 )
                                                         )
                                                 )
