b3edfdeb8c170fba564310226a6343b6e43eee96
Evan Hallmark <ehallmark@gttgrp.com>
2018-05-11 12:56:18 -0700

    c

--- src/seeding/google/elasticsearch/attributes/DateRangeAttribute.java
+++ src/seeding/google/elasticsearch/attributes/DateRangeAttribute.java
@@ -2,6 +2,7 @@ package seeding.google.elasticsearch.attributes;
 
 import java.time.Duration;
 import java.time.LocalDate;
+import java.time.Period;
 import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalAmount;
 
@@ -9,6 +10,6 @@ public interface DateRangeAttribute {
     default LocalDate getMinDate() { return LocalDate.now().minusYears(20).withMonth(1).withDayOfMonth(1); }
     default LocalDate getMaxDate() { return LocalDate.now().plusYears(1).withMonth(1).withDayOfMonth(1); }
     default String dateFormatString() { return "yyyy"; }
-    default TemporalAmount timeInterval() { return Duration.of(1L, ChronoUnit.YEARS); }
+    default TemporalAmount timeInterval() { return Period.ofYears(1); }
 
 }
