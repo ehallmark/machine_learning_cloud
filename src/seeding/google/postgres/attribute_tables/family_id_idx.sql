0f25c5c38970273bd7a347c4bb75632b33d03174
ehallmark <ehallmark@gttgrp.com>
2018-05-12 14:36:42 -0700

    c

--- src/seeding/google/postgres/attribute_tables/family_id_idx.sql
+++ src/seeding/google/postgres/attribute_tables/family_id_idx.sql
@@ -1,6 +1,7 @@
 \connect patentdb
 
 -- helper table for user interface to quickly find family members
+drop table big_query_family_id
 create table big_query_family_id (
     publication_number_full varchar(32) primary key,
     publication_number_with_country varchar(32) not null,
@@ -10,16 +11,19 @@ create table big_query_family_id (
     application_number varchar(32),
     application_number_formatted varchar(32),
     application_number_formatted_with_country varchar(32),
-    family_id varchar(32) not null
+    family_id varchar(32)
 );
 
-insert into big_query_family_id (
+-- must run this after patents_global merged
+insert into big_query_family_id (publication_number_full,publication_number_with_country,
+        publication_number,application_number_full,application_number_with_country,
+        application_number,application_number_formatted,application_number_formatted_with_country,
+        family_id) (
     select publication_number_full,publication_number_with_country,publication_number,
         application_number_full,application_number_with_country,application_number,
         application_number_formatted,application_number_formatted_with_country,
         family_id
-    from patents_global
-    where family_id is not null and family_id != '-1'
+    from patents_global_merged
 );
 
 
