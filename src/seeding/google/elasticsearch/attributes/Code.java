d35b1fec9c7df1873f96cec9f9be90aee328e65b
Evan Hallmark <ehallmark@gttgrp.com>
2018-05-15 16:30:25 -0700

    c

--- src/seeding/google/elasticsearch/attributes/Code.java
+++ src/seeding/google/elasticsearch/attributes/Code.java
@@ -3,6 +3,7 @@ package seeding.google.elasticsearch.attributes;
 import seeding.Constants;
 import seeding.google.elasticsearch.Attributes;
 import user_interface.ui_models.attributes.tools.AjaxMultiselect;
+import user_interface.ui_models.filters.AbstractFilter;
 
 public class Code extends KeywordWithPrefixAttribute implements AjaxMultiselect {
     @Override
@@ -13,5 +14,10 @@ public class Code extends KeywordWithPrefixAttribute implements AjaxMultiselect 
     @Override
     public String ajaxUrl() {
         return Constants.CPC_CODE_AJAX_URL;
+    }
+
+    @Override
+    public AbstractFilter.FieldType getFieldType() {
+        return AbstractFilter.FieldType.Multiselect;
     }
 }
--- src/seeding/google/elasticsearch/attributes/Tree.java
+++ src/seeding/google/elasticsearch/attributes/Tree.java
@@ -1,10 +1,17 @@
 package seeding.google.elasticsearch.attributes;
 
 import seeding.google.elasticsearch.Attributes;
+import user_interface.ui_models.filters.AbstractFilter;
 
 public class Tree extends KeywordAttribute {
     @Override
     public String getName() {
         return Attributes.TREE;
     }
+
+    @Override
+    public AbstractFilter.FieldType getFieldType() {
+        return AbstractFilter.FieldType.Multiselect;
+    }
+    
 }
