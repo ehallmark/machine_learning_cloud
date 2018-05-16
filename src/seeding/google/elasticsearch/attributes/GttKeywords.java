321d0e2531d882192e60bf10e811bc0933c6a09e
ehallmark <ehallmark@gttgrp.com>
2018-05-15 21:54:14 -0700

    c

--- src/seeding/google/elasticsearch/attributes/GttKeywords.java
+++ src/seeding/google/elasticsearch/attributes/GttKeywords.java
@@ -2,7 +2,7 @@ package seeding.google.elasticsearch.attributes;
 
 import seeding.google.elasticsearch.Attributes;
 
-public class GttKeywords extends KeywordAndTextAttribute implements SignificantTermsAttribute {
+public class GttKeywords extends KeywordAndTextAttribute {
     @Override
     public String getName() {
         return Attributes.KEYWORDS;
