2af487085063a8b411e8dd11c79f699669ad642f
ehallmark <ehallmark@gttgrp.com>
2018-05-16 09:02:15 -0700

    c

--- src/python_compatibility/rnn_enc/IngestWord2VecToText.java
+++ src/python_compatibility/rnn_enc/IngestWord2VecToText.java
@@ -8,6 +8,7 @@ import seeding.google.word2vec.Word2VecManager;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -39,10 +40,10 @@ public class IngestWord2VecToText {
         }
         {
             BufferedWriter bw = new BufferedWriter(new FileWriter(new File("/home/ehallmark/Downloads/word2vec256_vectors.txt")));
-            INDArray weights = word2Vec.getLookupTable().getWeights();
-            for(int i = 0; i < weights.rows(); i++) {
-                INDArray row = weights.getRow(i);
-                double[] d = row.data().asDouble();
+            double[] weights = word2Vec.getLookupTable().getWeights().data().asDouble();
+            for(int i = 0; i < weights.length/word2Vec.getLayerSize(); i++) {
+                double[] d = Arrays.copyOfRange(weights, i*word2Vec.getLayerSize(), i*word2Vec.getLayerSize()+word2Vec.getLayerSize());
+                if(d.length!=word2Vec.getLayerSize()) throw new RuntimeException("Invalid layer size: "+d.length);
                 for(int j = 0; j < d.length; j++) {
                     double x = d[j];
                     bw.write(String.valueOf(x));
