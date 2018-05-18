0747d33030173c4afdd4f5820b1f910248a9ef29
Evan Hallmark <ehallmark@gttgrp.com>
2018-05-18 11:16:36 -0700

    c

--- src/user_interface/server/GatherClassificationServer.java
+++ src/user_interface/server/GatherClassificationServer.java
@@ -1,24 +1,18 @@
 package user_interface.server;
 
 import com.google.gson.Gson;
-import elasticsearch.DataIngester;
-import elasticsearch.MyClient;
 import models.classification_models.TechnologyClassifier;
 import org.nd4j.linalg.primitives.Pair;
-import org.elasticsearch.client.transport.TransportClient;
-import org.elasticsearch.index.query.QueryBuilders;
-import seeding.Constants;
-import seeding.Database;
-import user_interface.server.tools.SimpleAjaxMessage;
+import seeding.google.elasticsearch.Attributes;
 import spark.Request;
 import spark.Response;
-import models.classification_models.ClassificationAttr;
-import models.classification_models.WIPOTechnologyClassifier;
-import user_interface.ui_models.attributes.computable_attributes.TechnologyAttribute;
+import user_interface.server.tools.SimpleAjaxMessage;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static spark.Spark.*;
 
@@ -26,8 +20,8 @@ import static spark.Spark.*;
  * Created by ehallmark on 11/1/16.
  */
 public class GatherClassificationServer {
-    private static ClassificationAttr techTagger = new TechnologyClassifier(TechnologyAttribute.getOrCreate());
-    private static ClassificationAttr wipoTagger = new WIPOTechnologyClassifier();
+    private static TechnologyClassifier techTagger = new TechnologyClassifier(Attributes.TECHNOLOGY);
+    private static TechnologyClassifier wipoTagger = new TechnologyClassifier(Attributes.WIPO_TECHNOLOGY);
     public static void StartServer() throws Exception {
         get("/predict_patents", (req, res) -> handleRequest(req, res, (patents, tagLimit) -> techTagger.attributesFor(patents,tagLimit)));
         post("/predict_patents", (req, res) -> handleRequest(req, res, (patents, tagLimit) -> techTagger.attributesFor(patents,tagLimit)));
@@ -40,10 +34,11 @@ public class GatherClassificationServer {
     private static String handleRequest(Request req, Response res, Function2<Collection<String>,Integer,List<Pair<String,Double>>> function) throws Exception {
         try {
             res.type("application/json");
-            if (req.queryParams("reelFrames") == null || req.queryParams("reelFrames").length() == 0)
+            String patentStr = req.queryParams("assets");
+            if (patentStr==null || patentStr.length() == 0)
                 return new Gson().toJson(new SimpleAjaxMessage("Please provide at least one patent."));
 
-            List<String> reelFrames = Arrays.asList(req.queryParams("reelFrames").split("\\s+"));
+            List<String> patents = Arrays.asList(patentStr.split("\\s+"));
             int tmp = 3;
             if (req.queryParams("limit") != null && req.queryParams("limit").length() > 0) {
                 try {
@@ -55,17 +50,6 @@ public class GatherClassificationServer {
 
             // make sure patents exist
             // run model
-
-            TransportClient client = MyClient.get();
-            System.out.println("Searching for reel frames: " + String.join("; ", reelFrames));
-
-            Collection<String> patents = Stream.of(client.prepareSearch(DataIngester.INDEX_NAME)
-                    .setTypes(DataIngester.TYPE_NAME)
-                    .setFetchSource(new String[]{Constants.NAME},new String[]{})
-                    .setSize(100)
-                    .setQuery(QueryBuilders.termsQuery(Constants.REEL_FRAME, reelFrames))
-                    .get().getHits().getHits()).map(hit -> (String)hit.getSource().get(Constants.NAME)).collect(Collectors.toList());
-            System.out.println("Found " + patents.size() + " patents");
 
             List<Pair<String, Double>> topTags = new ArrayList<>(function.apply(patents, tagLimit));
 
