27d48dca4923bb61c3b78829157ec947d60092a2
ehallmark <ehallmark@gttgrp.com>
2018-05-26 15:35:54 -0700

    c

--- src/seeding/google/postgres/attribute_tables/embedding.sql
+++ src/seeding/google/postgres/attribute_tables/embedding.sql
@@ -70,3 +70,8 @@ create table big_query_embedding_by_fam (
     family_id varchar(32) primary key,
     enc float[] not null
 );
+
+create table big_query_embedding_cpc (
+    code varchar(32) primary key, -- eg. US9923222B1
+    enc float[] not null
+);
