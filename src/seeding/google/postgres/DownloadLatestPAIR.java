f6cf95b92e497b88df6cbc19a2e5f18af4fcba4d
Evan Hallmark <ehallmark@gttgrp.com>
2018-05-30 11:44:18 -0700

    c

--- src/seeding/google/postgres/DownloadLatestPAIR.java
+++ src/seeding/google/postgres/DownloadLatestPAIR.java
@@ -1,17 +1,27 @@
 package seeding.google.postgres;
 
-import seeding.ai_db_updater.iterators.FileIterator;
-import seeding.ai_db_updater.pair_bulk_data.PAIRHandler;
+import seeding.Database;
 import seeding.data_downloader.PAIRDataDownloader;
 
+import java.io.File;
 import java.time.LocalDate;
 
 /**
  * Created by Evan on 8/5/2017.
  */
 public class DownloadLatestPAIR {
+    private static LocalDate lastIngestedDate;
+    private static final File lastIngestedDateFile = new File("pair_last_ingest_date.jobj");
+    static {
+        lastIngestedDate = (LocalDate) Database.tryLoadObject(lastIngestedDateFile);
+    }
     public static void main(String[] args) {
-        PAIRDataDownloader downloader = new PAIRDataDownloader();
-        downloader.pullMostRecentData();
+        if(lastIngestedDate!=null && lastIngestedDate.isBefore(LocalDate.now().minusDays(7))) {
+            PAIRDataDownloader downloader = new PAIRDataDownloader();
+            downloader.pullMostRecentData();
+            Database.trySaveObject(LocalDate.now(), lastIngestedDateFile);
+        } else {
+            System.out.println("Skipping redownload of pair...");
+        }
     }
 }
