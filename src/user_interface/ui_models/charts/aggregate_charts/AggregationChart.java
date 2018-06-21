4c862d0bcb55aab4be3663f281838833030ac067
Evan Hallmark <ehallmark@gttgrp.com>
2018-06-20 17:16:42 -0700

    c

--- src/user_interface/ui_models/charts/aggregate_charts/AggregationChart.java
+++ src/user_interface/ui_models/charts/aggregate_charts/AggregationChart.java
@@ -270,8 +270,8 @@ public abstract class AggregationChart<T> extends AbstractChartAttribute {
 
     public Aggregation handlePotentiallyNestedAgg(Aggregations aggregations, String attrNameWithSuffix, String attrNameNestedWithSuffix) {
         if (aggregations == null) return null; // important to stop recursion
-        //System.out.println("Handling potentially nested agg: "+attrNameNestedWithSuffix+" with nested name: "+attrNameNestedWithSuffix);
-        //System.out.println("Available aggregations: "+String.join("; ", aggregations.getAsMap().keySet()));
+        System.out.println("Handling potentially nested agg: "+attrNameNestedWithSuffix+" with nested name: "+attrNameNestedWithSuffix);
+        System.out.println("Available aggregations: "+String.join("; ", aggregations.getAsMap().keySet()));
         Aggregation agg = aggregations.get(attrNameWithSuffix);
         if (agg == null) {
             agg = aggregations.get(attrNameWithSuffix + SignificantTermsAggregation.SAMPLER_SUFFIX);
