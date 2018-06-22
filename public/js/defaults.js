f72f859d6b0fd4d24778a1b714e77b43eb121490
Evan Hallmark <ehallmark@gttgrp.com>
2018-06-22 12:20:23 -0700

    c

--- public/js/defaults.js
+++ public/js/defaults.js
@@ -1898,8 +1898,8 @@ var setupJSTree = function(tree_id, dblclickFunction, node_type, jsNodeDataFunct
         node_customize: {
             default: function(elem, node) {
                 if(node_type==='dataset' && node.type==='file' && node.hasOwnProperty('data') && node.data.hasOwnProperty('assetcount')) { // check node itself
-                    var $anchor = $('#'+node.id+'_anchor');
-                    if($anchor.length) {
+                    var $anchor = $(elem).children()[1];
+                    if($anchor) {
                         $anchor.prev().text(node.data['assetcount']);
                     }
                 }
