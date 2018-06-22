64384b9a0e3a573ed5792263e9486e6e763a553a
Evan Hallmark <ehallmark@gttgrp.com>
2018-06-22 14:26:12 -0700

    c

--- scripts/production/elasticsearch-no-init.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-source /home/ehallmark/machine_learning_cloud/scripts/production/helper/setup-elasticsearch.sh
-source /home/ehallmark/machine_learning_cloud/scripts/production/helper/recreate-elasticsearch.sh
--- scripts/production/helper/setup-elasticsearch-no-timeout.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-cd /home/ehallmark/machine_learning_cloud/scripts/production
-sudo -u ehallmark bash -c '    echo "in elasticsearch"'
-sudo -u ehallmark bash -c '    sudo docker-compose up -d'
-sudo -u ehallmark bash -c '    echo "Docker compose up"'
-cd /home/ehallmark/machine_learning_cloud
