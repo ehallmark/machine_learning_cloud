ef18c8c468233b29e47fa5211f0c29e817048ab2
Evan Hallmark <ehallmark@gttgrp.com>
2018-06-25 16:03:42 -0700

    c

--- src/seeding/google/postgres/attribute_tables/latest_assignee.sql
+++ src/seeding/google/postgres/attribute_tables/latest_assignee.sql
@@ -2,32 +2,44 @@
 
 drop table big_query_patent_to_latest_assignee;
 create table big_query_patent_to_latest_assignee (
-    doc_number varchar(32) not null, -- eg. US9923222B1
-    doc_kind varchar(8) not null,
-    is_filing boolean not null,
-    country_code varchar(8),
+    application_number_formatted_with_country primary key, -- eg. US9923222B1
     assignee text[] not null,
-    date date,
-    security_interest boolean,
-    primary key (doc_number,is_filing)
+    date date
 );
-
 -- ingest assignment table
-insert into big_query_patent_to_latest_assignee (doc_number,doc_kind,is_filing,country_code,assignee,date,security_interest) (
-    select distinct on (doc_number,doc_kind='X0')
-        doc_number,
-        doc_kind,
-        doc_kind='X0',
-        country_code,
+insert into big_query_patent_to_latest_assignee (
+    select distinct on (application_number_formatted_with_country)
+        application_number_formatted_with_country,
         assignee,
-        execution_date,
-        upper(coalesce(conveyance_text,'')) like '%SECURITY%'
+        execution_date
         from big_query_assignments as latest
         join big_query_assignment_documentid as document
         on (latest.reel_frame=document.reel_frame)
-        where assignee is not null
-        order by doc_number,doc_kind='X0',execution_date desc nulls last,recorded_date desc NULLS LAST
+        where assignee is not null and not upper(coalesce(conveyance_text,'')) like '%SECURITY%'
+        order by application_number_formatted_with_country,execution_date desc nulls last,recorded_date desc NULLS LAST
 );
+
+drop table big_query_patent_to_security_interest;
+create table big_query_patent_to_security_interest (
+    application_number_formatted_with_country primary key, -- eg. US9923222B1
+    security_interest_holder text[] not null,
+    date date
+);
+insert into big_query_patent_to_security_interest (
+    select application_number_formatted_with_country,assignee,execution_date from (
+        select distinct on (application_number_formatted_with_country)
+            application_number_formatted_with_country,
+            assignee,
+            execution_date,
+            upper(coalesce(conveyance_text,'')) like '%SECURITY%' as security_interest
+            from big_query_assignments as latest
+            join big_query_assignment_documentid as document
+            on (latest.reel_frame=document.reel_frame)
+            where assignee is not null and not
+            order by application_number_formatted_with_country,execution_date desc nulls last,recorded_date desc NULLS LAST
+    ) as temp where security_interest
+);
+
 
 -- definite
 drop table big_query_patent_to_latest_assignee_by_pub;
@@ -35,23 +47,33 @@ create table big_query_patent_to_latest_assignee_by_pub (
     publication_number_full varchar(32) primary key,
     first_assignee text not null,
     assignee text[] not null,
-    date date,
-    security_interest boolean not null
+    date date
 );
 
-insert into big_query_patent_to_latest_assignee_by_pub (publication_number_full,first_assignee,assignee,date,security_interest) (
-    select distinct on (publication_number_full) publication_number_full,(coalesce(la.assignee,p.assignee_harmonized))[1],coalesce(la.assignee,p.assignee_harmonized),coalesce(la.date,case when la.assignee is null then coalesce(p.priority_date,p.filing_date) else null end),coalesce(la.security_interest,'f')
+insert into big_query_patent_to_latest_assignee_by_pub (
+    select distinct on (publication_number_full) publication_number_full,(coalesce(la.assignee,p.assignee_harmonized))[1],coalesce(la.assignee,p.assignee_harmonized),coalesce(la.date,case when la.assignee is null then coalesce(p.priority_date,p.filing_date) else null end)
     from patents_global as p
     left outer join big_query_patent_to_latest_assignee as la
-    on (p.country_code='US' and((p.publication_number=la.doc_number and not la.is_filing) OR (p.application_number=la.doc_number and la.is_filing)))
-    where family_id!='-1' and (coalesce(la.assignee,p.assignee_harmonized))[1] is not null
+    on ('US'||p.application_number_formatted=la.application_number_formatted_with_country)
+    where (coalesce(la.assignee,p.assignee_harmonized))[1] is not null and p.application_number_formatted is not null
     order by publication_number_full,date desc nulls last,publication_date desc nulls last
 );
-
-
 create index big_query_latest_by_pub_first_assignee_idx on big_query_patent_to_latest_assignee_by_pub (first_assignee);
 
-
+drop table big_query_patent_to_security_interest_by_pub;
+create table big_query_patent_to_security_interest_by_pub (
+    publication_number_full varchar(32) primary key,
+    security_interest_holder text not null,
+    date date
+);
+insert into big_query_patent_to_security_interest_by_pub (
+    select distinct on (publication_number_full) publication_number_full,si.security_interest_holder[1],si.date
+    from patents_global as p
+    left outer join big_query_patent_to_security_interest as si
+    on ('US'||p.application_number_formatted=si.application_number_formatted_with_country)
+    where si.security_interest_holder is not null and si.security_interest_holder[1] is not null and p.application_number_formatted is not null
+    order by publication_number_full,date desc nulls last,publication_date desc nulls last
+);
 
 -- good guess
 drop table big_query_patent_to_latest_assignee_by_family;
@@ -59,17 +81,15 @@ create table big_query_patent_to_latest_assignee_by_family (
     family_id varchar(32) primary key,
     first_assignee text not null,
     assignee text[] not null,
-    date date,
-    security_interest boolean not null
+    date date
 );
 
-insert into big_query_patent_to_latest_assignee_by_family (family_id,first_assignee,assignee,date,security_interest) (
-    select distinct on (family_id) family_id,(coalesce(la.assignee,p.assignee_harmonized))[1],coalesce(la.assignee,p.assignee_harmonized),coalesce(la.date,case when la.assignee is null then coalesce(p.priority_date,p.filing_date) else null end),coalesce(la.security_interest,'f')
+insert into big_query_patent_to_latest_assignee_by_family (
+    select distinct on (family_id) family_id,(coalesce(la.assignee,p.assignee_harmonized))[1],coalesce(la.assignee,p.assignee_harmonized),coalesce(la.date,case when la.assignee is null then coalesce(p.priority_date,p.filing_date) else null end)
     from patents_global as p
     left outer join big_query_patent_to_latest_assignee as la
-    on (p.country_code='US' and((p.publication_number=la.doc_number and not la.is_filing) OR (p.application_number=la.doc_number and la.is_filing)))
-    where family_id!='-1' and (coalesce(la.assignee,p.assignee_harmonized))[1] is not null
+    on ('US'||p.application_number_formatted=la.application_number_formatted_with_country)
+    where family_id!='-1' and (coalesce(la.assignee,p.assignee_harmonized))[1] is not null and p.application_number_formatted is not null
     order by family_id,date desc nulls last,publication_date desc nulls last
 );
-
 create index big_query_latest_by_family_first_assignee_idx on big_query_patent_to_latest_assignee_by_family (first_assignee);
