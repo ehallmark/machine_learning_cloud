4753adeea1a76d11757d2dcf60325fa1c3532651
Evan Hallmark <ehallmark@gttgrp.com>
2018-06-26 13:43:07 -0700

    c

--- scripts/production/start.sh
+++ scripts/production/start.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 cd /home/ehallmark/repos/machine_learning_cloud
 
-# start server and save PID to /home/ehallmark/repos/machine_learning_cloud/app.pid
+# start python similarity vectorizer
+source /home/ehallmark/environments/tfenv/bin/activate
+cd /home/ehallmark/repos/gtt_models/
+python3 src/java_compatibility/BuildCPCEncodings.py &
+echo "$!" > /home/ehallmark/repos/machine_learning_cloud/python_server.pid
+cd /home/ehallmark/repos/machine_learning_cloud
 
-java -cp target/classes:"target/dependency/*" -Xms100000m -Xmx100000m user_interface.server.BigQueryServer &
+# start server and save PID to /home/ehallmark/repos/machine_learning_cloud/app.pid
+java -cp target/classes:"target/dependency/*" -Xms15000m -Xmx15000m user_interface.server.BigQueryServer &
 echo "$!" > /home/ehallmark/repos/machine_learning_cloud/app.pid
--- scripts/production/update_postgres_monthly.sh
+++ scripts/production/update_postgres_monthly.sh
@@ -6,8 +6,15 @@ psql postgres < src/seeding/google/postgres/attribute_tables/ai_value.sql
 # assignee portfolio sizes
 psql postgres < src/seeding/google/postgres/attribute_tables/assignee_table.sql
 
-# run technology tagger
+# run similarity model
+source /home/ehallmark/environments/tfenv/bin/activate
+cd /home/ehallmark/repos/gtt_models/
+python3 src/java_compatibility/BuildCPCEncodings.py
+python3 src/java_compatibility/BuildPatentEncodings.py
+cd /home/ehallmark/repos/machine_learning_cloud
 
+# run technology tagger
+java -cp target/classes:"target/dependency/*" -Xms10000m -Xmx10000m seeding.google.postgres.IngestAssigneeEmbeddingsToPostgres
 
 # run keyword prediction
 java -cp target/classes:"target/dependency/*" -Xms10000m -Xmx10000m seeding.google.tech_tag.PredictKeywords
