04bffde46f2c71cb84283b426c683ca1a4509ccf
Evan Hallmark <ehallmark@gttgrp.com>
2018-06-26 12:59:25 -0700

    c

--- scripts/production/update_postgres_monthly.sh
+++ scripts/production/update_postgres_monthly.sh
@@ -7,9 +7,10 @@ psql postgres < src/seeding/google/postgres/attribute_tables/ai_value.sql
 psql postgres < src/seeding/google/postgres/attribute_tables/assignee_table.sql
 
 # run technology tagger
+
+
+# run keyword prediction
 java -cp target/classes:"target/dependency/*" -Xms10000m -Xmx10000m seeding.google.tech_tag.PredictKeywords
 psql postgres < src/seeding/google/postgres/attribute_tables/patent_keywords_aggs.sql
 java -cp target/classes:"target/dependency/*" -Xms10000m -Xmx10000m seeding.google.tech_tag.FilterKeywordsByTFIDF
 
-
-# run keyword prediction
