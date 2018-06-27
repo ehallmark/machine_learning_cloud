b15ff8af1e2438494786d430e0d93d48fcb78f74
Evan Hallmark <ehallmark@gttgrp.com>
2018-06-27 11:42:47 -0700

    c

--- src/seeding/google/elasticsearch/attributes/CitedPublicationNumber.java
+++ src/seeding/google/elasticsearch/attributes/CitedPublicationNumber.java
@@ -1,31 +1,21 @@
 package seeding.google.elasticsearch.attributes;
 
 import seeding.google.elasticsearch.Attributes;
-import user_interface.ui_models.attributes.AbstractAttribute;
-import user_interface.ui_models.filters.AbstractFilter;
 
-import java.util.Collection;
-import java.util.Collections;
-
-public class CitedPublicationNumber extends AbstractAttribute {
-    public CitedPublicationNumber() {
-        super(Collections.emptyList());
-    }
-
+public class CitedPublicationNumber extends AssetKeywordAttribute {
     @Override
     public String getName() {
         return Attributes.CITED_PUBLICATION_NUMBER;
     }
 
     @Override
-    public String getType() {
-        return "keyword";
+    public String getAssetPrefix() {
+        return Attributes.CITATIONS+".cited_";
     }
 
     @Override
-    public AbstractFilter.FieldType getFieldType() {
-        return AbstractFilter.FieldType.Text;
+    public boolean isApplication() {
+        return false;
     }
-
 
 }
--- src/seeding/google/elasticsearch/attributes/CitedPublicationNumberFull.java
+++ src/seeding/google/elasticsearch/attributes/CitedPublicationNumberFull.java
@@ -1,20 +1,29 @@
 package seeding.google.elasticsearch.attributes;
 
 import seeding.google.elasticsearch.Attributes;
+import user_interface.ui_models.attributes.AbstractAttribute;
+import user_interface.ui_models.filters.AbstractFilter;
 
-public class CitedPublicationNumberFull extends AssetKeywordAttribute {
+import java.util.Collections;
+
+public class CitedPublicationNumberFull extends AbstractAttribute {
+    public CitedPublicationNumberFull() {
+        super(Collections.emptyList());
+    }
+
     @Override
     public String getName() {
         return Attributes.CITED_PUBLICATION_NUMBER_FULL;
     }
 
     @Override
-    public String getAssetPrefix() {
-        return Attributes.CITATIONS+".cited_";
+    public String getType() {
+        return "keyword";
     }
 
     @Override
-    public boolean isApplication() {
-        return false;
+    public AbstractFilter.FieldType getFieldType() {
+        return AbstractFilter.FieldType.Text;
     }
+
 }
