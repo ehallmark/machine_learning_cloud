54058200a3cf9c69fc468ca9986b468fa223928e
Evan Hallmark <ehallmark@gttgrp.com>
2018-06-29 11:34:46 -0700

    c

--- scripts/production/backup.sh
+++ scripts/production/backup.sh
@@ -7,6 +7,10 @@ java -cp target/classes:"target/dependency/*" -Xms5000m -Xmx5000m elasticsearch.
 
 # backup
 rsync -arv /home/ehallmark/repos/machine_learning_cloud/ /usb/machine_learning_cloud_backup/
+rsync -arv /home/ehallmark/repos/poi/ /usb/poi_backup/
+rsync -arv /home/ehallmark/data/python/ /usb/python_backup/
+rsync -arv /home/ehallmark/repos/gtt_models/ /usb/gtt_models_backup/
 
 # dump all
 pg_dump -Fc --dbname=postgresql://postgres:password@127.0.0.1:5432/patentdb > /usb/big_query_database.dump
+
