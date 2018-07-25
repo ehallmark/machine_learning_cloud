430a603d907b7e5709caced530e9254034e1e356
Evan Hallmark <ehallmark@gttgrp.com>
2018-07-25 11:31:50 -0700

    c

--- scripts/production/start.sh
+++ scripts/production/start.sh
@@ -1,13 +1,40 @@
 #!/usr/bin/env bash
-cd /home/ehallmark/repos/machine_learning_cloud
+
+SLEEP_TIME=1d
+
 
 # start python similarity vectorizer
 source /home/ehallmark/environments/tfenv/bin/activate
-cd /home/ehallmark/repos/gtt_models/
-python3 src/java_compatibility/BuildCPCEncodings.py &
-echo "$!" > /home/ehallmark/repos/machine_learning_cloud/python_server.pid
-cd /home/ehallmark/repos/machine_learning_cloud
 
-# start server and save PID to /home/ehallmark/repos/machine_learning_cloud/app.pid
-java -cp target/classes:"target/dependency/*" -Xms15000m -Xmx15000m user_interface.server.BigQueryServer &
-echo "$!" > /home/ehallmark/repos/machine_learning_cloud/app.pid
+
+while :
+do
+    # start python encoding server for text vectorizer
+    cd /home/ehallmark/repos/gtt_models/
+    python3 src/server/EncodingServer.py &
+    ENCODER_PID = $!
+    echo "$ENCODER_PID" > /home/ehallmark/repos/machine_learning_cloud/python_server.pid
+
+    # start server and save PID to /home/ehallmark/repos/machine_learning_cloud/app.pid
+    cd /home/ehallmark/repos/machine_learning_cloud
+    java -cp target/classes:"target/dependency/*" -Xms15000m -Xmx15000m user_interface.server.BigQueryServer &
+    APP_PID=$!
+    echo "$APP_PID" > /home/ehallmark/repos/machine_learning_cloud/app.pid
+
+    sleep 1h
+    # app is now running!
+
+
+    sleep $SLEEP_TIME
+    echo "Killing java and python servers..."
+    kill $APP_PID
+    kill $ENCODER_PID
+    echo "Starting updates..."
+    source scripts/production/update_postgres_weekly.sh
+
+    # backup
+    echo "Backing up..."
+    source scripts/production/backup.sh
+
+
+done
