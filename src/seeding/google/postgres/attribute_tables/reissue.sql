8bfed46760d0221e659d43793ecdfd338a14450e
Evan Hallmark <ehallmark@gttgrp.com>
2018-07-30 16:09:51 -0700

    c

--- src/seeding/google/postgres/attribute_tables/reissue.sql
+++ src/seeding/google/postgres/attribute_tables/reissue.sql
@@ -7,12 +7,23 @@ create table big_query_reissue (
 );
 
 insert into big_query_reissue (
-    select p.publication_number_full,
-    mode() within group (order by p2.kind_code)
-    from patents_global as p
-    join patents_global as p2
-    on ((p.application_number_formatted,p.country_code)=(p2.application_number_formatted,p2.country_code))
-    where p.country_code = 'US' and p.kind_code like 'E%'
-    and not p2.kind_code like 'E%'
-    group by p.publication_number_full
+    select pc.publication_number_full, 'S'
+    from big_query_priority_claims_by_pub as pc
+    join patents_global as p on (p.publication_number_full=pc.publication_number_full)
+    where p.country_code='US' and kind_code like 'E%' and (
+        array_to_string(pc.pc_publication_number_full,' ','') like '%USD%'
+        or array_to_string(pc.pc_application_number_formatted_with_country, ' ','') like '%US29%'
+    )
 );
+
+
+insert into big_query_reissue (
+    select pc.publication_number_full, 'P'
+    from big_query_priority_claims_by_pub as pc
+    join patents_global as p on (p.publication_number_full=pc.publication_number_full)
+    where p.country_code='US' and kind_code like 'E%' and (
+        array_to_string(pc.pc_publication_number_full,' ','') like '%USP%'
+    );
+) on conflict (publication_number_full) do nothing;
+
+
