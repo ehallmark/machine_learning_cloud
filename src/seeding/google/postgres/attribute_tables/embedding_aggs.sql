79310c01bf971fa52563811369b0c44276376fd2
ehallmark <ehallmark@gttgrp.com>
2018-07-29 17:25:51 -0700

    c

--- src/seeding/google/postgres/attribute_tables/embedding_aggs.sql
+++ src/seeding/google/postgres/attribute_tables/embedding_aggs.sql
@@ -1,10 +1,13 @@
 \connect patentdb
 
+drop table big_query_embedding_by_pub;
+create table big_query_embedding_by_pub (
+    publication_number_full varchar(32) primary key,
+    enc float[] not null
+);
 
 insert into big_query_embedding_by_pub (publication_number_full,enc) (
     select f.publication_number_full, p.enc from big_query_embedding_by_fam as p
     join big_query_family_id as f on (p.family_id=f.family_id)
-    full outer join big_query_embedding_by_pub as e
-    on (e.publication_number_full=f.publication_number_full)
-    where p.family_id != '-1' and e.publication_number_full is null
+    where p.family_id != '-1'
 );
