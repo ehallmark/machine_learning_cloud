43565236f695c0c658c849c841f3a649fca23482
Evan Hallmark <ehallmark@gttgrp.com>
2018-08-02 14:07:37 -0700

    c

--- src/seeding/google/postgres/attribute_tables/granted.sql
+++ src/seeding/google/postgres/attribute_tables/granted.sql
@@ -9,6 +9,6 @@ create table big_query_granted (
 insert into big_query_granted (publication_number_full,granted) (
     select publication_number_full, (country_code='US' and publication_date <= '2000-12-31'::date) OR not kind_code like 'A%'
     from patents_global
-    where country_code='US'
+    where country_code='US' OR country_code='EP'
 );
 
