c764c95a28680a17bc3fc5fd0679ca05a36abd21
ehallmark <ehallmark@gttgrp.com>
2018-08-06 19:43:36 -0700

    c

--- src/user_interface/server/BigQueryServer.java
+++ src/user_interface/server/BigQueryServer.java
@@ -2944,8 +2944,8 @@ public class BigQueryServer extends SimilarPatentServer {
         return html().with(
                 head().with(
                         title("PSP - GTT Group"),
-                        link().withRel("icon").withType("image/png").withHref("/images/favicon-16x16.png"),
-                        link().withRel("icon").withType("image/png").withHref("/images/favicon-32x32.png"),
+                        link().withRel("icon").withType("image/png").attr("sizes", "32x32").withHref("/images/favicon-32x32.png"),
+                        link().withRel("icon").withType("image/png").attr("sizes", "16x16").withHref("/images/favicon-16x16.png"),
                         script().withSrc("/js/jquery-3.3.1.min.js"),
                         script().withSrc("/js/jquery-ui-1.12.1.min.js"),
                         script().withText(
