5ea301e23994cb2a6500bc1ed9f52cbac452d5e6
ehallmark <ehallmark@gttgrp.com>
2018-08-13 08:31:23 -0700

    c

--- scripts/production/update_similarity.sh
+++ scripts/production/update_similarity.sh
@@ -1,6 +1,7 @@
-// run python code under gtt_models/src/java_compatibility/BuildPatentEncodings.py
+# run python code under gtt_models/src/java_compatibility/BuildPatentEncodings.py
 cd ~/repos/gtt_models/
 git pull origin master
 . ~/environments/tfenv/bin/activate
+cd src/java_compatibility
 python3 BuildPatentEncodings.py
 cd ~/repos/machine_learning_cloud/
