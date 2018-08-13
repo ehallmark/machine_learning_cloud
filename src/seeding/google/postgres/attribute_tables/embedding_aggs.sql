68d7e9ff4c871a345acecd5fd58c5d4fc9a65a39
ehallmark <ehallmark@gttgrp.com>
2018-08-13 09:06:22 -0700

    c

--- src/seeding/google/postgres/attribute_tables/embedding_aggs.sql
+++ src/seeding/google/postgres/attribute_tables/embedding_aggs.sql
@@ -8,14 +8,16 @@ delete from big_query_embedding_by_fam where family_id in (
 
 delete from big_query_embedding_by_fam where family_id = '-1';
 
-drop table big_query_embedding_by_pub;
-create table big_query_embedding_by_pub (
+create table big_query_embedding_by_pub2 (
     publication_number_full varchar(32) primary key,
     enc float[] not null
 );
 
-insert into big_query_embedding_by_pub (publication_number_full,enc) (
+insert into big_query_embedding_by_pub2 (publication_number_full,enc) (
     select f.publication_number_full, p.enc from big_query_embedding_by_fam as p
     join big_query_family_id as f on (p.family_id=f.family_id)
     where p.family_id != '-1'
 );
+
+drop table big_query_embedding_by_pub;
+alter table big_query_embedding_by_pub2 rename to big_query_embedding_by_pub;
