67d0060e7ac1e37f57cf0fc8652c6194a6bc5300
Evan Hallmark <ehallmark@gttgrp.com>
2018-08-29 14:24:02 -0700

    c

--- public/js/defaults.js
+++ public/js/defaults.js
@@ -396,13 +396,13 @@ var handlePreviewAssetsAjax = function(chartId, group1, group2) {
                 $box.append(data.html);
                 var $preview = $box.find('#data-table-preview');
                 var xButton = $('<span style="float: right; cursor: pointer;">X</span>');
-                $preview.find('form').filter(':first').append(xButton);
+                $preview.prepend(xButton);
                 xButton.click(function(e) {
                     $box.trigger('click');
                 });
                 var tableId = 'main-preview-data-table';
                 var $table = $('#'+tableId);
-                $table.wrap('<div style="overflow-y: auto; width: 100%; height: 100%;"></div>');
+                $table.wrap('<div style="overflow-y: auto; margin-top: 15px; width: 100%; height: 100%;"></div>');
                 $table.parent().prepend($table.parent().prev());
                 $table.parent().prepend($table.parent().prev());
 
