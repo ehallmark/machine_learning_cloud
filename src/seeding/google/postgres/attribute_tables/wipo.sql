2620310d3f25f7b81a08d253138fff98f796614f
ehallmark <ehallmark@gttgrp.com>
2018-09-03 09:04:09 -0700

    c

--- src/seeding/google/postgres/attribute_tables/wipo.sql
+++ src/seeding/google/postgres/attribute_tables/wipo.sql
@@ -7,3 +7,10 @@ create table big_query_wipo (
     wipo_technology text not null,
     primary key (publication_number, sequence)
 );
+
+
+drop table big_query_wipo_prediction;
+create table big_query_wipo_prediction (
+    publication_number_full varchar(32) primary key,
+    wipo_technology text not null
+);
