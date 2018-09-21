9fe640acc9aa36c262e7ae1ac418866a64e4d966
Evan Hallmark <ehallmark@gttgrp.com>
2018-09-21 16:26:10 -0700

    c

--- src/models/assignee/entity_prediction/AssigneeGuess.java
+++ src/models/assignee/entity_prediction/AssigneeGuess.java
@@ -2,6 +2,11 @@ package models.assignee.entity_prediction;
 
 import com.opencsv.CSVReader;
 import graphical_modeling.util.Pair;
+import models.genetics.GeneticAlgorithm;
+import models.genetics.Listener;
+import models.genetics.Solution;
+import models.genetics.SolutionCreator;
+import org.jetbrains.annotations.NotNull;
 import org.nd4j.linalg.primitives.AtomicDouble;
 import seeding.Database;
 
@@ -18,7 +23,7 @@ import java.util.stream.Collectors;
 
 public class AssigneeGuess {
 
-    private static Pair<String, Double> bestGuessAssignee(String[] inventors, LocalDate filingDate, Map<String, Map<LocalDate, String>> assigneeAndDateMap) {
+    static Pair<String, Double> bestGuessAssignee(String[] inventors, LocalDate filingDate, Map<String, Map<LocalDate, String>> assigneeAndDateMap, final double maxDateDiff, final double minScore, final double minTotalScore) {
         Map<String, Map<String,AtomicDouble>> inventorScoreMaps = new HashMap<>();
         for(String inventor : inventors) {
             Map<String,AtomicDouble> scoreMap = new HashMap<>();
@@ -28,7 +33,7 @@ public class AssigneeGuess {
                 assigneeAndDates.forEach((date, assignee)->{
                     scoreMap.putIfAbsent(assignee, new AtomicDouble(0));
                     double dateDiff = Math.abs((double)filingDate.getYear() + ((double)filingDate.getMonthValue()-1.0)/12.0 - ((double)date.getYear() + ((double)date.getMonthValue()-1.0)/12.0));
-                    if(dateDiff < 2.0) {
+                    if(dateDiff < maxDateDiff) {
                         scoreMap.get(assignee).getAndAdd(1.0 / (1.0 + dateDiff));
                     }
                 });
@@ -41,9 +46,9 @@ public class AssigneeGuess {
         Map.Entry<String,Double> bestEntry = inventorScoreMaps.values().stream().flatMap(map->map.entrySet().stream()).collect(Collectors.groupingBy(e->e.getKey(), Collectors.summingDouble(e->e.getValue().get())))
         .entrySet().stream().max(Comparator.comparingDouble(e->e.getValue())).orElse(null);
 
-        if(totalScore > 4 && bestEntry!=null) {
+        if(totalScore > minTotalScore && bestEntry!=null) {
             final double score = bestEntry.getValue() / totalScore;
-            if (score > 0.25) {
+            if (score > minScore) {
                 return new Pair<>(bestEntry.getKey(), score);
             }
         }
@@ -83,51 +88,146 @@ public class AssigneeGuess {
         long correctCount = 0L;
         long wrongCount = 0L;
         long available = 0L;
-        while(rs.next()) {
+
+        Map<String, Object[]> data = new HashMap<>();
+        while(rs.next() && count < 100000) {
             String publicationNumberFull = rs.getString(1);
             LocalDate date = rs.getDate(2).toLocalDate();
             String[] inventors = (String[]) rs.getArray(3).getArray();
-            Pair<String, Double> bestGuessAssignee = bestGuessAssignee(inventors, date, assigneeAndDateMap);
-            if(bestGuessAssignee!=null) {
-                //System.out.println(publicationNumberFull+": Found "+bestGuessAssignee._1+" with score: "+bestGuessAssignee._2);
-                insertStatement.setString(1, publicationNumberFull);
-                insertStatement.setString(2, bestGuessAssignee._1);
-                insertStatement.executeUpdate();
-                try {
-                    if(rs.getArray(4)!=null) {
-                        String[] assignees = (String[]) rs.getArray(4).getArray();
-                        if (assignees != null && assignees.length > 0) {
-                            // System.out.println("Actual assignees: "+String.join("; ", assignees));
-                            boolean found = false;
-                            for (String assignee : assignees) {
-                                if (bestGuessAssignee._1.equals(assignee)) {
-                                    found = true;
-                                }
-                            }
-                            if (found) {
-                                correctCount++;
-                            } else {
-                                wrongCount++;
-                            }
-                        }
+            try {
+                if(rs.getArray(4)!=null) {
+                    String[] assignees = (String[]) rs.getArray(4).getArray();
+                    if (assignees != null && assignees.length > 0) {
+                        data.put(publicationNumberFull, new Object[]{date, inventors, assignees});
                     }
-                } catch(Exception e) {
-                    e.printStackTrace();
                 }
-                available++;
+            } catch(Exception e) {
+                e.printStackTrace();
             }
+
             if(count%10000==9999) {
                 long missing = count - available;
                 conn.commit();
-                System.out.println("Seen "+count+", Predicted: "+available+", Correct: "+correctCount+", Wrong: "+wrongCount + ", Missing: "+missing+", Accuracy: "+((double)correctCount)/(wrongCount+correctCount));
+                final double accuracy = ((double)correctCount)/(wrongCount+correctCount);
+                final double predictPercent = ((double)available) / count;
+                final double score = accuracy * predictPercent;
+                System.out.println("Score: "+score+", Seen "+count+", Predicted: "+available+", Prediction %: "+predictPercent+", Correct: "+correctCount+", Wrong: "+wrongCount + ", Missing: "+missing+", Accuracy: "+accuracy);
             }
             count ++;
         }
 
         rs.close();
         ps.close();
+
+        SolutionCreator solutionCreator = new SolutionCreator() {
+            @Override
+            public Collection<Solution> nextRandomSolutions(int n) {
+                List<Solution> solutions = new ArrayList<>();
+                for(int i = 0; i < n; i++) {
+                    solutions.add(new AssigneeSolution(data, assigneeAndDateMap));
+                }
+                return solutions;
+            }
+        };
+
+        Listener listener = new Listener() {
+            @Override
+            public void print(Solution solution) {
+                System.out.println("Solution: "+solution.toString());
+            }
+        };
+        GeneticAlgorithm<Solution> algorithm = new GeneticAlgorithm<>(solutionCreator, 5, listener, 3);
+        algorithm.simulate(Long.MAX_VALUE, 0.5, 0.5);
+
+
+        insertStatement.close();
         conn.commit();
         conn.close();
 
+    }
+}
+
+class AssigneeSolution implements Solution {
+    private static final Random random = new Random(235928);
+    private double score;
+    private final double maxDateDiff;
+    private final double minScore;
+    private final double minTotalScore;
+    private Map<String,Object[]> data;
+    private Map<String, Map<LocalDate, String>> assigneeAndDateMap;
+    public AssigneeSolution(final double maxDateDiff, final double minScore, final double minTotalScore, Map<String,Object[]> data, Map<String, Map<LocalDate, String>> assigneeAndDateMap) {
+        this.maxDateDiff=maxDateDiff;
+        this.assigneeAndDateMap = assigneeAndDateMap;
+        this.data=data;
+        this.minScore=minScore;
+        this.minTotalScore=minTotalScore;
+    }
+
+    public AssigneeSolution(Map<String,Object[]> data, Map<String, Map<LocalDate, String>> assigneeAndDateMap) {
+        this(random.nextDouble()*4, random.nextDouble(), random.nextDouble()*10.0, data, assigneeAndDateMap);
+    }
+
+    @Override
+    public double fitness() {
+        return score;
+    }
+
+    @Override
+    public void calculateFitness() {
+        score = data.entrySet().stream().mapToDouble(e->{
+            String publicationNumberFull = e.getKey();
+            LocalDate date = (LocalDate) e.getValue()[0];
+            String[] inventors = (String[])e.getValue()[1];
+            String[] assignees = (String[])e.getValue()[2];
+            Pair<String, Double> bestGuessAssignee = AssigneeGuess.bestGuessAssignee(inventors, date, assigneeAndDateMap, maxDateDiff, minScore, minTotalScore);
+            if(bestGuessAssignee!=null) {
+                //System.out.println(publicationNumberFull+": Found "+bestGuessAssignee._1+" with score: "+bestGuessAssignee._2);
+
+                // System.out.println("Actual assignees: "+String.join("; ", assignees));
+                boolean found = false;
+                for (String assignee : assignees) {
+                    if (bestGuessAssignee._1.equals(assignee)) {
+                        found = true;
+                    }
+                }
+                if (found) {
+                    return 1.0;
+                }
+            }
+            return 0.0;
+        }).sum()/data.size();
+    }
+
+    @Override
+    public Solution mutate() {
+        return new AssigneeSolution(
+                random.nextBoolean() ? maxDateDiff : random.nextDouble() * 4,
+                random.nextBoolean() ? minScore : random.nextDouble(),
+                random.nextBoolean() ? minTotalScore : random.nextDouble() * 10.0,
+                data,
+                assigneeAndDateMap
+        );
+    }
+
+    @Override
+    public Solution crossover(Solution _other) {
+        AssigneeSolution other = (AssigneeSolution) _other;
+        return new AssigneeSolution(
+                random.nextBoolean() ? maxDateDiff : other.maxDateDiff,
+                random.nextBoolean() ? minScore : other.minScore,
+                random.nextBoolean() ? minTotalScore : other.minTotalScore,
+                data,
+                assigneeAndDateMap
+        );
+    }
+
+    @Override
+    public int compareTo(@NotNull Solution o) {
+        return Double.compare(fitness(), o.fitness());
+    }
+
+    @Override
+    public String toString() {
+        return "Score: "+fitness()+"; Params: maxDateDiff: "+maxDateDiff+", minScore: "+minScore+", minTotalScore: "+minTotalScore;
     }
 }
