ea4b8318f937506c14349df681934d55aba7b614
Evan Hallmark <ehallmark@gttgrp.com>
2018-10-05 13:50:55 -0700

    c

--- src/crunchbase_api/PullDataForCompany.java
+++ src/crunchbase_api/PullDataForCompany.java
@@ -10,34 +10,33 @@ import user_interface.ui_models.portfolios.items.Item;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
 public class PullDataForCompany {
     private static Map<String, Object> getDataForCompany(String company) {
+        int limit = 1;
+        Map<String, Object> data = new HashMap<>();
         SearchResponse response = DataSearcher.getClient().prepareSearch(IngestPatents.INDEX_NAME)
                 .setTypes(IngestPatents.TYPE_NAME)
                 .setQuery(QueryBuilders.boolQuery()
                     .filter(QueryBuilders.matchPhraseQuery(Attributes.LATEST_ASSIGNEES+"."+Attributes.LATEST_ASSIGNEE, company))
                 )
                 .setTrackScores(false)
-                .setSize(1)
+                .setSize(limit)
                 .setFetchSource(true)
                // .addAggregation() // TODO
                 .get();
         DataSearcher.iterateOverSearchResults(response, hit -> {
             Item item = new Item(hit.getId());
             item.getDataMap().putAll(hit.getSource());
+            data.put(item.getName(), item.getDataMap());
 
             return item;
-        }, -1, false, true);
-        Map<String, Object> data = new HashMap<>();
+        }, limit, false, false);
         return data;
     }
 
 
     public static void main(String[] args) throws Exception {
-        Set<String> assignees = null;
-
         String[] companies = new String[]{
                 "microsoft",
                 "google llc",
