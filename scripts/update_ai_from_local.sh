1cdcaa1546d437c939dfbedc2bc39d78f802ff08
Administrator <Admin@Administrators-MacBook-Pro.local>
2019-10-12 14:20:26 -0700

    c

--- scripts/update_ai_from_local.sh
+++ scripts/update_ai_from_local.sh
@@ -9,14 +9,14 @@ while :
 do
 
 echo "Updating data..."
-ssh -t $USER@$SERVER 'java -cp target/classes:"target/dependency/*" -Xms10000m -Xmx10000m seeding.google.UpdateAll'
+ssh -t $USER@$SERVER 'cd ~/repos/machine_learning_cloud && java -cp target/classes:"target/dependency/*" -Xms10000m -Xmx10000m seeding.google.UpdateAll'
 echo "Done."
 
 echo "Waiting a few hours before updating elasticsearch..."
 sleep 10000
 
 echo "Updating elasticsearch..."
-ssh -t $USER@$SERVER 'java -cp target/classes:"target/dependency/*" -Xms10000m -Xmx10000m seeding.google.elasticsearch.IngestESFromPostgres'
+ssh -t $USER@$SERVER 'cd ~/repos/machine_learning_cloud && java -cp target/classes:"target/dependency/*" -Xms10000m -Xmx10000m seeding.google.elasticsearch.IngestESFromPostgres'
 echo "Done."
 
 echo "Waiting a day to give the server some rest..."
