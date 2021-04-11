7ab3361bccaab845ad68791717ef64cf1e83d8f9
Evan Hallmark <ehallmar@usc.edu>
2021-04-10 17:31:59 -0700

    work on es merge

--- scripts/production/update_elasticsearch.sh
+++ scripts/production/update_elasticsearch.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-curl -XDELETE localhost:9200/big_query
+ES_HOST=10.20.20.10
+
+curl -XDELETE $ES_HOST:9200/big_query
 sleep 5
 java -cp target/classes:"target/dependency/*" -Xms10000m -Xmx10000m seeding.google.elasticsearch.CreatePatentIndex
 sleep 5
-curl -XPUT 'localhost:9200/big_query/_settings?pretty' -H 'Content-Type: application/json' -d'
+curl -XPUT "$ES_HOST:9200/big_query/_settings?pretty" -H 'Content-Type: application/json' -d'
 {
     "index" : {
         "refresh_interval" : "-1"
@@ -13,7 +15,7 @@ curl -XPUT 'localhost:9200/big_query/_settings?pretty' -H 'Content-Type: applica
 sleep 5
 java -cp target/classes:"target/dependency/*" -Xms10000m -Xmx10000m seeding.google.elasticsearch.IngestESFromPostgres
 sleep 5
-curl -XPUT 'localhost:9200/big_query/_settings?pretty' -H 'Content-Type: application/json' -d'
+curl -XPUT "$ES_HOST:9200/big_query/_settings?pretty" -H 'Content-Type: application/json' -d'
 {
     "index" : {
         "refresh_interval" : "60s"
