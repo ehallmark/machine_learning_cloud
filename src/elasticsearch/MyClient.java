584d7d08e06ea333bbe8cfff1b26a06905b7a31e
Evan Hallmark <ehallmar@usc.edu>
2021-04-10 17:19:25 -0700

    work on es merge

--- src/elasticsearch/MyClient.java
+++ src/elasticsearch/MyClient.java
@@ -31,9 +31,9 @@ public class MyClient {
                     .put("client.transport.sniff", sniff)
                     .put("cluster.name","elasticsearch").build();
             TransportClient client = new PreBuiltTransportClient(settings)
-                    .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9300))
-                    .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9301))
-                    .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9302));
+                    .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByAddress(new byte[]{10,20,20,10}), 9300));
+                    //.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9301))
+                    //.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9302));
                     //.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9303));
             CLIENT = client;
         } catch (UnknownHostException e) {
